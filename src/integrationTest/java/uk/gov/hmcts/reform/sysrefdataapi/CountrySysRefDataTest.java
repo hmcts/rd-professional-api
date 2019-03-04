ff256097fa4d2a93c6f8ce5dd0188427a4614e6d
Ravi Aghera <Ravi.Aghera@HMCTS.net:wq>
2019-03-04 17:24:27 +0000

    tweaking config defaults

--- src/integrationTest/java/uk/gov/hmcts/reform/sysrefdataapi/CountrySysRefDataTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/sysrefdataapi/CountrySysRefDataTest.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -23,7 +22,6 @@ import uk.gov.hmcts.reform.sysrefdataapi.domain.entities.Country;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = MOCK)
-@ActiveProfiles("Integration")
 public class CountrySysRefDataTest {
 
     @Autowired
