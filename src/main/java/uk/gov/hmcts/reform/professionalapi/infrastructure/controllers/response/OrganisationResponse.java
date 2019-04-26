1c07c2f4e32d345c1b32d32d8d557dabe806ef71
Oozeerally <778425@cognizant.com>
2019-04-26 16:14:31 +0100

    Updated Organisation Response after conflict

--- src/main/java/uk/gov/hmcts/reform/professionalapi/infrastructure/controllers/response/OrganisationResponse.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/infrastructure/controllers/response/OrganisationResponse.java
@@ -6,26 +6,6 @@ import uk.gov.hmcts.reform.professionalapi.domain.entities.Organisation;
 
 public class OrganisationResponse {
 
-    @JsonProperty
-    private final String id;
-    @JsonProperty
-    private final String name;
-    @JsonProperty
-    private final List<String> userIds;
-    @JsonProperty
-    private final List<String> pbaAccounts;
-
-    public OrganisationResponse(Organisation organisation) {
-        this.id = organisation.getId().toString();
-        this.name = organisation.getName();
-        this.userIds = organisation.getUsers()
-                .stream()
-                .map(user -> user.getId().toString())
-                .collect(toList());
-        this.pbaAccounts = organisation.getPaymentAccounts()
-                .stream()
-                .map(acc -> acc.getPbaNumber())
-                .collect(toList());
     private final String organisationIdentifier;
 	
     public OrganisationResponse(Organisation organisation) {
