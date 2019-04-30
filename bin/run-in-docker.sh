8618ed9229b169c5125a68b27eb3586bb31b3521
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2019-04-30 16:55:40 +0100

    Fix local docker env to replicate azure functional environment with working S2S

--- bin/run-in-docker.sh
+++ bin/run-in-docker.sh
@@ -3,8 +3,9 @@
 GRADLE_CLEAN=true
 GRADLE_INSTALL=true
 
-S2S_SECRET=AAAAAAAAAAAAAAAC
-S2S_MICROSERVICE=rd_professional_api
+# Test S2S key - not used in any HMCTS key vaults or services
+export S2S_SECRET=SZDUA3L7N32PE2IS
+export S2S_MICROSERVICE=rd_professional_api
 
 build_s2s_image() {
     git clone git@github.com:hmcts/s2s-test-tool.git
