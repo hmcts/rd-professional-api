770b5f201be753d66d7d78f9f16a257b5a45db4d
Tabitha Cromarty <39907749+TabbyC@users.noreply.github.com>
2019-05-01 18:26:17 +0100

    RDCC 73,85: Create pipeline for professional reference data (#16)
    * Add initial Helm configuration to enable Docker deployment
    
    * Change chart name to match the directory name
    
    * Use correct name for Java config in values.yaml
    
    Also add helm generated files to gitignore
    
    * Remove explicit postgres dependency and move templated values to env specific template file
    
    * Put in config for postgres chart from hmcts/java
    
    * Attempt to correct postgres config to fix failing functional tests
    
    * Update dependencies and clear out stale suppressions
    
    * Change build files to ensure S2S_SECRET is set from vault
    
    * Log S2S variables to ensure they're being obtained correctly in Java
    
    The secret is redacted for security reasons
    
    * Use secret from vault for S2S
    
    * Switch volume-properties library for trying to use the keyVault feature of chart-java
    
    * Go back to using volume-mount
    
    * Fix health endpoint
    
    * Get S2S URL from vault?
    
    * Exclude hibernate autoconfig from functional tests
    
    * Use client for functional tests so that they authenticate with S2S properly and easily
    
    * Restore accidentally deleted run-in-docker script
    
    * Log test S2S properties to see why they're not working
    
    * Increse redaction of logged secrets
    
    * Fix local docker env to replicate azure functional environment with working S2S
    
    * Use anonymousPaths and approvedServices from application.yaml in security config
    
    This fixes s2s authentication with functional tests :)
    
    * Set content type and accepts headers in functional test requests
    
    * Remove invalid TF outputs
    
    * Tidy up and ensure test S2S URL is set in all jenkins files
    
    * Remove sandbox Jenkins file as AKS builds don't work on sandbox

--- src/main/java/uk/gov/hmcts/reform/professionalapi/infrastructure/config/AuthCheckerConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/infrastructure/config/AuthCheckerConfiguration.java
@@ -1,25 +1,35 @@
 package uk.gov.hmcts.reform.professionalapi.infrastructure.config;
 
 import com.google.common.collect.ImmutableSet;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
+
 import javax.servlet.http.HttpServletRequest;
+
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "security")
+@Slf4j
 public class AuthCheckerConfiguration {
 
-    private final List<String> authorisedServices = new ArrayList<>();
-    private final List<String> authorisedRoles = new ArrayList<>();
+    List<String> authorisedServices;
+    List<String> authorisedRoles = new ArrayList<>();
 
     public List<String> getAuthorisedServices() {
         return authorisedServices;
+    }
+
+    public void setAuthorisedServices(List<String> authorisedServices) {
+        this.authorisedServices = authorisedServices;
     }
 
     public List<String> getAuthorisedRoles() {
@@ -28,6 +38,7 @@ public class AuthCheckerConfiguration {
 
     @Bean
     public Function<HttpServletRequest, Collection<String>> authorizedServicesExtractor() {
+        log.info(String.format("Configured authorised services: %s", String.join(", ", authorisedServices)));
         return any -> ImmutableSet.copyOf(authorisedServices);
     }
 
