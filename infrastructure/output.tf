f6a4db857b9132cc5a10ecc6fffbfbbe30ab0afb
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2019-05-03 17:49:47 +0100

    Restore outputs as they are needed, but correct the values

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -9,3 +9,11 @@ output "resourceGroup" {
 output "appServicePlan" {
   value = "${local.app_service_plan}"
 }
+
+output "vaultName" {
+  value = "${local.key_vault_name}"
+}
+
+output "vaultUri" {
+  value = "${local.s2s_vault_uri}"
+}
