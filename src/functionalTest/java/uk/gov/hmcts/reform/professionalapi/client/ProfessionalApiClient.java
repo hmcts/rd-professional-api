bb89ae5a86a5ba70f48ba826b26406e8d31843c9
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2019-05-03 18:55:43 +0100

    Log response from createOrganisation in functional test client if not success

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/client/ProfessionalApiClient.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/client/ProfessionalApiClient.java
@@ -18,9 +18,12 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import lombok.extern.slf4j.Slf4j;
+
 import uk.gov.hmcts.reform.professionalapi.infrastructure.controllers.request.OrganisationCreationRequest;
 import uk.gov.hmcts.reform.professionalapi.infrastructure.controllers.request.PbaAccountCreationRequest;
 
+@Slf4j
 public class ProfessionalApiClient {
 
     private static final ObjectMapper mapper = new ObjectMapper();
@@ -39,20 +42,24 @@ public class ProfessionalApiClient {
 
     public String getWelcomePage() {
         return withUnauthenticatedRequest()
-            .get("/")
-            .then()
-            .statusCode(OK.value())
-            .and()
-            .extract().body().asString();
+                .get("/")
+                .then()
+                .statusCode(OK.value())
+                .and()
+                .extract()
+                .body()
+                .asString();
     }
 
     public String getHealthPage() {
         return withUnauthenticatedRequest()
-            .get("/health")
-            .then()
-            .statusCode(OK.value())
-            .and()
-            .extract().body().asString();
+                .get("/health")
+                .then()
+                .statusCode(OK.value())
+                .and()
+                .extract()
+                .body()
+                .asString();
     }
 
     @SuppressWarnings("unchecked")
@@ -61,51 +68,55 @@ public class ProfessionalApiClient {
 
         List<PbaAccountCreationRequest> pbaAccounts = asList(
                                                              aPbaPaymentAccount()
-                                                                 .pbaNumber(pbas[0])
-                                                                 .build(),
+                                                                     .pbaNumber(pbas[0])
+                                                                     .build(),
                                                              aPbaPaymentAccount()
-                                                                 .pbaNumber(pbas[1])
-                                                                 .build());
+                                                                     .pbaNumber(pbas[1])
+                                                                     .build());
 
         PbaAccountCreationRequest superUserPaymentAccount = aPbaPaymentAccount()
-            .pbaNumber(pbas[1])
-            .build();
+                .pbaNumber(pbas[1])
+                .build();
 
         OrganisationCreationRequest organisationCreationRequest = someMinimalOrganisationRequest()
-            .name(organisationName)
-            .pbaAccounts(pbaAccounts)
-            .superUser(aUserCreationRequest()
-                .firstName("some-fname")
-                .lastName("some-lname")
-                .email("someone@somewhere.com")
-                .pbaAccount(superUserPaymentAccount)
-                .build())
-            .build();
+                .name(organisationName)
+                .pbaAccounts(pbaAccounts)
+                .superUser(aUserCreationRequest()
+                        .firstName("some-fname")
+                        .lastName("some-lname")
+                        .email("someone@somewhere.com")
+                        .pbaAccount(superUserPaymentAccount)
+                        .build())
+                .build();
 
         Response response = withAuthenticatedRequest()
-            .body(organisationCreationRequest)
-            .post("/organisations")
-            .andReturn();
+                .body(organisationCreationRequest)
+                .post("/organisations")
+                .andReturn();
+
+        if (response.statusCode() != CREATED.value()) {
+            log.info("Create organisation response: " + response.statusLine());
+        }
 
         response.then()
-            .assertThat()
-            .statusCode(CREATED.value());
+                .assertThat()
+                .statusCode(CREATED.value());
 
         return response.body().as(Map.class);
     }
 
     private RequestSpecification withUnauthenticatedRequest() {
         return RestAssured.given()
-            .relaxedHTTPSValidation()
-            .baseUri(professionalApiUrl)
-            .header("Content-Type", "application/json")
-            .header("Accepts", "application/json");
+                .relaxedHTTPSValidation()
+                .baseUri(professionalApiUrl)
+                .header("Content-Type", "application/json")
+                .header("Accepts", "application/json");
     }
 
     private RequestSpecification withAuthenticatedRequest() {
 
         return withUnauthenticatedRequest()
-            .header(SERVICE_HEADER, "Bearer " + s2sToken);
+                .header(SERVICE_HEADER, "Bearer " + s2sToken);
     }
 
     @SuppressWarnings("unused")
