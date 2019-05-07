b25918dd69d2a968a084aeb6ffe640b181c4ee86
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2019-05-07 14:32:52 +0100

    Fix @Name annotations in entity classes to match the table names in the database

--- src/main/java/uk/gov/hmcts/reform/professionalapi/domain/entities/Organisation.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/domain/entities/Organisation.java
@@ -6,16 +6,24 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import javax.persistence.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@Entity(name = "organisation")
+@Entity(name = "ORGANISATION")
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 @Getter
@@ -70,12 +78,12 @@ public class Organisation {
     private UUID organisationIdentifier;
 
     public Organisation(
-            String name,
-            String status,
-            String sraId,
-            String companyNumber,
-            Boolean sraRegulated,
-            String companyUrl) {
+                        String name,
+                        String status,
+                        String sraId,
+                        String companyNumber,
+                        Boolean sraRegulated,
+                        String companyUrl) {
 
         this.name = name;
         this.status = status;
