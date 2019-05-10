a6ee461188bbb039da21c09049fcc8fdd6f91ca0
KOTLAPRA23 <49564565+KOTLAPRA23@users.noreply.github.com>
2019-05-10 12:37:17 +0100

    RDCC-178-spring-auto-wire refactor completed (#26)
    * RDCC-178-spring-auto-wire refactor completed
    
    * RDCC-178-spring-auto-wire updated functional tests
    
    * RDCC-178-spring-auto-wire no changes, commited to restart build
    
    * Removed autowired and added lombok annotation in controller and removed autowired for member variables in organisationServiceImpl

--- src/main/java/uk/gov/hmcts/reform/professionalapi/persistence/ContactInformationRepository.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/persistence/ContactInformationRepository.java
@@ -4,8 +4,10 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.professionalapi.domain.ContactInformation;
 
+@Repository
 public interface ContactInformationRepository extends JpaRepository<ContactInformation, UUID> {
 
 }
