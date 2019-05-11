527924687c25e68d34da6132c47eabcba00a599c
KOTLAPRA23 <49564565+KOTLAPRA23@users.noreply.github.com>
2019-05-11 11:36:24 +0100

    RDCC-108-164 code changes committed (#30)
    * RDCC-108-164 code changes committed
    
    * RDCC-108-164 integration test case fix part 1
    
    * RDCC-108-164 integration test case fix part 2

--- src/main/java/uk/gov/hmcts/reform/professionalapi/service/OrganisationService.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/service/OrganisationService.java
@@ -2,9 +2,13 @@ package uk.gov.hmcts.reform.professionalapi.service;
 
 import uk.gov.hmcts.reform.professionalapi.controller.request.OrganisationCreationRequest;
 import uk.gov.hmcts.reform.professionalapi.controller.response.OrganisationResponse;
+import uk.gov.hmcts.reform.professionalapi.controller.response.OrganisationsDetailResponse;
 
 public interface OrganisationService {
 
     OrganisationResponse createOrganisationFrom(OrganisationCreationRequest organisationCreationRequest);
 
+    OrganisationsDetailResponse retrieveOrganisations();
+
 }
+
