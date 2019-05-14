f659c5e4415b3a287d66e63ff0801290eaf48c5f
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2019-05-14 16:47:05 +0100

    Specify HTTP method in SearchController for the new endpoint

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/SearchController.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/SearchController.java
@@ -2,6 +2,7 @@ package uk.gov.hmcts.reform.professionalapi.controller;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class SearchController {
         this.professionalUserService = professionalUserService;
     }
 
-    @RequestMapping("/user/{email}")
+    @GetMapping("/user/{email}")
     public ResponseEntity<ProfessionalUserResponse> findUserByEmail(@PathVariable(value = "email") String email) {
         return ResponseEntity
                 .status(200)
