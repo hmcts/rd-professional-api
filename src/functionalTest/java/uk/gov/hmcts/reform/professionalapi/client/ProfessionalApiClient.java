f90ee0320ab0664c5c55befbf46447c9f1bb56f9
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2019-05-15 16:32:48 +0100

    Fix PaymentAccountRetrieveByEmailTest so it doesn't rely on the correct data already being in the database

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/client/ProfessionalApiClient.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/client/ProfessionalApiClient.java
@@ -4,6 +4,7 @@ import static java.util.Arrays.asList;
 import static org.apache.commons.lang.RandomStringUtils.randomAlphabetic;
 import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static uk.gov.hmcts.reform.professionalapi.controller.request.ContactInformationCreationRequest.aContactInformationCreationRequest;
 import static uk.gov.hmcts.reform.professionalapi.controller.request.DxAddressCreationRequest.dxAddressCreationRequest;
 import static uk.gov.hmcts.reform.professionalapi.controller.request.PbaAccountCreationRequest.aPbaPaymentAccount;
@@ -66,43 +67,46 @@ public class ProfessionalApiClient {
                 .asString();
     }
 
-    @SuppressWarnings("unchecked")
     public Map<String, Object> createOrganisation(String organisationName,
                                                   String[] pbas) {
 
         List<PbaAccountCreationRequest> pbaAccounts = asList(aPbaPaymentAccount()
-                                                                     .pbaNumber(pbas[0])
-                                                                     .build(),
+                                                                .pbaNumber(pbas[0])
+                                                                .build(),
                                                              aPbaPaymentAccount()
-                                                                     .pbaNumber(pbas[1])
-                                                                     .build());
+                                                                 .pbaNumber(pbas[1])
+                                                                 .build());
 
-        OrganisationCreationRequest organisationCreationRequest = someMinimalOrganisationRequest()
-                        .name(organisationName)
-                        .sraId(randomAlphabetic(10) + "sra-id-number1")
-                        .sraRegulated(Boolean.FALSE)
-                        .companyUrl(randomAlphabetic(10) + "company-url")
-                        .companyNumber(randomAlphabetic(5) + "com")
-                        .pbaAccounts(pbaAccounts)
-                        .superUser(aUserCreationRequest()
-                                .firstName("some-fname")
-                                .lastName("some-lname")
-                                .email(randomAlphabetic(10) + "@somewhere.com")
-                                .build())
-                        .contactInformation(Arrays.asList(aContactInformationCreationRequest()
-                                .addressLine1("addressLine1")
-                                .addressLine2("addressLine2")
-                                .addressLine3("addressLine3")
-                                .country("some-country")
-                                .county("some-county")
-                                .townCity("some-town-city")
-                                .postCode("some-post-code")
-                                .dxAddress(Arrays.asList(dxAddressCreationRequest()
-                                        .dxNumber("DX 1234567890")
-                                        .dxExchange("dxExchange").build()))
+        return createOrganisation(someMinimalOrganisationRequest()
+                .name(organisationName)
+                .sraId(randomAlphabetic(10) + "sra-id-number1")
+                .sraRegulated(Boolean.FALSE)
+                .companyUrl(randomAlphabetic(10) + "company-url")
+                .companyNumber(randomAlphabetic(5) + "com")
+                .pbaAccounts(pbaAccounts)
+                .superUser(aUserCreationRequest()
+                        .firstName("some-fname")
+                        .lastName("some-lname")
+                        .email(randomAlphabetic(10) + "@somewhere.com")
+                        .build())
+                .contactInformation(Arrays.asList(aContactInformationCreationRequest()
+                        .addressLine1("addressLine1")
+                        .addressLine2("addressLine2")
+                        .addressLine3("addressLine3")
+                        .country("some-country")
+                        .county("some-county")
+                        .townCity("some-town-city")
+                        .postCode("some-post-code")
+                        .dxAddress(Arrays.asList(dxAddressCreationRequest()
+                                .dxNumber("DX 1234567890")
+                                .dxExchange("dxExchange")
                                 .build()))
-                        .build();
+                        .build()))
+                .build());
+    }
 
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> createOrganisation(OrganisationCreationRequest organisationCreationRequest) {
         Response response = withAuthenticatedRequest()
                 .body(organisationCreationRequest)
                 .post("v1/organisations")
@@ -119,6 +123,7 @@ public class ProfessionalApiClient {
         return response.body().as(Map.class);
     }
 
+    @SuppressWarnings("unchecked")
     public Map<String, Object> retrieveOrganisationDetails() {
         Response response = withAuthenticatedRequest()
                 .body("")
@@ -137,6 +142,7 @@ public class ProfessionalApiClient {
 
     }
 
+    @SuppressWarnings("unchecked")
     public Map<String, Object> retrievePaymentAccountsByEmail(String email) {
         Response response = withAuthenticatedRequest()
                 .body("")
@@ -152,7 +158,6 @@ public class ProfessionalApiClient {
                 .statusCode(OK.value());
 
         return response.body().as(Map.class);
-
     }
 
 
@@ -160,8 +165,8 @@ public class ProfessionalApiClient {
         return RestAssured.given()
                 .relaxedHTTPSValidation()
                 .baseUri(professionalApiUrl)
-                .header("Content-Type", "application/json")
-                .header("Accepts", "application/json");
+                .header("Content-Type", APPLICATION_JSON_UTF8_VALUE)
+                .header("Accepts", APPLICATION_JSON_UTF8_VALUE);
     }
 
     private RequestSpecification withAuthenticatedRequest() {
