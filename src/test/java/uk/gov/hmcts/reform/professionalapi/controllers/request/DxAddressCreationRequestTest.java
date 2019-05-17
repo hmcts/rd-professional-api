a78e9852ebe6683468acd72195ff9584d7ad2a9c
Tabitha Cromarty <tabitha.cromarty@hmcts.net>
2019-05-17 12:47:46 +0100

    Refactor to remove DxAddressCreationRequestValidatorImpl entirely

--- src/test/java/uk/gov/hmcts/reform/professionalapi/controllers/request/DxAddressCreationRequestTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/controllers/request/DxAddressCreationRequestTest.java
@@ -2,9 +2,6 @@ package uk.gov.hmcts.reform.professionalapi.controllers.request;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Set;
 
 import javax.validation.ConstraintViolation;
@@ -13,19 +10,11 @@ import javax.validation.Validator;
 
 import org.junit.Test;
 
-import uk.gov.hmcts.reform.professionalapi.controller.request.ContactInformationCreationRequest;
 import uk.gov.hmcts.reform.professionalapi.controller.request.DxAddressCreationRequest;
-import uk.gov.hmcts.reform.professionalapi.controller.request.DxAddressCreationRequestValidatorImpl;
-import uk.gov.hmcts.reform.professionalapi.controller.request.OrganisationCreationRequest;
-import uk.gov.hmcts.reform.professionalapi.controller.request.PbaAccountCreationRequest;
-import uk.gov.hmcts.reform.professionalapi.controller.request.UserCreationRequest;
-import uk.gov.hmcts.reform.professionalapi.domain.OrganisationStatus;
 
 public class DxAddressCreationRequestTest {
 
     private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-
-    private final DxAddressCreationRequestValidatorImpl dxValidator = new DxAddressCreationRequestValidatorImpl();
 
     @Test
     public void has_mandatory_fields_specified_not_null() {
@@ -49,114 +38,42 @@ public class DxAddressCreationRequestTest {
 
     @Test
     public void does_not_create_dx_address_creation_request_when_number_does_not_have_a_space() {
-
-        List<DxAddressCreationRequest> dxAddresses = new ArrayList<>();
         DxAddressCreationRequest dxAddressCreationRequest = new DxAddressCreationRequest("DX12345678901", "some-exchange");
-        dxAddresses.add(dxAddressCreationRequest);
 
-        List<PbaAccountCreationRequest> pbaAccounts = new ArrayList<>();
-        PbaAccountCreationRequest pbaAccountCreationRequests = new PbaAccountCreationRequest("some-pba-number");
-        pbaAccounts.add(pbaAccountCreationRequests);
+        Set<ConstraintViolation<DxAddressCreationRequest>> violations = validator
+                .validate(dxAddressCreationRequest);
 
-        UserCreationRequest superUser = new UserCreationRequest("some-name", "last-name", "some-email");
-
-        List<ContactInformationCreationRequest> contactInformationList = new ArrayList<>();
-        ContactInformationCreationRequest contactInformationCreationRequest = new ContactInformationCreationRequest(
-                "some-address1", "some-address2", "some-address3", "some-town-city", "some-county", "some-country", "some-post-code", dxAddresses);
-        contactInformationList.add(contactInformationCreationRequest);
-
-        OrganisationCreationRequest organisationCreationRequest = new OrganisationCreationRequest(
-                "org-name", OrganisationStatus.PENDING,"some-id", true, "some-number", "some-url", superUser, pbaAccounts, contactInformationList);
-
-        dxValidator.validate(organisationCreationRequest);
-        assertThat(dxAddressCreationRequest.getIsDxRequestValid() == false);
+        assertThat(violations.size()).isEqualTo(1);
 
     }
 
     @Test
     public void does_not_create_dx_address_creation_request_when_number_does_not_start_with_dx_or_ni() {
-
-        List<DxAddressCreationRequest> dxAddresses = new ArrayList<>();
         DxAddressCreationRequest dxAddressCreationRequest = new DxAddressCreationRequest("AB 1345678901", "some-exchange");
-        dxAddresses.add(dxAddressCreationRequest);
 
-        List<PbaAccountCreationRequest> pbaAccounts = new ArrayList<>();
-        PbaAccountCreationRequest pbaAccountCreationRequests = new PbaAccountCreationRequest("some-pba-number");
-        pbaAccounts.add(pbaAccountCreationRequests);
+        Set<ConstraintViolation<DxAddressCreationRequest>> violations = validator
+                .validate(dxAddressCreationRequest);
 
-        UserCreationRequest superUser = new UserCreationRequest("some-name", "last-name", "some-email");
-
-        List<ContactInformationCreationRequest> contactInformationList = new ArrayList<>();
-        ContactInformationCreationRequest contactInformationCreationRequest = new ContactInformationCreationRequest(
-                "some-address1", "some-address2", "some-address3", "some-town-city", "some-county", "some-country", "some-post-code", dxAddresses);
-        contactInformationList.add(contactInformationCreationRequest);
-
-        OrganisationCreationRequest organisationCreationRequest = new OrganisationCreationRequest(
-                "org-name", OrganisationStatus.PENDING, "some-id", true, "some-number", "some-url", superUser, pbaAccounts, contactInformationList);
-
-        dxValidator.validate(organisationCreationRequest);
-        assertThat(dxAddressCreationRequest.getIsDxRequestValid() == false);
+        assertThat(violations.size()).isEqualTo(1);
     }
 
     @Test
     public void does_not_create_dx_address_creation_request_when_number_contains_non_numerical_digits() {
-
-        List<DxAddressCreationRequest> dxAddresses = new ArrayList<>();
         DxAddressCreationRequest dxAddressCreationRequest = new DxAddressCreationRequest("DX 123456789Z", "some-exchange");
-        dxAddresses.add(dxAddressCreationRequest);
 
-        List<PbaAccountCreationRequest> pbaAccounts = new ArrayList<>();
-        PbaAccountCreationRequest pbaAccountCreationRequests = new PbaAccountCreationRequest("some-pba-number");
-        pbaAccounts.add(pbaAccountCreationRequests);
+        Set<ConstraintViolation<DxAddressCreationRequest>> violations = validator
+                .validate(dxAddressCreationRequest);
 
-        UserCreationRequest superUser = new UserCreationRequest("some-name", "last-name", "some-email");
-
-        List<ContactInformationCreationRequest> contactInformationList = new ArrayList<>();
-        ContactInformationCreationRequest contactInformationCreationRequest = new ContactInformationCreationRequest(
-                "some-address1", "some-address2", "some-address3", "some-town-city", "some-county", "some-country", "some-post-code", dxAddresses);
-        contactInformationList.add(contactInformationCreationRequest);
-
-        OrganisationCreationRequest organisationCreationRequest = new OrganisationCreationRequest(
-                "org-name", OrganisationStatus.PENDING, "some-id", true, "some-number", "some-url", superUser, pbaAccounts, contactInformationList);
-
-        dxValidator.validate(organisationCreationRequest);
-        assertThat(dxAddressCreationRequest.getIsDxRequestValid() == false);
+        assertThat(violations.size()).isEqualTo(1);
     }
 
     @Test
     public void does_not_create_dx_address_creation_request_when_number_does_not_have_10_digits() {
-
-        List<DxAddressCreationRequest> dxAddresses = new ArrayList<>();
         DxAddressCreationRequest dxAddressCreationRequest = new DxAddressCreationRequest("DX 123456789", "some-exchange");
-        dxAddresses.add(dxAddressCreationRequest);
 
-        List<PbaAccountCreationRequest> pbaAccounts = new ArrayList<>();
-        PbaAccountCreationRequest pbaAccountCreationRequests = new PbaAccountCreationRequest("some-pba-number");
-        pbaAccounts.add(pbaAccountCreationRequests);
+        Set<ConstraintViolation<DxAddressCreationRequest>> violations = validator
+                .validate(dxAddressCreationRequest);
 
-        UserCreationRequest superUser = new UserCreationRequest("some-name", "last-name", "some-email");
-
-        List<ContactInformationCreationRequest> contactInformationList = new ArrayList<>();
-        ContactInformationCreationRequest contactInformationCreationRequest = new ContactInformationCreationRequest(
-                "some-address1", "some-address2", "some-address3", "some-town-city", "some-county", "some-country", "some-post-code", dxAddresses);
-        contactInformationList.add(contactInformationCreationRequest);
-
-        OrganisationCreationRequest organisationCreationRequest = new OrganisationCreationRequest(
-                "org-name", OrganisationStatus.PENDING, "some-id", true, "some-number", "some-url", superUser, pbaAccounts, contactInformationList);
-
-        dxValidator.validate(organisationCreationRequest);
-        assertThat(dxAddressCreationRequest.getIsDxRequestValid() == false);
-    }
-
-    @Test
-    public void does_not_throw_exception_if_no_contact_information_in_request() {
-        OrganisationCreationRequest request = new OrganisationCreationRequest(null, null, null, null, null, null, null, null, null);
-        dxValidator.validate(request);
-    }
-
-    @Test
-    public void does_not_throw_exception_if_no_dx_address_in_contact_information_in_request() {
-        OrganisationCreationRequest request = new OrganisationCreationRequest(null, null, null, null, null, null, null, null, Arrays.asList(new ContactInformationCreationRequest(null, null, null, null, null, null, null, null)));
-        dxValidator.validate(request);
+        assertThat(violations.size()).isEqualTo(1);
     }
 }
