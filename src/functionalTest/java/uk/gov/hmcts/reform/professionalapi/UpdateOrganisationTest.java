d535fd7a6b87fc88d23e53afb27ffbec085aaecf
satya <1426759+satyachundur@users.noreply.github.com>
2019-05-21 14:40:05 +0100

    changing runner to serenity to have better reporting  (#45)
    * changing runner to serenity
    
    * RDCC-176 tactical fix for flaky functional test

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/UpdateOrganisationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/UpdateOrganisationTest.java
@@ -3,13 +3,12 @@ package uk.gov.hmcts.reform.professionalapi;
 import static org.assertj.core.api.Java6Assertions.assertThat;
 
 import java.util.Map;
-
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringIntegrationSerenityRunner.class)
 @ActiveProfiles("functional")
 public class UpdateOrganisationTest extends FunctionalTestSuite {
 
