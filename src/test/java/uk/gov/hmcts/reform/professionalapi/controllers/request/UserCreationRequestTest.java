04c45b14ae676e666ad74175b8de0df740e9d60b
Akio Cox <akio.cox@hmcts.net>
2019-05-22 13:45:46 +0100

    RDCC-210 adding unit test coverage to request objects

--- src/test/java/uk/gov/hmcts/reform/professionalapi/controllers/request/UserCreationRequestTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/controllers/request/UserCreationRequestTest.java
@@ -6,12 +6,24 @@ import java.util.Set;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
+
+import org.junit.Before;
 import org.junit.Test;
 import uk.gov.hmcts.reform.professionalapi.controller.request.UserCreationRequest;
 
 public class UserCreationRequestTest {
 
     private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+    private UserCreationRequest userCreationRequest;
+
+    private String firstName = "Joe";
+    private String lastName = "Bloggs";
+    private String email = "joe.bloggs@email.com";
+
+    @Before
+    public void setUp() {
+        userCreationRequest = new UserCreationRequest(firstName, lastName, email);
+    }
 
     @Test
     public void has_mandatory_fields_specified_not_null() {
@@ -24,5 +36,26 @@ public class UserCreationRequestTest {
 
         assertThat(violations.size()).isEqualTo(3);
     }
-    
+
+    @Test
+    public void testThatUserCreationIsChangedByBuilder() {
+        String firstName2 = "Jane";
+        String lastName2 = "Doe";
+        String email2 = "jane.doe@email.com";
+
+        UserCreationRequest testUserCreationRequest = UserCreationRequest.aUserCreationRequest()
+                .firstName(firstName2)
+                .lastName(lastName2)
+                .email(email2)
+                .build();
+
+        assertThat(testUserCreationRequest.getFirstName()).isEqualTo(firstName2);
+        assertThat(testUserCreationRequest.getLastName()).isEqualTo(lastName2);
+        assertThat(testUserCreationRequest.getEmail()).isEqualTo(email2);
+        System.out.println(testUserCreationRequest.getFirstName());
+        System.out.println(testUserCreationRequest.getLastName());
+        System.out.println(testUserCreationRequest.getEmail());
+
+    }
+
 }
