ae1565bbea7fb1a3f342094bfa820969f08494f1
Lomte <429962@cognizant.com>
2019-05-25 14:54:57 +0100

    RDCC-224 Added comments for class responsibility

--- src/main/java/uk/gov/hmcts/reform/professionalapi/generator/ProfessionalApiGenerator.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/generator/ProfessionalApiGenerator.java
@@ -2,6 +2,10 @@ package uk.gov.hmcts.reform.professionalapi.generator;
 
 import java.util.UUID;
 
+/**
+ * This interface is responsible for generating unique UUID
+ */
+
 public interface ProfessionalApiGenerator {
 
     int LENGTH_OF_UUID = 36;
