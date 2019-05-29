70b7d194e7434ae2ebca93f6068bbc64e847ab6d
KOTLAPRA23 <49564565+KOTLAPRA23@users.noreply.github.com>
2019-05-29 09:17:28 +0100

    RDCC-209 Search Legacy PBA by Email related code changes committed (#54)
    * RDCC-209 Code changes committed
    
    * RDCC-209 CodeReview  changes committed
    
    * RDCC-209 changes committed to fix issue while retrieve payments account details for user

--- src/test/java/uk/gov/hmcts/reform/professionalapi/entities/OrganisationTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/entities/OrganisationTest.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+
 import org.junit.Test;
 import uk.gov.hmcts.reform.professionalapi.domain.*;
 
