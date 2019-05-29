8ba638488ae11bf9fa08ac8e817b3b9bc5b13f38
KOTLAPRA23 <49564565+KOTLAPRA23@users.noreply.github.com>
2019-05-29 09:17:28 +0100

    RDCC-209 Search Legacy PBA by Email related code changes committed (#54)
    * RDCC-209 Code changes committed
    
    * RDCC-209 CodeReview  changes committed
    
    * RDCC-209 changes committed to fix issue while retrieve payments account details for user

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/OrganisationController.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/OrganisationController.java
@@ -12,6 +12,7 @@ import java.util.UUID;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.xml.ws.http.HTTPException;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -42,6 +43,7 @@ import uk.gov.hmcts.reform.professionalapi.controller.response.ProfessionalUserR
 import uk.gov.hmcts.reform.professionalapi.domain.Organisation;
 import uk.gov.hmcts.reform.professionalapi.domain.OrganisationStatus;
 import uk.gov.hmcts.reform.professionalapi.domain.PrdEnum;
+import uk.gov.hmcts.reform.professionalapi.domain.ProfessionalUser;
 import uk.gov.hmcts.reform.professionalapi.service.ProfessionalUserService;
 import uk.gov.hmcts.reform.professionalapi.service.impl.OrganisationServiceImpl;
 import uk.gov.hmcts.reform.professionalapi.service.impl.PaymentAccountServiceImpl;
@@ -171,6 +173,11 @@ public class OrganisationController {
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE
     )
     public ResponseEntity<ProfessionalUserResponse> findUserByEmail(@RequestParam(value = "email") String email) {
+
+        ProfessionalUser user = professionalUserService.findProfessionalUserByEmailAddress(email);
+        if (null == user) {
+            throw new HTTPException(404);
+        }
         return ResponseEntity
                 .status(200)
                 .body(new ProfessionalUserResponse(professionalUserService.findProfessionalUserByEmailAddress(email)));
