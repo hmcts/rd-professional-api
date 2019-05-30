643af57e85cf4583ae2f0e3b23944303542669ee
Oozeerally <778425@cognizant.com>
2019-05-30 16:17:24 +0100

    RDCC-208 fixed checkstyle error

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/response/ProfessionalUserResponse.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/response/ProfessionalUserResponse.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 import org.springframework.util.StringUtils;
 import uk.gov.hmcts.reform.professionalapi.domain.ProfessionalUser;
