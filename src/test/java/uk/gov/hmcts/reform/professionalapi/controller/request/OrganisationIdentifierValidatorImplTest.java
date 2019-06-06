4750a7e4c76dceec52228064304c56b12fc63e0b
Reem Ibrahim <reem.ibrahim@hmcts.net>
2019-06-06 16:17:21 +0100

    Added couple more test classes

---
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/controller/request/OrganisationIdentifierValidatorImplTest.java
@@ -0,0 +1,39 @@
+package uk.gov.hmcts.reform.professionalapi.controller.request;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.springframework.dao.EmptyResultDataAccessException;
+import uk.gov.hmcts.reform.professionalapi.domain.Organisation;
+import uk.gov.hmcts.reform.professionalapi.domain.OrganisationStatus;
+
+@RunWith(value = BlockJUnit4ClassRunner.class)
+public class OrganisationIdentifierValidatorImplTest {
+
+    OrganisationIdentifierValidatorImpl organisationIdentifierValidatorImpl = new OrganisationIdentifierValidatorImpl();
+
+    @Test
+    public void validateTest() {
+        Organisation dummyOrganisation = new Organisation(
+                "dummyName",
+                OrganisationStatus.ACTIVE,
+                "sraId",
+                "12345678",
+                Boolean.FALSE,
+                "dummySite.com");
+
+        organisationIdentifierValidatorImpl.validate(dummyOrganisation, dummyOrganisation.getStatus(), dummyOrganisation.getOrganisationIdentifier());
+        try {
+            organisationIdentifierValidatorImpl.validate(dummyOrganisation, OrganisationStatus.ACTIVE, dummyOrganisation.getOrganisationIdentifier());
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test(expected = EmptyResultDataAccessException.class)
+    public void shouldCheckOrganisationDoesNotExist() {
+        organisationIdentifierValidatorImpl.validate(null, null, null);
+    }
+}
