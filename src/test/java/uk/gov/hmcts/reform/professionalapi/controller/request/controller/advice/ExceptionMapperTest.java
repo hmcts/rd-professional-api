b898334116095af43c911dca4c80e6a98bb5a57b
Adil Cassam Oozeerally <49557838+AdilOoze@users.noreply.github.com>
2019-06-11 12:07:22 +0100

    Rdcc 173 - updated error handling (now with responses) (#66)
    * RDCC-173 added error responses onto ExceptionMapper class, adjusted tests to reflect new error response
    
    * RDCC-173 updated exception mapper to properly pass tests
    
    * RDCC-173 fixed checkstyle
    
    * RDCC-173 fixing sonar
    
    * RDCC-173 fixing sonar
    
    * RDCC-173 fixing sonar
    
    * RDCC-173 sonar coverage up
    
    * RDCC-173 updated ExceptionMapper to create response for all other exceptions
    
    * RDCC-173 changed String to Object, fixed failing test

--- src/test/java/uk/gov/hmcts/reform/professionalapi/controller/request/controller/advice/ExceptionMapperTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/controller/request/controller/advice/ExceptionMapperTest.java
@@ -3,22 +3,18 @@ package uk.gov.hmcts.reform.professionalapi.controller.request.controller.advice
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 
-import javax.servlet.http.HttpServletRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import uk.gov.hmcts.reform.professionalapi.controller.advice.ExceptionMapper;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ExceptionMapperTest {
-
-    @Mock
-    private HttpServletRequest httpServletRequest;
 
     @InjectMocks
     private ExceptionMapper exceptionMapper;
@@ -28,10 +24,46 @@ public class ExceptionMapperTest {
 
         EmptyResultDataAccessException emptyResultDataAccessException = mock(EmptyResultDataAccessException.class);
 
-        ResponseEntity<String> responseEntity =
-            exceptionMapper.handleEmptyResultDataAccessException(httpServletRequest, emptyResultDataAccessException);
+        ResponseEntity<Object> responseEntity =
+                exceptionMapper.handleEmptyResultDataAccessException(emptyResultDataAccessException);
 
         assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
+
+    }
+
+    @Test
+    public void should_handle_illegal_argument_exception() {
+
+        IllegalArgumentException exception = mock(IllegalArgumentException.class);
+
+        ResponseEntity<Object> responseEntity =
+                exceptionMapper.handleIllegalArgumentException(exception);
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+
+    }
+
+    @Test
+    public void should_handle_http_message_not_readable_exception() {
+
+        HttpMessageNotReadableException exception = mock(HttpMessageNotReadableException.class);
+
+        ResponseEntity<Object> responseEntity =
+                exceptionMapper.httpMessageNotReadableExceptionError(exception);
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+
+    }
+
+    @Test
+    public void should_handle_http_media_type_not_supported_exception() {
+
+        IllegalArgumentException exception = mock(IllegalArgumentException.class);
+
+        ResponseEntity<Object> responseEntity =
+                exceptionMapper.handleHttpMediaTypeNotSupported(exception);
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
 
     }
 
@@ -40,7 +72,7 @@ public class ExceptionMapperTest {
 
         Exception exception = mock(Exception.class);
 
-        ResponseEntity<String> responseEntity = exceptionMapper.handleException(httpServletRequest, exception);
+        ResponseEntity<Object> responseEntity = exceptionMapper.handleException(exception);
 
         assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, responseEntity.getStatusCode());
 
