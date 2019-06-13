bf8c3e2c2f70ae25c7f3a87b6ab8209e9660b79d
Akio Cox <akio.cox@hmcts.net>
2019-06-13 22:03:16 +0100

    changing Flyway strategy due to indexes

--- src/main/resources/db/migration/V5__drop_unique_constraints_add_org_manager.sql
+++ src/main/resources/db/migration/V5__drop_unique_constraints_add_org_manager.sql
@@ -1,5 +1,9 @@
-ALTER TABLE organisation DROP CONSTRAINT company_url_uq1;
+ALTER TABLE organisation DROP COLUMN company_url;
 
-ALTER TABLE organisation DROP CONSTRAINT sra_id_uq1;
+ALTER TABLE organisation DROP COLUMN sra_id;
 
-INSERT INTO PRD_ENUM (ENUM_CODE, ENUM_NAME, ENUM_TYPE, ENUM_DESC) VALUES (4, 'ORGANISATION_ADMIN', 'PRD_ROLE', 'Identifying the first user of an organisation');
+ALTER TABLE organisation ADD COLUMN company_url varchar(512);
+
+ALTER TABLE organisation ADD COLUMN sra_id varchar(255);
+
+INSERT INTO PRD_ENUM (ENUM_CODE, ENUM_NAME, ENUM_TYPE, ENUM_DESC) VALUES (4, 'ORGANISATION_ADMIN', 'PRD_ROLE', 'Identifies the first user of an organisation');
