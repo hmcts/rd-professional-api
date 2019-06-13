fe4623970e2f6c96da47bf1dfd5973b9da9c4a1d
Akio Cox <akio.cox@hmcts.net>
2019-06-13 16:10:49 +0100

    RDCC-178 Correcting check style test

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/advice/ExceptionMapper.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/advice/ExceptionMapper.java
@@ -16,7 +16,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
