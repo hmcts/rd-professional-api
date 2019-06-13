3629ad4990b772ee5d6fb8ec1204913c6c5e7f3a
Akio Cox <akio.cox@hmcts.net>
2019-06-13 18:45:41 +0100

    Fix for travis

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/OrganisationController.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/OrganisationController.java
@@ -12,7 +12,6 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
-
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
