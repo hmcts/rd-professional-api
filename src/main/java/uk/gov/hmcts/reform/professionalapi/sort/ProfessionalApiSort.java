9c7d3cfb65266522612f72dec75e6b3e3c384dea
Oozeerally <778425@cognizant.com>
2019-07-05 12:48:07 +0100

    RDCC-305 ApiSort methods added, not tested

--- src/main/java/uk/gov/hmcts/reform/professionalapi/sort/ProfessionalApiSort.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/sort/ProfessionalApiSort.java
@@ -1,5 +1,9 @@
 package uk.gov.hmcts.reform.professionalapi.sort;
 
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.nullsFirst;
+
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,5 +19,37 @@ public interface ProfessionalApiSort {
      */
     static List<ProfessionalUser> sortUserListByCreatedDate(Organisation organisation) {
         return organisation.getUsers().stream().sorted((Comparator.comparing(ProfessionalUser::getCreated))).collect(Collectors.toList());
+    }
+
+    static List<ProfessionalUser> sortUsers(Organisation organisation) {
+        List<ProfessionalUser> userList = organisation.getUsers();
+
+        sortUserListerByDeletedTimestamp(userList);
+        return sortUserListByOrgAdmin(userList);
+    }
+
+    static List<ProfessionalUser> sortUserListerByDeletedTimestamp(List<ProfessionalUser> userList) {
+        userList.sort(nullsFirst(comparing(ProfessionalUser::getDeleted)));
+        return userList;
+    }
+
+    static List<ProfessionalUser> sortUserListByOrgAdmin(List<ProfessionalUser> userList) {
+        List<ProfessionalUser> orgAdminSortedList = new ArrayList<>();
+
+        userList.forEach(user -> {
+            user.getUserAttributes().forEach(userAttribute -> {
+                if(userAttribute.getPrdEnum().getEnumName() == "ORGANISATION_ADMIN") {
+                    orgAdminSortedList.add(user);
+                }
+            });
+        });
+
+        userList.forEach(user -> {
+            if (!orgAdminSortedList.contains(user)) {
+                orgAdminSortedList.add(user);
+            }
+        });
+
+        return orgAdminSortedList;
     }
 }
