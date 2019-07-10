5b7d3c85e2fb46853242cf97cf25b220c79326d5
shreedhar-hmcts <shreedhar.lomte@hmcts.net>
2019-07-10 15:29:09 +0100

    RDCC-273 adding scopes while authenticating users

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/idam/IdamService.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/idam/IdamService.java
@@ -94,7 +94,7 @@ public class IdamService {
                 BASIC + base64Authorisation,
                 CODE,
                 testConfig.getOauth2().getClientId(),
-                testConfig.getOauth2().getRedirectUrl());
+                testConfig.getOauth2().getRedirectUrl(),"openid profile roles create-user manage-user search-user");
 
         log.info("authenticateUserResponse::" + authenticateUserResponse);
         log.info("authenticateUserResponse::Code::" + authenticateUserResponse.getCode());
