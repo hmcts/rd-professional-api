59da07b3ff31fdf0e7fd21b6c209abd620c3c654
Oozeerally <778425@cognizant.com>
2019-07-10 16:56:10 +0100

    RDCC-364 updated ProffessionalExternalUserControllerTest

--- src/test/java/uk/gov/hmcts/reform/professionalapi/controller/ProfessionalExternalUserControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/controller/ProfessionalExternalUserControllerTest.java
@@ -1,37 +1,29 @@
 package uk.gov.hmcts.reform.professionalapi.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import uk.gov.hmcts.reform.professionalapi.controller.external.OrganisationExternalController;
 import uk.gov.hmcts.reform.professionalapi.controller.external.ProfessionalExternalUserController;
-import uk.gov.hmcts.reform.professionalapi.controller.internal.OrganisationInternalController;
-import uk.gov.hmcts.reform.professionalapi.controller.request.OrganisationCreationRequest;
 import uk.gov.hmcts.reform.professionalapi.controller.request.OrganisationCreationRequestValidator;
 import uk.gov.hmcts.reform.professionalapi.controller.request.OrganisationIdentifierValidatorImpl;
 import uk.gov.hmcts.reform.professionalapi.controller.request.ProfessionalUserReqValidator;
-import uk.gov.hmcts.reform.professionalapi.controller.request.UpdateOrganisationRequestValidator;
-import uk.gov.hmcts.reform.professionalapi.controller.response.OrganisationResponse;
 import uk.gov.hmcts.reform.professionalapi.domain.Organisation;
 import uk.gov.hmcts.reform.professionalapi.domain.OrganisationStatus;
 import uk.gov.hmcts.reform.professionalapi.domain.ProfessionalUser;
 import uk.gov.hmcts.reform.professionalapi.service.OrganisationService;
-import uk.gov.hmcts.reform.professionalapi.service.PaymentAccountService;
 import uk.gov.hmcts.reform.professionalapi.service.ProfessionalUserService;
-import uk.gov.hmcts.reform.professionalapi.service.UserAttributeService;
-import uk.gov.hmcts.reform.professionalapi.service.impl.PrdEnumServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class ProfessionalExternalUserControllerTest {
 
@@ -40,6 +32,7 @@ public class ProfessionalExternalUserControllerTest {
     private Organisation organisation;
     private ProfessionalUserReqValidator profExtUsrReqValidator;
     private OrganisationIdentifierValidatorImpl organisationIdentifierValidatorImpl;
+    private OrganisationCreationRequestValidator organisationCreationRequestValidator;
 
     @InjectMocks
     private ProfessionalExternalUserController professionalExternalUserController;
@@ -51,6 +44,7 @@ public class ProfessionalExternalUserControllerTest {
         professionalUserServiceMock = mock(ProfessionalUserService.class);
         organisationIdentifierValidatorImpl = mock(OrganisationIdentifierValidatorImpl.class);
         profExtUsrReqValidator = mock(ProfessionalUserReqValidator.class);
+        organisationCreationRequestValidator = mock(OrganisationCreationRequestValidator.class);
         MockitoAnnotations.initMocks(this);
     }
 
@@ -71,6 +65,7 @@ public class ProfessionalExternalUserControllerTest {
 
         doNothing().when(profExtUsrReqValidator).validateRequest(any(String.class), any(String.class), any(String.class));
         doNothing().when(organisationIdentifierValidatorImpl).validate(any(Organisation.class), any(OrganisationStatus.class), any(String.class));
+        doNothing().when(organisationCreationRequestValidator).validateOrganisationIdentifier(any(String.class));
 
         ResponseEntity<?> actual = professionalExternalUserController.findUsersByOrganisation(organisation.getOrganisationIdentifier(), "false", "");
         assertThat(actual).isNotNull();
