b5a30d360a353aa28e92ad88952cfb929426c7d7
Kotla <182588@cognizant.com>
2019-07-15 19:05:29 +0100

    RDCC-326:owsp version updated to fix build issues

--- src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/FeignConfiguration.java
@@ -36,15 +36,6 @@ public class FeignConfiguration {
             if (attrs != null) {
                 HttpServletRequest request = attrs.getRequest();
                 // If you take the following method in the cookie
-                Cookie[] cookies = request.getCookies();
-                if (cookies != null && cookies.length > 0) {
-                    for (Cookie cookie : cookies) {
-                        requestTemplate.header(cookie.getName(), cookie.getValue());
-                    }
-                } else {
-                    log.warn("FeignHeadConfiguration", "Failed to get a cookie!");
-                }
-
                 Enumeration<String> headerNames = request.getHeaderNames();
                 if (headerNames != null) {
                     while (headerNames.hasMoreElements()) {
