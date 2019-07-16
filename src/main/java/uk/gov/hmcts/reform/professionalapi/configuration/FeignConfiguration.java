b030fecdbc7487b3dc9e0396e419165432c597e1
Kotla <182588@cognizant.com>
2019-07-16 09:51:55 +0100

    RDCC-326:To fix build issues

--- src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/FeignConfiguration.java
@@ -30,14 +30,21 @@ public class FeignConfiguration {
         return new SpringFormEncoder(new SpringEncoder(messageConverters));
     }
 
-    @Bean
     public RequestInterceptor requestInterceptor() {
         return requestTemplate -> {
             ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
             if (attrs != null) {
                 HttpServletRequest request = attrs.getRequest();
                 // If you take the following method in the cookie
-               
+                Cookie[] cookies = request.getCookies();
+                if (cookies != null && cookies.length > 0) {
+                    for (Cookie cookie : cookies) {
+                        requestTemplate.header(cookie.getName(), cookie.getValue());
+                    }
+                } else {
+                    log.warn("FeignHeadConfiguration", "Failed to get a cookie!");
+                }
+
                 Enumeration<String> headerNames = request.getHeaderNames();
                 if (headerNames != null) {
                     while (headerNames.hasMoreElements()) {
