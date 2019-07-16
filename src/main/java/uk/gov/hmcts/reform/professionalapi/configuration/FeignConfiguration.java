b243e3d586e7a20f97b69174a0d913462378a073
Kotla <182588@cognizant.com>
2019-07-16 10:35:02 +0100

    RDCC-326:To fix build issues

--- src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/FeignConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/FeignConfiguration.java
@@ -5,6 +5,7 @@ import feign.codec.Encoder;
 import feign.form.spring.SpringFormEncoder;
 
 import java.util.Enumeration;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import lombok.extern.slf4j.Slf4j;
@@ -35,6 +36,15 @@ public class FeignConfiguration {
             if (attrs != null) {
                 HttpServletRequest request = attrs.getRequest();
                 // If you take the following method in the cookie
+                // If you take the following method in the cookie
+                Cookie[] cookies = request.getCookies();
+                if (cookies != null && cookies.length > 0) {
+                    for (Cookie cookie : cookies) {
+                        requestTemplate.header(cookie.getName(), cookie.getValue());
+                    }
+                } else {
+                    log.warn("FeignHeadConfiguration", "Failed to get a cookie!");
+                }
                 Enumeration<String> headerNames = request.getHeaderNames();
                 if (headerNames != null) {
                     while (headerNames.hasMoreElements()) {
