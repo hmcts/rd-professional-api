6d78a70a75b9056868d9804ed0f394562e2ed6c9
Mark Nuthalapati <mark.nuthalapati@gmail.com>
2019-07-17 15:30:18 +0100

    Removed Resource group tag

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -20,12 +20,6 @@ locals {
   USER_PROFILE_URL = "http://rd-user-profile-api-${local.local_env}.service.core-compute-${local.local_env}.internal"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name = "${var.product}-${var.component}-${var.env}"
-  location = "${var.location}"
-  tags = "${merge(var.common_tags, map("lastUpdated", "${timestamp()}"))}"
-}
-
 data "azurerm_key_vault" "rd_key_vault" {
   name = "${local.key_vault_name}"
   resource_group_name = "${local.key_vault_name}"
