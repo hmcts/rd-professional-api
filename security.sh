8c928e1110cb4ee3483839a86a5f54ef63e2e7d5
Mark Nuthalapati <mark.nuthalapati@gmail.com>
2019-07-24 15:36:23 +0100

    Attempt to fix security scan with suggestions from satya

--- security.sh
+++ security.sh
@@ -3,5 +3,4 @@ echo ${TEST_URL}
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -u ${SecurityRules} -P 1001 -l FAIL
 cat zap.out
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
-cp /zap/api-report.html build/reports/
-zap-cli -p 1001 alerts -l High
+cp /zap/api-report.html functional-output/
