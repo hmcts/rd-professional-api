8f9e967b297eb3d3c3053c57393511066150f7e4
Oozeerally <778425@cognizant.com>
2019-07-29 13:00:44 +0100

    RDCC-275 fixing checkstyle

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/response/ProfessionalUsersResponse.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/response/ProfessionalUsersResponse.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.professionalapi.controller.response;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
