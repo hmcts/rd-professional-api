0251d451f3bd8b15453acd361c70717ceb38857b
KOTLAPRA23 <prashanth.kotla@hmcts.net>
2019-08-14 16:23:06 +0100

    fix-build-issue:New roles added to CCD

---
+++ src/main/resources/db/migration/V6_6__updating_jurisdiction_enum.sql
@@ -0,0 +1,2 @@
+INSERT INTO PRD_ENUM (ENUM_CODE, ENUM_NAME, ENUM_TYPE, ENUM_DESC) VALUES (29, 'caseworker-probate', 'CCD_ROLE', 'caseworker probate');
+INSERT INTO PRD_ENUM (ENUM_CODE, ENUM_NAME, ENUM_TYPE, ENUM_DESC) VALUES (30, 'caseworker-probate-solicitor', 'CCD_ROLE', 'caseworker probate solicitor');
