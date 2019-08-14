02852d93521bd1b5564e06445bed0ff96b0d5bde
KOTLAPRA23 <49564565+KOTLAPRA23@users.noreply.github.com>
2019-08-14 11:55:49 +0100

    RDCC-prd-roles-fix:new roles added (#165)
    * RDCC-prd-roles-fix:new roles added
    
    * RDCC-prd-roles-fix:new roles added
    
    * RDCC-prd-roles-fix:new roles added
    
    * RDCC-prd-roles-fix:new roles added

---
+++ src/main/resources/db/migration/V6_6__adding_ccd_roles_enum.sql
@@ -0,0 +1,3 @@
+--RDCC-392 CCD roles
+INSERT INTO PRD_ENUM (ENUM_CODE, ENUM_NAME, ENUM_TYPE, ENUM_DESC) VALUES (29, 'caseworker-probate', 'CCD_ROLE', 'caseworker probate');
+INSERT INTO PRD_ENUM (ENUM_CODE, ENUM_NAME, ENUM_TYPE, ENUM_DESC) VALUES (30, 'caseworker-probate-solicitor', 'CCD_ROLE', 'caseworker probate solicitor');
