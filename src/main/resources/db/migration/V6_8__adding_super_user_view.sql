937a14e37dc52b3f86d2a0af428b7117aebedb7d
KOTLAPRA23 <prashanth.kotla@hmcts.net>
2019-08-24 17:58:49 +0100

    preview: Code committed for superuser validation

---
+++ src/main/resources/db/migration/V6_8__adding_super_user_view.sql
@@ -0,0 +1,2 @@
+create view super_user_view as select * from professional_user where id in (select professional_user_id from user_attribute where prd_enum_code = 4 AND prd_enum_type like 'ADMIN_ROLE');
+create trigger t_super_user_view instead of insert, update, delete on super_user_view for each row call "uk.gov.hmcts.reform.professionalapi.domain.SuperUser";
