cd4f825e4f8ea92c9fb14de0321f48f65d41b7f8
KOTLAPRA23 <prashanth.kotla@hmcts.net>
2019-09-27 12:10:09 +0100

    RDCC-453: added UP demo url in application.yaml file

--- bin/run-in-docker.sh
+++ bin/run-in-docker.sh
@@ -5,7 +5,7 @@ GRADLE_INSTALL=true
 
 # Test S2S key - not used in any HMCTS key vaults or services
 export S2S_SECRET=SZDUA3L7N32PE2IS
-export S2S_MICROSERVICE=rd_judicial_api
+export S2S_MICROSERVICE=rd_professional_api
 
 build_s2s_image() {
     git clone https://github.com/hmcts/s2s-test-tool.git
