e11379e41be5813f2fd8b6ddb17bd8f04ba7a728
rosemaryscodebaby <rosemaryscodebaby@gmail.com>
2019-10-09 16:32:43 +0100

    RDCC-449 syncing branch

--- src/test/java/uk/gov/hmcts/reform/professionalapi/controller/ErrorConstantsTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/controller/ErrorConstantsTest.java
@@ -10,11 +10,11 @@ public class ErrorConstantsTest {
     @Test
     public void check_constants() {
 
-        assertThat(ErrorConstants.MALFORMED_JSON.equals("1 : Malformed Input Request"));
+        assertThat(ErrorConstants.MALFORMED_JSON.getErrorMessage()).isEqualTo("1 : Malformed Input Request");
 
-        assertThat(ErrorConstants.UNSUPPORTED_MEDIA_TYPES.equals("2 : Unsupported Media Type"));
+        assertThat(ErrorConstants.UNSUPPORTED_MEDIA_TYPES.getErrorMessage()).isEqualTo("2 : Unsupported Media Type");
 
-        assertThat(ErrorConstants.INVALID_REQUEST.equals("There is a problem with your request. Please check and try again"));
+        assertThat(ErrorConstants.INVALID_REQUEST.getErrorMessage()).isEqualTo("There is a problem with your request. Please check and try again");
 
     }
 
