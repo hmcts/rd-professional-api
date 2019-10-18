1db0a0b3b492dfe7a248502e846217fc48837f1a
jamesohmcts <51128048+jamesohmcts@users.noreply.github.com>
2019-10-18 17:01:28 +0100

    Origin/rdcc 529 (#263)
    * RDCC-529 updated ErrorConstantsTest For QA PRD queries
    
    * Failing test OrganisationServImplTest
    
    * "origin/RDCC-529, adding to unit test - still failing, but progress made"
    
    * RDCC-529 semi-fixed UserAttributeServiceTest
    
    * RDCC-529 Fixed failling test w/Akio
    
    * origin/rdcc-529 style check clean up
    
    * RDCC-529 reverted codebase test now passing
    
    * Updated OrganisationServiceImpTest
    
    * Merge branch 'origin/rdcc-529' of C:\Users\764106\RDCC-529\rd-professional-api with conflicts.
    
    * RDCC-529 sonarcloud major changes fixed
    
    * RDCC-529 updated Minor issues in sonarCloud & Akio comment
    
    * RDCC-529 fixed integration return emptyRoles.size() > 0 ? tru
    
    * RDCC-529 Fixed integration and legacyPBARes code
    
    * RDCC-529 fixed failling integration test
    
    * RDCC-529 fixed failing integration test

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/ProfessionalUserReqValidator.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/ProfessionalUserReqValidator.java
@@ -20,6 +20,7 @@ import uk.gov.hmcts.reform.professionalapi.domain.RoleName;
 @Slf4j
 public class ProfessionalUserReqValidator {
 
+
     public static boolean isValidEmail(String email) {
         if (!StringUtils.isEmpty(email)) {
             Pattern p = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
@@ -40,7 +41,7 @@ public class ProfessionalUserReqValidator {
         }
     }
 
-    public static void validateUserStatus(String status) {
+    private static void validateUserStatus(String status) {
         boolean valid = false;
 
         for (IdamStatus idamStatus : IdamStatus.values()) {
@@ -70,14 +71,14 @@ public class ProfessionalUserReqValidator {
         }
     }
 
-    public boolean invalidRoleName(Set<RoleName> roleNames) {
+    private boolean invalidRoleName(Set<RoleName> roleNames) {
 
         List<RoleName> emptyRoles = new ArrayList<>();
         if (!CollectionUtils.isEmpty(roleNames)) {
             emptyRoles = roleNames.stream().filter(roleName -> StringUtils.isBlank(roleName.getName())).collect(Collectors.toList());
 
         }
-        return emptyRoles.size() > 0 ? true : false;
+        return emptyRoles.size() > 0;
     }
 
 
