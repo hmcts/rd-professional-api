34a837951cd6ed094cb8c51a79e5e49c57b6e6e4
AdilOoze <adil.oozeerally@hmcts.net>
2019-10-23 16:56:15 +0100

    RDCC-529 adding unit test coverage for Professional User Req Validator

--- src/test/java/uk/gov/hmcts/reform/professionalapi/controller/request/ProfessionalUserReqValidatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/controller/request/ProfessionalUserReqValidatorTest.java
@@ -1,20 +1,57 @@
 package uk.gov.hmcts.reform.professionalapi.controller.request;
 
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.mockito.Mockito.mock;
+
 import org.junit.Test;
+import uk.gov.hmcts.reform.professionalapi.domain.ModifyUserProfileData;
 
 public class ProfessionalUserReqValidatorTest {
 
     ProfessionalUserReqValidator profUserReqValidator = new ProfessionalUserReqValidator();
+    ModifyUserProfileData modifyUserProfileDataMock = mock(ModifyUserProfileData.class);
 
     @Test(expected = InvalidRequest.class)
     public void testValidateRequestAllNull() {
         profUserReqValidator
-                .validateRequest(null,null, null);
+                .validateRequest(null, null, null);
     }
 
     @Test
     public void testValidateRequestNoneNull() {
         profUserReqValidator
-                .validateRequest("ordId","true", "");
+                .validateRequest("ordId", "true", "");
+    }
+
+    @Test
+    public void test_isValidEmailReturnsTrueForValidEmail() {
+        String email = "email@test.com";
+        Boolean result = profUserReqValidator.isValidEmail(email);
+        assertTrue(result);
+    }
+
+    @Test
+    public void test_isValidEmailReturnsFalseForInvalidEmail() {
+        String email = "this-is-not-an-email";
+        Boolean result = profUserReqValidator.isValidEmail(email);
+        assertFalse(result);
+    }
+
+    @Test(expected = InvalidRequest.class)
+    public void test_validateUserStatusThrows400ForInvalidStatus() {
+        String status = "this-is-not-a-status";
+        profUserReqValidator.validateUserStatus(status);
+    }
+
+    @Test(expected = InvalidRequest.class)
+    public void test_validateStatusIsActiveThrows400ForInvalidStatus() {
+        String status = "this-is-not-a-status";
+        profUserReqValidator.validateStatusIsActive(status);
+    }
+
+    @Test(expected = InvalidRequest.class)
+    public void test_validateModifyRolesRequestThrows400WhenInvalidRequest() {
+        profUserReqValidator.validateModifyRolesRequest(modifyUserProfileDataMock, "");
     }
 }
