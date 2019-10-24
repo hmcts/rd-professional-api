0ba3d8930fff0db546f9abf3315dce7aae226c47
AdilOoze <adil.oozeerally@hmcts.net>
2019-10-24 16:24:37 +0100

    RDCC-529 added unit test coverage for Jurisdiction User Creation Request @Setter

--- src/test/java/uk/gov/hmcts/reform/professionalapi/controller/request/JurisdictionUserCreationRequestTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/controller/request/JurisdictionUserCreationRequestTest.java
@@ -1,14 +1,13 @@
 package uk.gov.hmcts.reform.professionalapi.controller.request;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-import org.junit.Test;
+import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.mockito.Mockito.mock;
+import org.junit.Test;
 
 public class JurisdictionUserCreationRequestTest {
 
