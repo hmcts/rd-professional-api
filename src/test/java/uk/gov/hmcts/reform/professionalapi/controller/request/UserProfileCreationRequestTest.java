bc02115acfd4669bba7f036e8640da034609a972
AdilOoze <adil.oozeerally@hmcts.net>
2019-10-24 17:22:00 +0100

    RDCC-529 increasing unit test coverage

--- src/test/java/uk/gov/hmcts/reform/professionalapi/controller/request/UserProfileCreationRequestTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/controller/request/UserProfileCreationRequestTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.professionalapi.controller.request;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +29,36 @@ public class UserProfileCreationRequestTest {
         assertThat(userProfileCreationRequest.getUserCategory()).isEqualTo(UserCategory.PROFESSIONAL);
         assertThat(userProfileCreationRequest.getUserType()).isEqualTo(UserType.EXTERNAL);
         assertThat(userProfileCreationRequest.getRoles().size()).isEqualTo(2);
+    }
+
+    @Test
+    public void test_BuilderMethod() {
+        String email = "email";
+        String firstName = "fName";
+        String lastName = "lName";
+        LanguagePreference langPref = mock(LanguagePreference.class);
+        UserCategory userCategory = mock(UserCategory.class);
+        UserType userType = mock(UserType.class);
+        List<String> roles = new ArrayList<>();
+        roles.add("aRole");
+
+        UserProfileCreationRequest userProfileCreationRequest = UserProfileCreationRequest.anUserProfileCreationRequest()
+                .email(email)
+                .firstName(firstName)
+                .lastName(lastName)
+                .languagePreference(langPref)
+                .userCategory(userCategory)
+                .userType(userType)
+                .roles(roles)
+                .build();
+
+        assertThat(userProfileCreationRequest.getEmail()).isEqualTo(email);
+        assertThat(userProfileCreationRequest.getFirstName()).isEqualTo(firstName);
+        assertThat(userProfileCreationRequest.getLastName()).isEqualTo(lastName);
+        assertThat(userProfileCreationRequest.getLanguagePreference()).isEqualTo(langPref);
+        assertThat(userProfileCreationRequest.getUserCategory()).isEqualTo(userCategory);
+        assertThat(userProfileCreationRequest.getUserType()).isEqualTo(userType);
+        assertThat(userProfileCreationRequest.getRoles()).isEqualTo(roles);
     }
 
 }
