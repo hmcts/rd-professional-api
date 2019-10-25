b6c9b2646fea2b9952ac87b95d3d7a1a00dcfff3
Adil Cassam Oozeerally <49557838+AdilOoze@users.noreply.github.com>
2019-10-25 17:06:46 +0100

    RDCC-529 increasing unit test coverage (#268)
    * RDCC-529 adding unit test coverage for Legacy Pba Controller
    
    * RDCC-529 fixing checkstyle, adding test for Organisation ID validator impl
    
    * RDCC-529 added unit test coverage for Organisation Identifier Validator
    
    * fixing checkstyle
    
    * Delete fastermutationtestingoutput]
    
    * RDCC-529 added unit test coverage for User Creation Request Validator
    
    * RDCC-529 adding unit test coverage for Professional User Req Validator
    
    * RDCC-529 added test coverage for Professional User Internal Controller
    
    * RDCC-529 added more test for Professional User Internal Controller
    
    * RDCC-529 fixing checkstyle
    
    * RDCC-529_2 test coverage updated
    
    * RDCC-529 updated test coverage
    
    * RDCC-529 added coverage for Exception Mapper and Professional Api Generator Constants
    
    * RDCC-529 fixing checkstyle
    
    * RDCC-529_2 Updated Test coverage
    
    * RDCC-529 added unit test coverage for Jurisdiction User Creation Request @Setter
    
    * RDCC-529 added unit test coverage for Jurisdiction User Creation Request @Setter
    
    * Update ProfessionalUserStatus.java
    
    * RDCC-529 added test coverage for Retrieve User Profile Request
    
    * RDCC-529_2 updated DxAddCrReqTest
    
    * RDCC-529 increasing unit test coverage
    
    * RDCC-529 added test coverage
    
    * RDCC-529 increasing JaCoCo test coverage threshold to 80%
    
    * RDCC-529 increased code coverage to 80%

--- src/test/java/uk/gov/hmcts/reform/professionalapi/controller/request/controller/request/ContactInformationCreationRequestTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/controller/request/controller/request/ContactInformationCreationRequestTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.professionalapi.controller.request.controller.request;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -58,7 +59,7 @@ public class ContactInformationCreationRequestTest {
         dxAddresses.add(dxAddressCreationRequest);
 
         ContactInformationCreationRequest contactInformationCreationRequest = new ContactInformationCreationRequest(
-                "some-address1",  null, null, null, null, null, null, null);
+                "some-address1", null, null, null, null, null, null, null);
 
         assertThat(contactInformationCreationRequest.getAddressLine1()).isEqualTo("some-address1");
         assertThat(contactInformationCreationRequest.getAddressLine2()).isNull();
@@ -85,5 +86,39 @@ public class ContactInformationCreationRequestTest {
         assertThat(contactInformationCreationRequest.getCountry()).isEqualTo("some-country");
         assertThat(contactInformationCreationRequest.getPostCode()).isEqualTo("some-post-code");
         assertThat(contactInformationCreationRequest.getDxAddress()).isNull();
+    }
+
+    @Test
+    public void test_contactInformationCreationRequestBuilder() {
+        String addressLine1 = "addressLine1";
+        String addressLine2 = "addressLine2";
+        String addressLine3 = "addressLine3";
+        String townCity = "townCity";
+        String county = "county";
+        String country = "country";
+        String postCode = "postC";
+        List<DxAddressCreationRequest> dxAddress = new ArrayList<>();
+        DxAddressCreationRequest dxAddressCreationRequestMock = mock(DxAddressCreationRequest.class);
+        dxAddress.add(dxAddressCreationRequestMock);
+
+        ContactInformationCreationRequest contactInformationCreationRequest = ContactInformationCreationRequest.aContactInformationCreationRequest()
+                .addressLine1(addressLine1)
+                .addressLine2(addressLine2)
+                .addressLine3(addressLine3)
+                .townCity(townCity)
+                .county(county)
+                .country(country)
+                .postCode(postCode)
+                .dxAddress(dxAddress)
+                .build();
+
+        assertThat(contactInformationCreationRequest.getAddressLine1()).isEqualTo(addressLine1);
+        assertThat(contactInformationCreationRequest.getAddressLine2()).isEqualTo(addressLine2);
+        assertThat(contactInformationCreationRequest.getAddressLine3()).isEqualTo(addressLine3);
+        assertThat(contactInformationCreationRequest.getTownCity()).isEqualTo(townCity);
+        assertThat(contactInformationCreationRequest.getCounty()).isEqualTo(county);
+        assertThat(contactInformationCreationRequest.getCountry()).isEqualTo(country);
+        assertThat(contactInformationCreationRequest.getPostCode()).isEqualTo(postCode);
+        assertThat(contactInformationCreationRequest.getDxAddress()).isEqualTo(dxAddress);
     }
 }
