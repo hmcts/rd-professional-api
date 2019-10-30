936838a533488aaccca99572797aab9d8be49c7e
akiohmcts <akio.cox@hmcts.net>
2019-10-30 06:27:57 +0000

    RDCC-375 removing spike provider and reverting prod code

--- src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/docs/SwaggerPublisher.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/docs/SwaggerPublisher.java
@@ -17,8 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
-import uk.gov.hmcts.reform.professionalapi.ConsumerApplication;
-//import uk.gov.hmcts.reform.professionalapi.Application;
+import uk.gov.hmcts.reform.professionalapi.Application;
 
 
 /**
@@ -26,7 +25,7 @@ import uk.gov.hmcts.reform.professionalapi.ConsumerApplication;
  * Each travis run on master should automatically save and upload (if updated) documentation.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ConsumerApplication.class, webEnvironment = MOCK)
+@SpringBootTest(classes = Application.class, webEnvironment = MOCK)
 public class SwaggerPublisher {
 
     private static final Logger LOG = getLogger(SwaggerPublisher.class);
--- src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/util/SpringBootIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/util/SpringBootIntegrationTest.java
@@ -4,11 +4,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.hmcts.reform.professionalapi.ConsumerApplication;
-//import uk.gov.hmcts.reform.professionalapi.Application;
+import uk.gov.hmcts.reform.professionalapi.Application;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = ConsumerApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class SpringBootIntegrationTest {
 
     @LocalServerPort
