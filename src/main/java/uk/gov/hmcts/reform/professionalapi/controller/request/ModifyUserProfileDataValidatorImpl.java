7693d44830b29e191c4b8db08c790cae8fe5268d
AdilOoze <adil.oozeerally@hmcts.net>
2019-11-01 12:02:02 +0000

    RDCC-418 updated validator to throw error if required fields are not present

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/ModifyUserProfileDataValidatorImpl.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/ModifyUserProfileDataValidatorImpl.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.professionalapi.controller.request;
 
 import org.apache.commons.lang3.StringUtils;
+import uk.gov.hmcts.reform.professionalapi.controller.advice.ErrorConstants;
 import uk.gov.hmcts.reform.professionalapi.domain.ModifyUserProfileData;
 
 public class ModifyUserProfileDataValidatorImpl implements ModifyUserProfileDataValidator {
@@ -10,7 +11,12 @@ public class ModifyUserProfileDataValidatorImpl implements ModifyUserProfileData
 
         ModifyUserProfileData validatedData = new ModifyUserProfileData();
 
-        //if all
+        //if required fields are empty
+        if (null == modifyUserProfileData.getRolesAdd() && null == modifyUserProfileData.getRolesDelete() && StringUtils.isEmpty(modifyUserProfileData.getIdamStatus())) {
+            throw new InvalidRequest(ErrorConstants.INVALID_REQUEST.getErrorMessage());
+        }
+
+        //if all are present
         if (null != modifyUserProfileData.getRolesAdd() && null != modifyUserProfileData.getRolesDelete() && !StringUtils.isEmpty(modifyUserProfileData.getIdamStatus())
                 && !StringUtils.isEmpty(modifyUserProfileData.getFirstName()) && !StringUtils.isEmpty(modifyUserProfileData.getLastName()) && !StringUtils.isEmpty(modifyUserProfileData.getEmail())) {
 
@@ -19,21 +25,21 @@ public class ModifyUserProfileDataValidatorImpl implements ModifyUserProfileData
             return validatedData;
         }
 
-        //if both roles and status
+        //if both roles and status are present
         if (null != modifyUserProfileData.getRolesAdd() && null != modifyUserProfileData.getRolesDelete() && !StringUtils.isEmpty(modifyUserProfileData.getIdamStatus())) {
             validatedData.setRolesAdd(modifyUserProfileData.getRolesAdd());
             validatedData.setRolesDelete(modifyUserProfileData.getRolesDelete());
             return validatedData;
         }
 
-        //if one role and status
+        //if one role and status are present
         if (null != modifyUserProfileData.getRolesDelete() || null != modifyUserProfileData.getRolesAdd() && StringUtils.isEmpty(modifyUserProfileData.getIdamStatus())) {
             validatedData.setRolesAdd(modifyUserProfileData.getRolesAdd());
             validatedData.setRolesDelete(modifyUserProfileData.getRolesDelete());
             return validatedData;
         }
 
-        //if no roles and status
+        //if no roles and status is present
         if (null == modifyUserProfileData.getRolesDelete() && null == modifyUserProfileData.getRolesAdd() && !StringUtils.isEmpty(modifyUserProfileData.getIdamStatus())) {
             validatedData.setIdamStatus(modifyUserProfileData.getIdamStatus());
             return validatedData;
