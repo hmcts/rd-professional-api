815e5cf83785cc39422672fa2bfed3de325df50c
Reem Ibrahim <reem.ibrahim@hmcts.net>
2019-11-06 12:09:31 +0000

    Refactored and fixed test

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/RetrievePaymentAccountForAnOrganisation.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/RetrievePaymentAccountForAnOrganisation.java
@@ -6,9 +6,10 @@ import static uk.gov.hmcts.reform.professionalapi.controller.request.NewUserCrea
 
 import io.restassured.specification.RequestSpecification;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import lombok.extern.slf4j.Slf4j;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
@@ -27,18 +28,17 @@ public class RetrievePaymentAccountForAnOrganisation extends AuthorizationFuncti
 
     RequestSpecification bearerTokenForUser;
 
-    public RequestSpecification generateBearerTokenForUser(String role) {
+    public RequestSpecification generateBearerTokenForUser(String roleUnderTest, String... otherRoles) {
         Map<String, Object> response = professionalApiClient.createOrganisation();
         String orgIdentifierResponse = (String) response.get("organisationIdentifier");
         professionalApiClient.updateOrganisation(orgIdentifierResponse, hmctsAdmin);
 
-        List<String> userRoles = new ArrayList<>();
-        userRoles.add("pui-user-manager");
+        List<String> userRoles = Arrays.stream(otherRoles).collect(Collectors.toList());
         String userEmail = randomAlphabetic(5).toLowerCase() + "@hotmail.com";
         String lastName = "someLastName";
         String firstName = "someName";
 
-        bearerTokenForUser = professionalApiClient.getMultipleAuthHeadersExternal(role, firstName, lastName, userEmail);
+        bearerTokenForUser = professionalApiClient.getMultipleAuthHeadersExternal(roleUnderTest, firstName, lastName, userEmail);
 
         NewUserCreationRequest userCreationRequest = aNewUserCreationRequest()
                 .firstName(firstName)
@@ -53,14 +53,14 @@ public class RetrievePaymentAccountForAnOrganisation extends AuthorizationFuncti
     }
 
     @Test
-    public void ac1_hmcts_user_with_appropriate_permission_can_retrieve_a_list_of_pbas_of_a_given_organisation() {
+    public void rdcc117_ac1_hmcts_user_with_appropriate_permission_can_retrieve_a_list_of_pbas_of_a_given_organisation() {
         Map<String, Object> response = professionalApiClient.retrievePbaAccountsForAnOrganisationExternal(HttpStatus.OK, generateBearerTokenForUser(puiFinanceManager));
         assertThat(response.get("paymentAccount")).asList().hasSize(1);
     }
 
     @Test
-    public void ac1_hmcts_user_without_appropriate_permission_cannot_retrieve_a_list_of_pbas_of_a_given_organisation() {
-        Map<String, Object> response = professionalApiClient.retrievePbaAccountsForAnOrganisationExternal(HttpStatus.OK, generateBearerTokenForUser(puiCaseManager));
+    public void rdcc117_ac1_hmcts_user_without_appropriate_permission_cannot_retrieve_a_list_of_pbas_of_a_given_organisation() {
+        Map<String, Object> response = professionalApiClient.retrievePbaAccountsForAnOrganisationExternal(HttpStatus.FORBIDDEN, generateBearerTokenForUser(puiCaseManager));
         assertThat(response.isEmpty());
     }
 }
