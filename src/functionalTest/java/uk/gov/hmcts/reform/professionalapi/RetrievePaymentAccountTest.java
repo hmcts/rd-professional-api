ade6984c91afcc4cfb5114032d1a196d772a97c5
Reem Ibrahim <reem.ibrahim@hmcts.net>
2019-11-06 16:53:44 +0000

    Refactored tests

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/RetrievePaymentAccountTest.java
@@ -0,0 +1,146 @@
+package uk.gov.hmcts.reform.professionalapi;
+
+import static org.apache.commons.lang.RandomStringUtils.randomAlphabetic;
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.professionalapi.controller.request.NewUserCreationRequest.aNewUserCreationRequest;
+import static uk.gov.hmcts.reform.professionalapi.controller.request.UserCreationRequest.aUserCreationRequest;
+import static uk.gov.hmcts.reform.professionalapi.utils.OrganisationFixtures.someMinimalOrganisationRequest;
+
+import io.restassured.specification.RequestSpecification;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import lombok.extern.slf4j.Slf4j;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.professionalapi.controller.request.NewUserCreationRequest;
+import uk.gov.hmcts.reform.professionalapi.controller.request.OrganisationCreationRequest;
+import uk.gov.hmcts.reform.professionalapi.utils.OrganisationFixtures;
+
+@RunWith(SpringIntegrationSerenityRunner.class)
+@ActiveProfiles("functional")
+@Slf4j
+public class RetrievePaymentAccountTest extends AuthorizationFunctionalTest {
+
+    RequestSpecification bearerTokenForUser;
+
+    public RequestSpecification generateBearerTokenForUser(String roleUnderTest, String... otherRoles) {
+        Map<String, Object> response = professionalApiClient.createOrganisation();
+        String orgIdentifierResponse = (String) response.get("organisationIdentifier");
+        professionalApiClient.updateOrganisation(orgIdentifierResponse, hmctsAdmin);
+
+        List<String> userRoles = Arrays.stream(otherRoles).collect(Collectors.toList());
+        userRoles.add(roleUnderTest);
+
+        String userEmail = randomAlphabetic(5).toLowerCase() + "@hotmail.com";
+        String lastName = "someLastName";
+        String firstName = "someName";
+
+        bearerTokenForUser = professionalApiClient.getMultipleAuthHeadersExternal(roleUnderTest, firstName, lastName, userEmail);
+
+        NewUserCreationRequest userCreationRequest = aNewUserCreationRequest()
+                .firstName(firstName)
+                .lastName(lastName)
+                .email(userEmail)
+                .roles(userRoles)
+                .jurisdictions(OrganisationFixtures.createJurisdictions())
+                .build();
+        professionalApiClient.addNewUserToAnOrganisation(orgIdentifierResponse, hmctsAdmin, userCreationRequest);
+
+        return bearerTokenForUser;
+    }
+
+    @Test
+    public void rdcc117_ac1_pui_finance_manager_can_retrieve_a_list_of_pbas_of_a_given_organisation() {
+        Map<String, Object> response = professionalApiClient.retrievePbaAccountsForAnOrganisationExternal(HttpStatus.OK, generateBearerTokenForUser(puiFinanceManager));
+        assertThat(response.get("paymentAccount")).asList().hasSize(1);
+    }
+
+    @Test
+    public void rdcc117_ac2_pui_organisation_manager_can_retrieve_a_list_of_pbas_of_a_given_organisation() {
+        Map<String, Object> response = professionalApiClient.retrievePbaAccountsForAnOrganisationExternal(HttpStatus.OK, generateBearerTokenForUser(puiOrgManager));
+        assertThat(response.get("paymentAccount")).asList().hasSize(1);
+    }
+
+    @Test
+    public void rdcc117_ac3_user_without_appropriate_permission_cannot_retrieve_a_list_of_pbas_of_a_given_organisation() {
+        Map<String, Object> response = professionalApiClient.retrievePbaAccountsForAnOrganisationExternal(HttpStatus.FORBIDDEN, generateBearerTokenForUser(puiCaseManager));
+        assertThat(response.isEmpty());
+    }
+
+    @Test
+    public void can_retrieve_active_organisation_payment_accounts_user_by_email() {
+        String email = randomAlphabetic(10) + "@pbasearch.test".toLowerCase();
+
+        List<String> paymentAccounts = new ArrayList<>();
+        paymentAccounts.add("PBA" + randomAlphabetic(7));
+
+        OrganisationCreationRequest request = someMinimalOrganisationRequest()
+                .paymentAccount(paymentAccounts)
+                .superUser(aUserCreationRequest()
+                        .firstName("some-fname")
+                        .lastName("some-lname")
+                        .email(email)
+                        .jurisdictions(OrganisationFixtures.createJurisdictions())
+                        .build())
+                .build();
+
+        Map<String, Object> response =  professionalApiClient.createOrganisation(request);
+        String orgIdentifierResponse = (String) response.get("organisationIdentifier");
+        assertThat(orgIdentifierResponse).isNotEmpty();
+        request.setStatus("ACTIVE");
+        professionalApiClient.updateOrganisation(request, hmctsAdmin, orgIdentifierResponse);
+        Map<String, Object> orgResponse = professionalApiClient.retrievePaymentAccountsByEmail(email.toLowerCase(), hmctsAdmin);
+        assertThat(orgResponse).isNotEmpty();
+        responseValidate(orgResponse);
+    }
+
+    @Test
+    public void can_return_404_when_pending_organisation_payment_account_user_by_email() {
+        String email = randomAlphabetic(10) + "@pbasearch.test".toLowerCase();
+
+        List<String> paymentAccounts = new ArrayList<>();
+        paymentAccounts.add("PBA" + randomAlphabetic(7));
+
+        Map<String, Object> response =  professionalApiClient.createOrganisation(
+                someMinimalOrganisationRequest()
+                        .paymentAccount(paymentAccounts)
+                        .superUser(aUserCreationRequest()
+                                .firstName("some-fname")
+                                .lastName("some-lname")
+                                .email(email)
+                                .jurisdictions(OrganisationFixtures.createJurisdictions())
+                                .build())
+                        .build());
+
+        professionalApiClient.retrieveBadRequestForPendingOrganisationWithPbaEmail(email, hmctsAdmin);
+    }
+
+    private void responseValidate(Map<String, Object> orgResponse) {
+
+        orgResponse.forEach((k,v) -> {
+
+            if ("organisationIdentifier".equals(k) && "http_status".equals(k)
+                    && "name".equals(k) &&  "status".equals(k)
+                    && "superUser".equals(k) && "paymentAccount".equals(k)) {
+
+                Assertions.assertThat(v.toString()).isNotEmpty();
+                Assertions.assertThat(v.toString().contains("Ok"));
+                Assertions.assertThat(v.toString().contains("some-org-name"));
+                Assertions.assertThat(v.toString().equals("ACTIVE"));
+                Assertions.assertThat(v.toString()).isNotEmpty();
+            }
+
+        });
+
+    }
+}
