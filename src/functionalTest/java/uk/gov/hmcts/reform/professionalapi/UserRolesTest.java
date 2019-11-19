da82dcabb453d5ffea31d2b6e3415c210529fe28
Reem Ibrahim <reem.ibrahim@hmcts.net>
2019-11-19 16:50:31 +0000

    Functional test for super user having fpla and iac roles

---
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/UserRolesTest.java
@@ -0,0 +1,33 @@
+package uk.gov.hmcts.reform.professionalapi;
+
+import lombok.extern.slf4j.Slf4j;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringIntegrationSerenityRunner.class)
+@ActiveProfiles("functional")
+@Slf4j
+public class UserRolesTest extends AuthorizationFunctionalTest{
+
+    private String orgIdentifier;
+
+    @Test
+    public void ac1_super_user_can_have_fpla_or_iac_roles() {
+        Map<String, Object> response = professionalApiClient.createOrganisation();
+        log.info(response.toString());
+        orgIdentifier = (String) response.get("organisationIdentifier");
+        professionalApiClient.updateOrganisation(orgIdentifier, hmctsAdmin);
+
+        response = professionalApiClient.retrieveOrganisationDetails(orgIdentifier, puiUserManager);
+        List<String> userRoles = (List<String>) response.get("roles");
+        assertThat(userRoles).contains("caseworker-publiclaw", "caseworker-publiclaw-solicitor", "caseworker-ia-legalrep-solicitor");
+    }
+}
