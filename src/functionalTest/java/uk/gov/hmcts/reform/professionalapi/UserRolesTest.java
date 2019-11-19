a41f512433f3cf215ab30f9947afaca79e3ed82c
Reem Ibrahim <reem.ibrahim@hmcts.net>
2019-11-19 17:25:30 +0000

    Added loggers for debugging

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/UserRolesTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/UserRolesTest.java
@@ -21,12 +21,13 @@ public class UserRolesTest extends AuthorizationFunctionalTest {
     @Test
     public void ac1_super_user_can_have_fpla_or_iac_roles() {
         Map<String, Object> response = professionalApiClient.createOrganisation();
-        log.info(response.toString());
         orgIdentifier = (String) response.get("organisationIdentifier");
         professionalApiClient.updateOrganisation(orgIdentifier, hmctsAdmin);
 
         response = professionalApiClient.retrieveOrganisationDetails(orgIdentifier, puiUserManager);
+        log.info("ORG DETAILS RESPONSE::::::::::" + response);
         List<String> userRoles = (List<String>) response.get("roles");
+        log.info("USER ROLES::::::::::::" + userRoles);
         assertThat(userRoles).contains("caseworker-publiclaw", "caseworker-publiclaw-solicitor", "caseworker-ia-legalrep-solicitor");
     }
 }
