8da1d0feb0bf6d0c35334c13d1d3dc827c55daa6
Reem Ibrahim <reem.ibrahim@hmcts.net>
2019-11-20 11:12:55 +0000

    Alternative approach to retrieving super user roles and more loggers

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/UserRolesTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/UserRolesTest.java
@@ -1,6 +1,9 @@
 package uk.gov.hmcts.reform.professionalapi;
 
+import static org.apache.commons.lang.RandomStringUtils.randomAlphabetic;
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.professionalapi.controller.request.UserCreationRequest.aUserCreationRequest;
+import static uk.gov.hmcts.reform.professionalapi.utils.OrganisationFixtures.someMinimalOrganisationRequest;
 
 import java.util.List;
 import java.util.Map;
@@ -10,6 +13,8 @@ import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
+import uk.gov.hmcts.reform.professionalapi.controller.request.OrganisationCreationRequest;
+import uk.gov.hmcts.reform.professionalapi.utils.OrganisationFixtures;
 
 @RunWith(SpringIntegrationSerenityRunner.class)
 @ActiveProfiles("functional")
@@ -20,14 +25,30 @@ public class UserRolesTest extends AuthorizationFunctionalTest {
 
     @Test
     public void ac1_super_user_can_have_fpla_or_iac_roles() {
-        Map<String, Object> response = professionalApiClient.createOrganisation();
+
+        String email = randomAlphabetic(10) + "@usersearch.test".toLowerCase();
+        OrganisationCreationRequest request = someMinimalOrganisationRequest()
+                .superUser(aUserCreationRequest()
+                        .firstName("some-fname")
+                        .lastName("some-lname")
+                        .email(email)
+                        .jurisdictions(OrganisationFixtures.createJurisdictions())
+                        .build())
+                .build();
+
+        Map<String, Object> response = professionalApiClient.createOrganisation(request);
         orgIdentifier = (String) response.get("organisationIdentifier");
         professionalApiClient.updateOrganisation(orgIdentifier, hmctsAdmin);
 
-        response = professionalApiClient.retrieveOrganisationDetails(orgIdentifier, puiUserManager);
-        log.info("ORG DETAILS RESPONSE::::::::::" + response);
-        List<String> userRoles = (List<String>) response.get("roles");
+        //response = professionalApiClient.retrieveOrganisationDetails(orgIdentifier, puiUserManager);
+        //log.info("ORG DETAILS RESPONSE::::::::::" + response);
+
+        Map<String, Object> searchResponse = professionalApiClient.searchForUserByEmailAddress(email.toLowerCase(), hmctsAdmin);
+        log.info("USER EMAIL SEARCH RESPONSE::::::::::::" + searchResponse);
+
+        List<String> userRoles = (List<String>) searchResponse.get("roles");
         log.info("USER ROLES::::::::::::" + userRoles);
+
         assertThat(userRoles).contains("caseworker-publiclaw", "caseworker-publiclaw-solicitor", "caseworker-ia-legalrep-solicitor");
     }
 }
