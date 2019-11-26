13d73de2394f61465f5cc4623b411df54de15774
ConorH994 <conor.halpin@hmcts.net>
2019-11-26 11:36:18 +0000

    security changes

--- security.sh
+++ security.sh
@@ -2,7 +2,11 @@
 echo ${TEST_URL}
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -u ${SecurityRules} -P 1001 -l FAIL
 cat zap.out
+echo "ZAP has successfully started"
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
 mkdir -p functional-output
+chmod a+wx functional-output
 cp /zap/api-report.html functional-output/
 zap-cli -p 1001 alerts -l High
