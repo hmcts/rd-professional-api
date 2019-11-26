d0cd401b4502573a0851452218bc4b26320f392a
ConorH994 <conor.halpin@hmcts.net>
2019-11-26 12:19:02 +0000

    rearragning steps

--- security.sh
+++ security.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 echo ${TEST_URL}
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -u ${SecurityRules} -P 1001 -l FAIL
 cat zap.out
 echo "ZAP has successfully started"
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
 mkdir -p functional-output
 chmod a+wx functional-output
