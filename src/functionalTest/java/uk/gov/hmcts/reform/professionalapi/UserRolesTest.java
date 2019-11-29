fd5fd989b41b94bf56658b976a204c089c247357
Reem Ibrahim <reem.ibrahim@hmcts.net>
2019-11-29 10:39:13 +0000

    Slight tidy

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/UserRolesTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/UserRolesTest.java
@@ -152,10 +152,10 @@ public class UserRolesTest extends AuthorizationFunctionalTest {
 
     }
 
-    @Test
+    //External endpoint for create new user is failing because PUM is unable to add new user with fpla and iac roles in AAT env
+    //Awaiting https://tools.hmcts.net/jira/browse/SIDM-3475 and https://tools.hmcts.net/jira/browse/SIDM-3476
     public void ac6_external_user_cannot_add_user_with_non_fpla_or_iac_roles() {
-        //External endpoint for create new user is failing because PUM is unable to add new user with fpla and iac roles in AAT env
-        //Awaiting https://tools.hmcts.net/jira/browse/SIDM-3475 and https://tools.hmcts.net/jira/browse/SIDM-3476
+
     }
 
     public RequestSpecification generateBearerTokenForPuiManager() {
