1544a18316ea386a3a2868b06b72bbed599d2d5b
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2020-01-06 16:47:43 +0000

    RDCC-797 Abort flow while CCD error

--- src/main/java/uk/gov/hmcts/reform/professionalapi/service/impl/JurisdictionServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/service/impl/JurisdictionServiceImpl.java
@@ -26,6 +26,9 @@ public class JurisdictionServiceImpl implements JurisdictionService {
     @Autowired
     AuthTokenGenerator authTokenGenerator;
 
+    static final String errorMessage = "Jurisdiction create user profile failed or CCD service is down";
+    static final String successMessage = "Jurisdiction create user profile success!!";
+
     @Override
     public void propagateJurisdictionIdsForSuperUserToCcd(ProfessionalUser user, String userId) {
 
@@ -54,11 +57,18 @@ public class JurisdictionServiceImpl implements JurisdictionService {
     public void callCcd(JurisdictionUserCreationRequest request, String userId) {
         String s2sToken = authTokenGenerator.generate();
         try (Response response = jurisdictionFeignClient.createJurisdictionUserProfile(userId, s2sToken, request)) {
-            log.info("Jurisdiction create user profile success!!");
+            if (response == null || response.status() > 300) {
+                log.info("CCD status code: " + (response == null ? null : response.status()));
+                throwException(null);
+            }
         } catch (FeignException ex) {
-            String errorMessage = "Jurisdiction create user profile failed or CCD service is down";
-            log.error(errorMessage, ex);
-            throw new ExternalApiException(HttpStatus.INTERNAL_SERVER_ERROR, errorMessage);
+            throwException(ex);
         }
+        log.info(successMessage);
+    }
+
+    public void throwException(Exception ex) {
+        log.error(errorMessage, ex);
+        throw new ExternalApiException(HttpStatus.INTERNAL_SERVER_ERROR, errorMessage);
     }
 }
