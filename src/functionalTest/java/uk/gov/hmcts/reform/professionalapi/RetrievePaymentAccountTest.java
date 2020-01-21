80348776d30fdd84961f3b813502f03fc7669f43
ConorH994 <conor.halpin@hmcts.net>
2020-01-21 12:29:37 +0000

    changed outdated test to still apply

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/RetrievePaymentAccountTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/RetrievePaymentAccountTest.java
@@ -75,9 +75,9 @@ public class RetrievePaymentAccountTest extends AuthorizationFunctionalTest {
         assertThat(response.get("paymentAccount")).asList().hasSize(1);
     }
 
-    //@Test
+    @Test
     public void rdcc117_ac3_user_without_appropriate_permission_cannot_retrieve_a_list_of_pbas_of_a_given_organisation() {
-        Map<String, Object> response = professionalApiClient.retrievePbaAccountsForAnOrganisationExternal(HttpStatus.FORBIDDEN, generateBearerTokenForUser(puiCaseManager));
+        Map<String, Object> response = professionalApiClient.retrievePbaAccountsForAnOrganisationExternal(HttpStatus.FORBIDDEN, generateBearerTokenForUser(puiUserManager));
         assertThat(response.get("errorMessage")).isNotNull();
         assertThat(response.get("errorMessage")).isEqualTo("9 : Access Denied");
     }
