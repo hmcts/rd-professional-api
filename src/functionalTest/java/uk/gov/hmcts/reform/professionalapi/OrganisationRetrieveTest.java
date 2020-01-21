cb00c6eba6358c13a9d0becafa710daf7532d18d
ConorH994 <conor.halpin@hmcts.net>
2020-01-21 10:31:19 +0000

    changes

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/OrganisationRetrieveTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/OrganisationRetrieveTest.java
@@ -1,16 +1,24 @@
 package uk.gov.hmcts.reform.professionalapi;
 
+import static org.apache.commons.lang.RandomStringUtils.randomAlphabetic;
 import static org.assertj.core.api.Assertions.assertThat;
+import static uk.gov.hmcts.reform.professionalapi.controller.request.NewUserCreationRequest.aNewUserCreationRequest;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
+import io.restassured.specification.RequestSpecification;
 import lombok.extern.slf4j.Slf4j;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.springframework.http.HttpStatus;
+import uk.gov.hmcts.reform.professionalapi.controller.request.NewUserCreationRequest;
 import uk.gov.hmcts.reform.professionalapi.domain.OrganisationStatus;
+import uk.gov.hmcts.reform.professionalapi.utils.OrganisationFixtures;
 
 @RunWith(SpringIntegrationSerenityRunner.class)
 @Slf4j
@@ -42,6 +50,37 @@ public class OrganisationRetrieveTest extends AuthorizationFunctionalTest {
         Assertions.assertThat(response.size()).isGreaterThanOrEqualTo(1);
     }
 
+    public RequestSpecification generateBearerTokenForPuiManager() {
+        Map<String, Object> response = professionalApiClient.createOrganisation();
+        String orgIdentifierResponse = (String) response.get("organisationIdentifier");
+        professionalApiClient.updateOrganisation(orgIdentifierResponse, hmctsAdmin);
+
+        List<String> userRoles = new ArrayList<>();
+        userRoles.add("pui-user-manager");
+        String userEmail = randomAlphabetic(5).toLowerCase() + "@hotmail.com";
+        String lastName = "someLastName";
+        String firstName = "someName";
+
+        bearerTokenForPuiCaseManager = professionalApiClient.getMultipleAuthHeadersExternal(puiCaseManager, firstName, lastName, userEmail);
+
+        NewUserCreationRequest userCreationRequest = aNewUserCreationRequest()
+                .firstName(firstName)
+                .lastName(lastName)
+                .email(userEmail)
+                .roles(userRoles)
+                .jurisdictions(OrganisationFixtures.createJurisdictions())
+                .build();
+        professionalApiClient.addNewUserToAnOrganisation(orgIdentifierResponse, hmctsAdmin, userCreationRequest, HttpStatus.CREATED);
+
+        return bearerTokenForPuiCaseManager;
+    }
+
+    //@Test
+    public void can_retrieve_an_organisation() {
+        Map<String, Object> response = professionalApiClient.retrievePbaAccountsForAnOrganisationExternal(HttpStatus.OK, generateBearerTokenForPuiManager());
+        assertThat(response).isNotEmpty();
+    }
+
     @Test
     public void can_retrieve_Pending_and_Active_organisations() {
 
@@ -65,6 +104,11 @@ public class OrganisationRetrieveTest extends AuthorizationFunctionalTest {
         Map<String, Object> response = professionalApiClient
                 .retrieveOrganisationDetailsByStatus(OrganisationStatus.PENDING.name(), hmctsAdmin);
         assertThat(response.size()).isGreaterThanOrEqualTo(1);
+    }
+
+    //@Test
+    public void can_retrieve_an_organisation_with_role() {
+
     }
 
     @Test
