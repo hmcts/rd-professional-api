5ee9d267671ccad5cba1d9e43e540fdebe8aac84
AdilOoze <adil.oozeerally@hmcts.net>
2020-02-19 15:34:01 +0000

    RDCC-822 changing regex used for org id

--- src/main/java/uk/gov/hmcts/reform/professionalapi/generator/ProfessionalApiGeneratorConstants.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/generator/ProfessionalApiGeneratorConstants.java
@@ -7,7 +7,7 @@ public class ProfessionalApiGeneratorConstants {
 
     public static final int LENGTH_OF_UUID = 36;
     public static final int LENGTH_OF_ORGANISATION_IDENTIFIER = 7;
-    public static final String ORGANISATION_IDENTIFIER_FORMAT_REGEX = "^(?=.*[A-Z])(?=.*[0-9])[A-Z0-9]{7}$";
+    public static final String ORGANISATION_IDENTIFIER_FORMAT_REGEX = "^[A-Z0-9]{7}$";
     public static final String ORG_ID_VALIDATION_ERROR_MESSAGE = "The given organisationIdentifier must be 7 Alphanumeric Characters";
 
 }
