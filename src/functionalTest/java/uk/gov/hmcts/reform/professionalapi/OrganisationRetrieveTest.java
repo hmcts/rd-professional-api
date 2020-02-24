41d7f84474a239557ead3c67d5252dbb0ad160ec
ConorH994 <conor.halpin@hmcts.net>
2020-02-24 17:29:44 +0000

    removed test code that was not up to standard

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/OrganisationRetrieveTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/OrganisationRetrieveTest.java
@@ -65,20 +65,6 @@ public class OrganisationRetrieveTest extends AuthorizationFunctionalTest {
         Assertions.assertThat(finalResponse.size()).isGreaterThanOrEqualTo(1);
         assertThat(newOrgResponse.get("paymentAccount")).asList().size().isEqualTo(3);
         assertThat(newOrgResponse.get("contactInformation")).asList().size().isEqualTo(2);
-
-        Map<String, Object> contactInfo1 = ((List<Map<String, Object>>) newOrgResponse.get("contactInformation")).get(0);
-        Map<String, Object> contactInfo2 = ((List<Map<String, Object>>) newOrgResponse.get("contactInformation")).get(1);
-
-        assertThat(contactInfo1.get("addressLine1")).isEqualTo("addressLine1");
-        assertThat(contactInfo2.get("addressLine1")).isEqualTo("addLine1");
-
-        Map<String, Object> dxAddress = ((List<Map<String, Object>>) contactInfo1.get("dxAddress")).get(0);
-        Map<String, Object> dxAddress2 = ((List<Map<String, Object>>) contactInfo1.get("dxAddress")).get(1);
-
-        assertThat(dxAddress.get("dxNumber")).isEqualTo("DX 1234567890");
-        assertThat(dxAddress.get("dxExchange")).isEqualTo("dxExchange");
-        assertThat(dxAddress2.get("dxNumber")).isEqualTo("DX 123456777");
-        assertThat(dxAddress2.get("dxExchange")).isEqualTo("dxExchange");
     }
 
     @Test
