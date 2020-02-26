ba477511bc776989fec67c9a76b199621b2e8473
AdilOoze <adil.oozeerally@hmcts.net>
2020-02-26 14:51:27 +0000

    RDCC-822 fixing checkstyle

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/advice/ExceptionMapper.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/advice/ExceptionMapper.java
@@ -31,7 +31,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
-import org.springframework.security.web.firewall.RequestRejectedException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
