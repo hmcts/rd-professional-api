87457ead7b0f048519c01d0c1d53d514f5746e6d
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2020-03-27 12:52:42 +0000

    Added new files from master

---
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/validator/UserCreationRequestValidator.java
@@ -0,0 +1,25 @@
+package uk.gov.hmcts.reform.professionalapi.controller.request.validator;
+
+import java.util.List;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
+import uk.gov.hmcts.reform.professionalapi.controller.request.InvalidRequest;
+
+@Slf4j
+public class UserCreationRequestValidator {
+
+    private UserCreationRequestValidator() {
+    }
+
+
+    public static List<String> validateRoles(List<String> roles) {
+
+        if (CollectionUtils.isEmpty(roles)) {
+            log.error("No user role(s) provided");
+            throw new InvalidRequest("No role(s) provided");
+        }
+
+        return roles;
+    }
+}
