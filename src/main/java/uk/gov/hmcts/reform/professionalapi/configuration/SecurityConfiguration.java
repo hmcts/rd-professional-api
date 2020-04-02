f402196890d2c2753aefbd63631718d8b24cda84
KOTLAPRA23 <prashanth.kotla@hmcts.net>
2020-04-02 15:24:35 +0100

    updated jar version and configurations

--- src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/SecurityConfiguration.java
@@ -122,7 +122,6 @@ public class SecurityConfiguration {
                     .formLogin().disable()
                     .logout().disable()
                     .authorizeRequests()
-                    // To preserve v1EndpointsPathParamSecurityFilter 403 response
                     .antMatchers("/error").permitAll()
                     .anyRequest()
                     .authenticated()
@@ -143,8 +142,9 @@ public class SecurityConfiguration {
             // We are using issuerOverride instead of issuerUri as SIDAM has the wrong issuer at the moment
             OAuth2TokenValidator<Jwt> withTimestamp = new JwtTimestampValidator();
             OAuth2TokenValidator<Jwt> withIssuer = new JwtIssuerValidator(issuerOverride);
-            OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp, withIssuer);
-
+            // FIXME : enable `withIssuer` once idam migration is done
+            //OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp, withIssuer);
+            OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp);
             jwtDecoder.setJwtValidator(validator);
             return jwtDecoder;
         }
