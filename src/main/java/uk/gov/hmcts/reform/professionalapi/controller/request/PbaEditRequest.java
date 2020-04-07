a614459433364dc11478c8ad7c9bdc9791ee8e2c
AdilOoze <adil.oozeerally@hmcts.net>
2020-04-07 13:47:01 +0100

    RDCC-969: removing @JsonProperty & adding @NoArgsConstructor

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/PbaEditRequest.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/PbaEditRequest.java
@@ -1,24 +1,23 @@
 package uk.gov.hmcts.reform.professionalapi.controller.request;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.Set;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Builder(builderMethodName = "anPbaEditRequest")
+@NoArgsConstructor
 public class PbaEditRequest {
 
-    @JsonIgnore
+    @JsonProperty
     private Set<String> paymentAccounts;
 
-    @JsonCreator
-    public PbaEditRequest(@JsonProperty("payment_accounts") Set<String> paymentAccounts) {
+    public PbaEditRequest(Set<String> paymentAccounts) {
         this.paymentAccounts = paymentAccounts;
     }
 }
