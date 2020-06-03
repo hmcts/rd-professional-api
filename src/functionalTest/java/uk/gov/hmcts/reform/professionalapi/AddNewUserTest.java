96f85b1f529683865cc53c1c0a22ef36851229c3
AdilOoze <adil.oozeerally@hmcts.net>
2020-06-03 09:13:19 +0100

    RDCC-1387: amending test to add the roles and check user roles contains them

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/AddNewUserTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/AddNewUserTest.java
@@ -160,8 +160,8 @@ public class AddNewUserTest extends AuthorizationFunctionalTest {
     @Test
     public void add_new_user_with_caa_roles_to_organisation_should_return_201() {
         List<String> userRoles = new ArrayList<>();
-        //userRoles.add("pui-caa");
-        //userRoles.add("caseworker-caa");
+        userRoles.add("pui-caa");
+        userRoles.add("caseworker-caa");
         userRoles.add("pui-user-manager");
         String firstName = "someName";
         String lastName = "someLastName";
@@ -186,7 +186,7 @@ public class AddNewUserTest extends AuthorizationFunctionalTest {
         Map newUserDetails = users.get(1);
         List<String> superUserRoles = getNestedValue(newUserDetails, "roles");
 
-        assertThat(superUserRoles).doesNotContain("pui-caa");
-        assertThat(superUserRoles).doesNotContain("caseworker-caa");
+        assertThat(superUserRoles).contains("pui-caa");
+        assertThat(superUserRoles).contains("caseworker-caa");
     }
 }
