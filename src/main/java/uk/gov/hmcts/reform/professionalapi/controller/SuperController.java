4a16703146d6eb5f751d535b611386fbe2acce4e
KOTLAPRA23 <49564565+KOTLAPRA23@users.noreply.github.com>
2020-06-10 16:30:27 +0100

    feat: get users list for active status by pui-caa role (#574)
    * feat: adding test case to fail to get active users for pui-caa role in the PR branch
    
    * feat: adding pui-caa role get the users of organisation and adding integration and functional test cases in the PR branch.
    
    * feat: code refactor for get the user roles of an external organisation in the PR branch.
    
    * feat: extension modified from application-functional.yaml to yml file in the PR branch.
    
    * feat: code refactor in the functional test cases for get the user roles of an external organisation in the PR branch.

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/SuperController.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/SuperController.java
@@ -90,19 +90,19 @@ public abstract class SuperController {
     @Autowired
     protected UserProfileUpdateRequestValidator userProfileUpdateRequestValidator;
 
-    @Value("${exui.role.hmcts-admin:}")
+    @Value("${prd.security.roles.hmcts-admin:}")
     protected String prdAdmin;
 
-    @Value("${exui.role.pui-user-manager:}")
+    @Value("${prd.security.roles.pui-user-manager:}")
     protected String puiUserManager;
 
-    @Value("${exui.role.pui-organisation-manager:}")
+    @Value("${prd.security.roles.pui-organisation-manager:}")
     protected String puiOrgManager;
 
-    @Value("${exui.role.pui-finance-manager}")
+    @Value("${prd.security.roles.pui-finance-manager}")
     protected String puiFinanceManager;
 
-    @Value("${exui.role.pui-case-manager:}")
+    @Value("${prd.security.roles.pui-case-manager:}")
     protected String puiCaseManager;
 
     @Value("${prdEnumRoleType}")
