751b159a5a4373de4b85589f9808730812444cf4
pksaxena01 <praveen.saxena@hmcts.net>
2020-06-17 16:36:50 +0100

    Removing unwanted changes

--- bin/run-in-docker.sh
+++ bin/run-in-docker.sh
@@ -44,11 +44,10 @@ execute_script() {
 
   clean_old_docker_artifacts
 
-    #build_s2s_image
+    build_s2s_image
 
-    #build_service_auth_app
-    docker-compose down -v
-    docker system prune
+    build_service_auth_app
+
     ./gradlew clean assemble
 
     export SERVER_PORT="${SERVER_PORT:-8090}"
