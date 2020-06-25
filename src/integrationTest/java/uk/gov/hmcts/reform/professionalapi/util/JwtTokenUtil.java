950ab80bde0d62933c94f3d8610978ed5a96d944
541839 <sushant.choudhari@hmcts.net>
2020-06-25 16:33:34 +0100

    chanenges for log componant

--- src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/util/JwtTokenUtil.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/util/JwtTokenUtil.java
@@ -13,16 +13,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.binary.Base64;
-import org.springframework.beans.factory.annotation.Value;
 import uk.gov.hmcts.reform.authorisation.exceptions.InvalidTokenException;
 
 @Slf4j
 public final class JwtTokenUtil {
 
     private static final String SUBJECT = "sub";
-
-    @Value("${logging-component-name}")
-    protected static String loggingComponentName;
 
     private JwtTokenUtil() {
     }
@@ -56,7 +52,7 @@ public final class JwtTokenUtil {
                     builder.build());
             signedJwt.sign(new RSASSASigner(KeyGenUtil.getRsaJwk()));
         } catch (JOSEException e) {
-            log.error("{}:: error while creating bearer token : ", loggingComponentName, (e.getMessage()));
+            log.error("error while creating bearer token : " + (e.getMessage()));
         }
         return signedJwt.serialize();
     }
