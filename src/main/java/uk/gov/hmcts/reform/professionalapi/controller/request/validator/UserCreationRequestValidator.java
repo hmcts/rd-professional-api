7c54179b248e525e4d53d11ec44c14c24922c363
AdilOoze <adil.oozeerally@hmcts.net>
2020-07-10 16:52:48 +0100

    RDCC-1582: bringing changes from PR#639

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/validator/UserCreationRequestValidator.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/validator/UserCreationRequestValidator.java
@@ -3,11 +3,15 @@ package uk.gov.hmcts.reform.professionalapi.controller.request.validator;
 import java.util.List;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.CollectionUtils;
 import uk.gov.hmcts.reform.professionalapi.controller.request.InvalidRequest;
 
 @Slf4j
 public class UserCreationRequestValidator {
+
+    @Value("${logging-component-name}")
+    protected static String loggingComponentName;
 
     private UserCreationRequestValidator() {
     }
@@ -16,7 +20,7 @@ public class UserCreationRequestValidator {
     public static List<String> validateRoles(List<String> roles) {
 
         if (CollectionUtils.isEmpty(roles)) {
-            log.error("No user role(s) provided");
+            log.error("{}:: No user role(s) provided",loggingComponentName);
             throw new InvalidRequest("No role(s) provided");
         }
 
