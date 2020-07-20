dae5107b8ddcc5348fb25e7c532872931b7112d8
abhijit <Abhijit.diwan@hmcts.net>
2020-07-20 23:03:15 +0100

    fix: functional test performance improvement

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/OrganisationRetrieveTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/OrganisationRetrieveTest.java
@@ -18,13 +18,13 @@ import uk.gov.hmcts.reform.professionalapi.domain.OrganisationStatus;
 @Slf4j
 public class OrganisationRetrieveTest extends AuthorizationFunctionalTest {
 
+    Map<String, Object> finalResponse = professionalApiClient.retrieveAllOrganisations(hmctsAdmin);
+
     @Test
     public void can_retrieve_all_organisations() {
         professionalApiClient.createOrganisation();
-
-        Map<String, Object> response = professionalApiClient.retrieveAllOrganisations(hmctsAdmin);
-        assertThat(response.get("organisations")).isNotNull();
-        Assertions.assertThat(response.size()).isGreaterThanOrEqualTo(1);
+        assertThat(finalResponse.get("organisations")).isNotNull();
+        Assertions.assertThat(finalResponse.size()).isGreaterThanOrEqualTo(1);
     }
 
     @Test
@@ -63,7 +63,7 @@ public class OrganisationRetrieveTest extends AuthorizationFunctionalTest {
         professionalApiClient.updateOrganisation(orgIdentifierTwo, hmctsAdmin);
         Map<String, Object> newOrgResponse = professionalApiClient.retrieveOrganisationDetails(orgIdentifierTwo,
                 hmctsAdmin,HttpStatus.OK);
-        Map<String, Object> finalResponse = professionalApiClient.retrieveAllOrganisations(hmctsAdmin);
+
 
         assertThat(finalResponse.get("organisations")).isNotNull();
         Assertions.assertThat(finalResponse.size()).isGreaterThanOrEqualTo(1);
