26a80d301eb722f9b19f9b887a03b2e3a462c4b2
KOTLAPRA23 <prashanth.kotla@hmcts.net>
2020-07-27 18:47:17 +0100

    feat:adding exclusion class for spring boot failure

--- src/main/java/uk/gov/hmcts/reform/professionalapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/Application.java
@@ -7,14 +7,17 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
+import uk.gov.hmcts.reform.health.HealthAutoConfiguration;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 @EnableJpaAuditing
 @EnableJpaRepositories
-@SpringBootApplication
+@SpringBootApplication(exclude = HealthAutoConfiguration.class)
 @EnableCaching
 @EnableFeignClients(basePackages = {
-        "uk.gov.hmcts.reform.professionalapi" }, basePackageClasses = { IdamApi.class, ServiceAuthorisationApi.class })
+        "uk.gov.hmcts.reform.professionalapi" },
+        basePackageClasses = { IdamApi.class, ServiceAuthorisationApi.class }
+        )
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application  {
 
