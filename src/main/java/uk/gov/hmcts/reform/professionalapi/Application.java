f118c88c8a5a477f632d2b908d30438d841ea5bb
KOTLAPRA23 <prashanth.kotla@hmcts.net>
2020-07-27 20:25:51 +0100

    feat:reverting the change

--- src/main/java/uk/gov/hmcts/reform/professionalapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/Application.java
@@ -12,7 +12,7 @@ import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 @EnableJpaAuditing
 @EnableJpaRepositories
-@SpringBootApplication(exclude = HealthAutoConfiguration.class)
+@SpringBootApplication
 @EnableCaching
 @EnableFeignClients(basePackages = {
         "uk.gov.hmcts.reform.professionalapi" },
