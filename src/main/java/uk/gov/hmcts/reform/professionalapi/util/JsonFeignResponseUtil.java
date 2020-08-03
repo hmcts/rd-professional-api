20e5aac5d77c623b694bc8f25f014d89c8dd84e4
AdilOoze <adil.oozeerally@hmcts.net>
2020-08-03 13:29:01 +0100

    RDCC-1728: removing extra lines, re-organising imports

--- src/main/java/uk/gov/hmcts/reform/professionalapi/util/JsonFeignResponseUtil.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/util/JsonFeignResponseUtil.java
@@ -1,25 +1,23 @@
 package uk.gov.hmcts.reform.professionalapi.util;
 
+import static java.time.LocalDateTime.now;
+import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiGeneratorConstants.UNAUTHORISED;
+import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiGeneratorConstants.UNSUCCESSFUL_AUTHENTICATION;
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.Response;
-
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import uk.gov.hmcts.reform.professionalapi.controller.advice.ErrorResponse;
-
-import static java.time.LocalDateTime.now;
-import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiGeneratorConstants.UNAUTHORISED;
-import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiGeneratorConstants.UNSUCCESSFUL_AUTHENTICATION;
 
 @SuppressWarnings("unchecked")
 public class JsonFeignResponseUtil {
@@ -27,7 +25,6 @@ public class JsonFeignResponseUtil {
             .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     private JsonFeignResponseUtil() {
-
     }
 
     public static Optional<Object> decode(Response response, Object clazz) {
@@ -40,18 +37,18 @@ public class JsonFeignResponseUtil {
     }
 
     public static ResponseEntity<Object> toResponseEntity(Response response, Object clazz) {
-
-        Optional<Object> payload = decode(response, clazz);
+        Optional<Object> payload;
 
         if (response.status() == 401) {
             payload = Optional.of(new ErrorResponse(UNAUTHORISED, UNSUCCESSFUL_AUTHENTICATION, now().toString()));
+        } else {
+            payload = decode(response, clazz);
         }
 
         return new ResponseEntity<>(
                 payload.orElse(null),
                 convertHeaders(response.headers()),
                 HttpStatus.valueOf(response.status()));
-
     }
 
     public static MultiValueMap<String, String> convertHeaders(Map<String, Collection<String>> responseHeaders) {
