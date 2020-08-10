5e0c76b0e9a3a058e4144aa2b3c7d48a1334ce07
KOTLAPRA23 <prashanth.kotla@hmcts.net>
2020-08-10 16:15:04 +0100

    feat:removed feature  condition and disabled integration test cases

--- src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/FeatureConditionEvaluation.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/FeatureConditionEvaluation.java
@@ -2,7 +2,6 @@ package uk.gov.hmcts.reform.professionalapi.configuration;
 
 import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiConstants.ERROR_MESSAGE_403_FORBIDDEN;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -13,14 +12,12 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class FeatureConditionEvaluation  implements HandlerInterceptor {
 
-    @Value("${deleteOrganisationEnabled}")
-    private boolean deleteOrganisationEnabled;
 
     @Override
     public boolean preHandle(HttpServletRequest request,
                              HttpServletResponse response, Object arg2) throws Exception {
 
-        if (!deleteOrganisationEnabled) {
+        if (request.getMethod().equals("DELETE")) {
 
             response.sendError(403, ERROR_MESSAGE_403_FORBIDDEN);
         }
