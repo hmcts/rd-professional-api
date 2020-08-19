9e994ef259ab843defd809f0d5820f40816b2a25
vijayrajagopalan-hmcts <vijay.rajagopalan@hmcts.net>
2020-08-19 11:03:41 +0100

    Removed unwanted core infra references

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,26 +1,3 @@
 terraform {
-  required_version = ">= 0.12.28"
   backend "azurerm" {}
-}
-
-data "terraform_remote_state" "core_apps_infrastructure" {
-  backend = "azurerm"
-
-  config {
-    resource_group_name  = "mgmt-state-store-${var.subscription}"
-    storage_account_name = "mgmtstatestore${var.subscription}"
-    container_name       = "mgmtstatestorecontainer${var.env}"
-    key                  = "core-infra/${var.env}/terraform.tfstate"
-  }
-}
-
-data "terraform_remote_state" "core_apps_compute" {
-  backend = "azurerm"
-
-  config {
-    resource_group_name  = "mgmt-state-store-${var.subscription}"
-    storage_account_name = "mgmtstatestore${var.subscription}"
-    container_name       = "mgmtstatestorecontainer${var.env}"
-    key                  = "core-compute/${var.env}/terraform.tfstate"
-  }
 }
