710e11a682a439e8246d168ed790e33182b03a72
Adil Cassam Oozeerally <49557838+AdilOoze@users.noreply.github.com>
2020-08-19 13:30:14 +0100

    RDCC-1706: Adding query param to return Address details of Organisations (#694)
    * RDCC-1706: Bringing changes from PR#661
    
    * RDCC-1706: additional query param changes
    
    * RDCC-1706: adding query param changes to integration test
    
    * RDCC-1706: functional test changes
    
    * RDCC-1706: functional test changes
    
    * RDCC-1706: functional test changes
    
    * RDCC-1706: functional test changes
    
    * RDCC-1706: functional test changes
    
    * RDCC-1706: Swagger Info updates
    
    * RDCC-1706: removing DX Address from response
    
    * RDCC-1706: pull from master/resolving conflicts
    
    * RDCC-1706: unfinished toggle changes
    
    * RDCC-1706: addressing review comments
    
    * RDCC-1706: feature toggle changes
    
    * RDCC-1706: feature toggle changes
    
    * RDCC-1706: addressing review comments
    
    * RDCC-1706: addressing review comments
    
    * RDCC-1706: creating new parent response entity with tests
    
    * RDCC-1706: addressing review comments & sonar smells
    
    * RDCC-1706: toggling feature on
    
    * RDCC-1706: omitting contactInformation field in response when address flag is false
    
    * RDCC-1706: toggling feature off
    
    Co-authored-by: Shashank Rastogi <49564391+shashank-rastogi@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/professionalapi/service/OrganisationService.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/service/OrganisationService.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.professionalapi.service;
 
+import java.util.List;
 import uk.gov.hmcts.reform.professionalapi.controller.request.OrganisationCreationRequest;
 import uk.gov.hmcts.reform.professionalapi.controller.response.DeleteOrganisationResponse;
 import uk.gov.hmcts.reform.professionalapi.controller.response.OrganisationEntityResponse;
@@ -25,5 +26,7 @@ public interface OrganisationService {
     OrganisationsDetailResponse findByOrganisationStatus(OrganisationStatus status);
 
     DeleteOrganisationResponse deleteOrganisation(Organisation organisation, String userId);
+
+    List<Organisation> getOrganisationByStatus(OrganisationStatus status);
 }
 
