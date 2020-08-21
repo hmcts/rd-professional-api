bb7f84ded16a73f52ebd3da57d6576d05d021292
vijayrajagopalan-hmcts <vijay.rajagopalan@hmcts.net>
2020-08-21 14:15:13 +0100

    Added logging

--- src/main/java/uk/gov/hmcts/reform/professionalapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/Application.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.professionalapi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
 
+@Slf4j
 @EnableJpaAuditing
 @EnableJpaRepositories
 @SpringBootApplication
@@ -19,8 +21,19 @@ import uk.gov.hmcts.reform.idam.client.IdamApi;
         )
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application  {
-
     public static void main(final String[] args) {
+        log.error("Getting System environment variables ");
+        log.error("variables :  " + System.getenv());
+        System.getenv().entrySet().iterator().forEachRemaining(entry -> {
+            log.error(entry.getKey() + "     " + entry.getValue());
+        });
+        log.error("End getting env veriables");
+        System.out.println("Getting System environment variables ");
+        System.out.println("variables :  " + System.getenv());
+        System.getenv().entrySet().iterator().forEachRemaining(entry -> {
+            System.out.println(entry.getKey() + "     " + entry.getValue());
+        });
+        System.out.println("End getting env veriables");
         SpringApplication.run(Application.class, args);
     }
 }
