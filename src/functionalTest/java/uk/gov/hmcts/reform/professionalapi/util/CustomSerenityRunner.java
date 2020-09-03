6487ea6c344536564f80bc1d5c4002bf9e7e1f4d
abhijit <Abhijit.diwan@hmcts.net>
2020-09-03 15:06:26 +0100

    fix:LD in functional cases

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/util/CustomSerenityRunner.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/util/CustomSerenityRunner.java
@@ -6,10 +6,19 @@ import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import org.junit.runners.model.FrameworkMethod;
 import org.junit.runners.model.InitializationError;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import uk.gov.hmcts.reform.professionalapi.config.LaunchDarklyTestConfig;
+import uk.gov.hmcts.reform.professionalapi.config.Oauth2;
+import uk.gov.hmcts.reform.professionalapi.config.TestConfigProperties;
 import uk.gov.hmcts.reform.professionalapi.service.impl.FeatureToggleServiceImpl;
 
 import static net.logstash.logback.encoder.org.apache.commons.lang3.BooleanUtils.negate;
 
+@ContextConfiguration(classes = {TestConfigProperties.class, Oauth2.class, LaunchDarklyTestConfig.class})
+@ComponentScan("uk.gov.hmcts.reform.professionalapi")
+@TestPropertySource("classpath:application-functional.yaml")
 public class CustomSerenityRunner extends SpringIntegrationSerenityRunner {
 
     @Autowired
