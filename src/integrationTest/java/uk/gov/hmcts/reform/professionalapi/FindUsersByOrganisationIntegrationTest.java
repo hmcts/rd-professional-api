75f5e36ffa30d77176643dc393a495cbf0cb8180
Adil Cassam Oozeerally <49557838+AdilOoze@users.noreply.github.com>
2020-09-23 09:50:32 +0100

    RDCC-1894: Convert Functional Tests to Integration Tests (#791)
    * RDCC-1894: removed and replaced tests in FindUsersByOrganisationTest
    
    * RDCC-1894: removed and replaced tests in OrganisationCreationsTest
    
    * RDCC-1894: removed and replaced tests in OrganisationRetrieveTest, RetrievePaymentAccountTest & UserEmailSearchTest
    
    * RDCC-1894: upgrading mockito version in attemp to resolve NPE in unit tests
    
    * RDCC-1894: downgrading mockito version in attemp to resolve NPE in unit tests
    
    * RDCC-1894: removing DeleteOrg 403 test & amending integration test
    
    * RDCC-1894: fixing checkstyle

--- src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/FindUsersByOrganisationIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/FindUsersByOrganisationIntegrationTest.java
@@ -44,7 +44,15 @@ public class FindUsersByOrganisationIntegrationTest extends AuthorizationEnabled
         Map<String, Object> response = professionalReferenceDataClient.findUsersByOrganisation(organisationIdentifier,
                 null, hmctsAdmin);
         validateUsers(response, 3, true);
+    }
 
+    @Test
+    public void can_retrieve_users_with_showDeleted_invalid_should_return_status_200() {
+        String organisationIdentifier = createOrganisationRequest();
+        updateOrganisation(organisationIdentifier, hmctsAdmin, "ACTIVE");
+        Map<String, Object> response = professionalReferenceDataClient.findUsersByOrganisation(organisationIdentifier,
+                "INVALID", hmctsAdmin);
+        validateUsers(response, 3, true);
     }
 
     @Test
@@ -131,6 +139,14 @@ public class FindUsersByOrganisationIntegrationTest extends AuthorizationEnabled
         Map<String, Object> response = professionalReferenceDataClient.findAllUsersForOrganisationByStatus(
                 "false", "", puiUserManager, id);
         validateUsers(response, 3, true);
+    }
+
+    @Test
+    public void retrieve_all_users_for_an_organisation_with_pui_user_manager_role_with_invalid_status_return_400() {
+        String id = settingUpOrganisation("pui-user-manager");
+        Map<String, Object> response = professionalReferenceDataClient.findAllUsersForOrganisationByStatus(
+                "false", "INVALID", puiUserManager, id);
+        assertThat(response.get("http_status")).isEqualTo("400");
     }
 
     @Test
