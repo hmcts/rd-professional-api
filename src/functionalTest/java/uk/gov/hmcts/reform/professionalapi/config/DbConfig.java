a95044d07ac5b4ae99255c61b2fb619c3863ff94
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2020-09-23 15:43:26 +0100

    RDCC-1739 Refactored code to initialise connection once by PreDestroy bean

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/config/DbConfig.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/config/DbConfig.java
@@ -1,13 +1,20 @@
 package uk.gov.hmcts.reform.professionalapi.config;
 
+import static java.lang.System.getenv;
+
 import com.zaxxer.hikari.HikariDataSource;
+import javax.annotation.PreDestroy;
 import javax.sql.DataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.context.TestPropertySource;
+import java.sql.Connection;
 
 @Configuration
 @TestPropertySource("classpath:application-functional.yaml")
@@ -29,6 +36,8 @@ public class DbConfig {
     @Value("${postgres.port}")
     public String postgresPort;
 
+    private HikariDataSource dataSource;
+
     @Bean
     public DataSource dataSource() {
 
@@ -37,9 +46,8 @@ public class DbConfig {
         log.info("Func DB name: {}", postgresDbName);
         log.info("Func DB user name: {}", postgresUserName);
         String url = String.format("jdbc:postgresql://%s:%s/%s", postgresHost, postgresPort, postgresDbName);
-        log.info("Func DB url: {}",  url);
+        log.info("Func DB url: {}", url);
 
-        HikariDataSource dataSource = null;
         try {
             DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
             dataSourceBuilder.driverClassName("org.postgresql.Driver");
@@ -51,5 +59,36 @@ public class DbConfig {
             log.error("Unable to connect to database from functional test case : {}", e.getMessage());
         }
         return dataSource;
+    }
+
+    @PreDestroy
+    public void createDbConnection() {
+        Connection connection;
+        String isNightlyBuild = getenv("isNightlyBuild");
+        String testUrl = getenv("TEST_URL");
+        log.info("isNightlyBuild: {}", isNightlyBuild);
+        log.info("testUrl: {}", testUrl);
+        if (Boolean.TRUE.toString().equalsIgnoreCase(isNightlyBuild) && testUrl.contains("aat")) {
+            log.info("Delete test data script execution started");
+            try {
+                connection = dataSource.getConnection();
+                connection.setAutoCommit(false);
+                ScriptUtils.executeSqlScript(connection,
+                        new EncodedResource(new ClassPathResource("delete-functional-test-data.sql")),
+                        false, true,
+                        ScriptUtils.DEFAULT_COMMENT_PREFIX,
+                        ScriptUtils.DEFAULT_STATEMENT_SEPARATOR,
+                        ScriptUtils.DEFAULT_BLOCK_COMMENT_START_DELIMITER,
+                        ScriptUtils.DEFAULT_BLOCK_COMMENT_END_DELIMITER);
+                log.info("Delete test data script execution completed");
+                if (!connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (Exception exe) {
+                log.error("Delete test data script execution failed: {}", exe.getMessage());
+            }
+        } else {
+            log.info("Not executing delete test data script");
+        }
     }
 }
