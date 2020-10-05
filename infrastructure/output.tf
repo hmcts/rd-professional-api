7a4c13896dd999c8a72b9886554cdbefaca66f32
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-10-05 18:58:47 +0100

    Commit TF Upgrade changes. TF 0.12.16, AzureRM 2.20.0

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,16 +1,3 @@
-
 output "resourceGroup" {
-  value = "${azurerm_resource_group.rg.name}"
-}
-
-output "appServicePlan" {
-  value = "${local.app_service_plan}"
-}
-
-output "vaultName" {
-  value = "${local.key_vault_name}"
-}
-
-output "vaultUri" {
-  value = "${local.s2s_vault_uri}"
+  value = azurerm_resource_group.rg.name
 }
