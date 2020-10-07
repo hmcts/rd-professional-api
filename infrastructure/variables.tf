3f0fb037fdd5ef953c547071e9f71037f4fe1709
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-10-07 15:44:44 +0100

    Removed unused variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -24,40 +24,11 @@ variable "subscription" {
   type = string
 }
 
-# variable "ilbIp" {
-#   type = string
-# }
-
 variable "common_tags" {
   type = map(string)
 }
-
-# variable "appinsights_instrumentation_key" {
-#   type    = string
-#   default = ""
-# }
-
-# variable "root_logging_level" {
-#   type    = string
-#   default = "INFO"
-# }
-
-# variable "log_level_spring_web" {
-#   type    = string
-#   default = "INFO"
-# }
-
-# variable "log_level_rd" {
-#   type    = string
-#   default = "INFO"
-# }
 
 variable "team_name" {
   type    = string
   default = "RD"
 }
-
-# variable "managed_identity_object_id" {
-#   type    = string
-#   default = ""
-# }
