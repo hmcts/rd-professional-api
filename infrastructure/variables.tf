5733b998305cb0dfe99cacb42017a75c4551d04c
Vijay Kumar Rajagopalan <vijay.rajagopalan@hmcts.net>
2020-10-07 15:03:01 +0100

    TF Upgrade to 13.3 and Azure RM to 2.25+

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -24,40 +24,40 @@ variable "subscription" {
   type = string
 }
 
-variable "ilbIp" {
-  type = string
-}
+# variable "ilbIp" {
+#   type = string
+# }
 
 variable "common_tags" {
   type = map(string)
 }
 
-variable "appinsights_instrumentation_key" {
-  type    = string
-  default = ""
-}
+# variable "appinsights_instrumentation_key" {
+#   type    = string
+#   default = ""
+# }
 
-variable "root_logging_level" {
-  type    = string
-  default = "INFO"
-}
+# variable "root_logging_level" {
+#   type    = string
+#   default = "INFO"
+# }
 
-variable "log_level_spring_web" {
-  type    = string
-  default = "INFO"
-}
+# variable "log_level_spring_web" {
+#   type    = string
+#   default = "INFO"
+# }
 
-variable "log_level_rd" {
-  type    = string
-  default = "INFO"
-}
+# variable "log_level_rd" {
+#   type    = string
+#   default = "INFO"
+# }
 
 variable "team_name" {
   type    = string
   default = "RD"
 }
 
-variable "managed_identity_object_id" {
-  type    = string
-  default = ""
-}
+# variable "managed_identity_object_id" {
+#   type    = string
+#   default = ""
+# }
