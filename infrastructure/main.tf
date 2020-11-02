79d1704075f3ff657b02ef05d761926cdf475432
zc-hmcts <54357991+zc-hmcts@users.noreply.github.com>
2020-11-02 14:19:53 +0000

    RDO-9575

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,40 +4,45 @@ locals {
   key_vault_name          = var.env == "preview" || var.env == "spreview" ? local.preview_vault_name : local.non_preview_vault_name
 }
 
+provider "azurerm" {
+  alias = "azurerm-1"
+  version = "1.27.0"
+}
+
 data "azurerm_key_vault" "rd_key_vault" {
   name                = local.key_vault_name
   resource_group_name = local.key_vault_name
 }
 
-# resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-#   name          = join("-", [var.component, "POSTGRES-HOST"])
-#   value         = module.db-professional-ref-data.host_name
-#   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-# }
+ resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+   name          = join("-", [var.component, "POSTGRES-HOST"])
+   value         = module.db-professional-ref-data.host_name
+   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+ }
 
-# resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-#   name          = join("-", [var.component, "POSTGRES-PORT"])
-#   value         = "5432"
-#   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-# }
+ resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+   name          = join("-", [var.component, "POSTGRES-PORT"])
+   value         = "5432"
+   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+ }
 
-# resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-#   name          = join("-", [var.component, "POSTGRES-USER"])
-#   value         = module.db-professional-ref-data.user_name
-#   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-# }
+ resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+   name          = join("-", [var.component, "POSTGRES-USER"])
+   value         = module.db-professional-ref-data.user_name
+   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+ }
 
-# resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-#   name          = join("-", [var.component, "POSTGRES-PASS"])
-#   value         = module.db-professional-ref-data.postgresql_password
-#   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-# }
+ resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+   name          = join("-", [var.component, "POSTGRES-PASS"])
+   value         = module.db-professional-ref-data.postgresql_password
+   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+ }
 
-# resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-#   name          = join("-", [var.component, "POSTGRES-DATABASE"])
-#   value         = module.db-professional-ref-data.postgresql_database
-#   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-# }
+ resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+   name          = join("-", [var.component, "POSTGRES-DATABASE"])
+   value         = module.db-professional-ref-data.postgresql_database
+   key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
+ }
 
 resource "azurerm_resource_group" "rg" {
   name      = join("-", [var.product, var.component, var.env])
