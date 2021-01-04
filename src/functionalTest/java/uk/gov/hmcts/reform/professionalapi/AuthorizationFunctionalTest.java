b5aee698ba474e8e16120c2a2bf9ddc7dc118aef
shreedhar-hmcts <ed71fae8f60e250eedd92483bcf497c20a22ce95>
2021-01-04 18:43:38 +0000

    RDCC-2049 : added cve

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/AuthorizationFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/AuthorizationFunctionalTest.java
@@ -17,7 +17,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
-import net.serenitybdd.rest.SerenityRest;
 import org.assertj.core.api.Assertions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
