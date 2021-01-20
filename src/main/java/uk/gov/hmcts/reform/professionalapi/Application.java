5305ea158b3e8033136ebd01cb6a9c2504ed6040
ruban72 <ruban72@hotmail.com>
2021-01-20 16:36:54 +0000

    TA-224: Remove JPA annotation from application.class  as Spring Autodetects

--- src/main/java/uk/gov/hmcts/reform/professionalapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/Application.java
@@ -4,9 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
 
+@EnableJpaAuditing
+@EnableJpaRepositories
 @SpringBootApplication
 @EnableCaching
 @EnableFeignClients(basePackages = {
