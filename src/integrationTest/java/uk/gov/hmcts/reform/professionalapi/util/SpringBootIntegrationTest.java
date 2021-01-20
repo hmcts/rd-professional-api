c233e9a14b1c2ec1547f6dbbab9bffaa68bb4772
ruban72 <ruban72@hotmail.com>
2021-01-20 18:02:47 +0000

    TA-224: Remove JPA annotation from application.class  as Spring Autodetects

--- src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/util/SpringBootIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/util/SpringBootIntegrationTest.java
@@ -3,11 +3,14 @@ package uk.gov.hmcts.reform.professionalapi.util;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.professionalapi.Application;
+import uk.gov.hmcts.reform.professionalapi.repository.TestConfig;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestConfig.class)
 public abstract class SpringBootIntegrationTest {
 
     @LocalServerPort
