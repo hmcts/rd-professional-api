1830519f76b8e4516c98d6c436fc46407f87c8c7
ruban72 <ruban72@hotmail.com>
2021-01-20 16:34:02 +0000

    TA-224: Remove JPA annotation from application.class  as Spring Autodetects

--- src/main/java/uk/gov/hmcts/reform/professionalapi/Application.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/Application.java
@@ -10,11 +10,11 @@ import uk.gov.hmcts.reform.idam.client.IdamApi;
 @SpringBootApplication
 @EnableCaching
 @EnableFeignClients(basePackages = {
-    "uk.gov.hmcts.reform.professionalapi"},
-    basePackageClasses = {IdamApi.class, ServiceAuthorisationApi.class}
-)
+        "uk.gov.hmcts.reform.professionalapi" },
+        basePackageClasses = { IdamApi.class, ServiceAuthorisationApi.class }
+        )
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
-public class Application {
+public class Application  {
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
