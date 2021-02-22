bff9f4cf6bdd0edb3440a95928666302945c3926
ruban72 <ruban72@hotmail.com>
2021-02-22 14:40:41 +0000

    TA-224: Refactor ExUi approve org provider tests

--- src/contractTest/java/uk/gov/hmcts/reform/professionalapi/provider/OrganisationalInternalControllerProviderTestConfiguration.java
+++ src/contractTest/java/uk/gov/hmcts/reform/professionalapi/provider/OrganisationalInternalControllerProviderTestConfiguration.java
@@ -46,7 +46,6 @@ public class OrganisationalInternalControllerProviderTestConfiguration extends P
     @MockBean
     IdamRepository idamRepository;
 
-
     @Bean
     @Primary
     protected OrganisationInternalController organisationInternalController() {
