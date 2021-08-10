2e4998e4dd3cce68126782fdf2546fa2d81e6f25
amitkumarshrivastava <86308925+amitkumarshrivastava@users.noreply.github.com>
2021-08-10 15:19:09 +0100

    RDCC-3119 - code changes and initial table changes

--- src/integrationTest/resources/db/migration/V1_1__init_tables.sql
+++ src/integrationTest/resources/db/migration/V1_1__init_tables.sql
@@ -140,3 +140,9 @@ references contact_information (id) on delete cascade;
 
 alter table domain add constraint organisation_fk6 foreign key (organisation_id)
 references organisation (id) on delete cascade;
+
+alter table payment_account add COLUMN pba_status VARCHAR(10) default 'PENDING' not null;
+
+alter table payment_account add COLUMN status_message VARCHAR(256);
+
+alter table payment_account add CONSTRAINT pba_status_values_constraint CHECK (pba_status IN ('PENDING','ACCEPTED','REJECTED'));
