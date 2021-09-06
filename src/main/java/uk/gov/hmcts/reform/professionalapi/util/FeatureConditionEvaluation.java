8f81d9d800e607622c82151f7250575f19ee8059
viswajit-kuna <viswajit.kuna@HMCTS.NET>
2021-09-06 14:45:12 +0100

    RDCC-3118: Removed unused imports.

--- src/main/java/uk/gov/hmcts/reform/professionalapi/util/FeatureConditionEvaluation.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/util/FeatureConditionEvaluation.java
@@ -1,22 +1,17 @@
 package uk.gov.hmcts.reform.professionalapi.util;
 
-import com.auth0.jwt.JWT;
-import java.util.Map;
-import java.util.Optional;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.constraints.NotNull;
-
 import lombok.AllArgsConstructor;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import uk.gov.hmcts.reform.professionalapi.exception.ForbiddenException;
 import uk.gov.hmcts.reform.professionalapi.service.FeatureToggleService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotNull;
+import java.util.Map;
 
 import static java.util.Objects.nonNull;
 import static org.apache.commons.lang.BooleanUtils.isNotTrue;
