cb51afdaef2ea80a7571054c842bc47f7a48ba07
AdilOoze <adil.oozeerally@hmcts.net>
2021-11-12 16:51:15 +0000

    RDCC-3522: refactored tests in package 'controller/response/'

--- src/test/java/uk/gov/hmcts/reform/professionalapi/controller/response/DxAddressResponseTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/controller/response/DxAddressResponseTest.java
@@ -2,9 +2,12 @@ package uk.gov.hmcts.reform.professionalapi.controller.response;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.professionalapi.domain.DxAddress;
 
+@ExtendWith(MockitoExtension.class)
 public class DxAddressResponseTest {
 
     @Test
--- src/test/java/uk/gov/hmcts/reform/professionalapi/controller/response/GetUserProfileResponseTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/controller/response/GetUserProfileResponseTest.java
@@ -3,11 +3,14 @@ package uk.gov.hmcts.reform.professionalapi.controller.response;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.UUID;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.professionalapi.controller.constants.IdamStatus;
 import uk.gov.hmcts.reform.professionalapi.domain.UserProfile;
 
+@ExtendWith(MockitoExtension.class)
 public class GetUserProfileResponseTest {
 
     @Test
