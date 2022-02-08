8a565b121a55cf265fbc6ccde4f7b931f2b1cd86
amitkumarshrivastava <86308925+amitkumarshrivastava@users.noreply.github.com>
2022-02-08 12:25:23 +0000

    RDCC-3872 changed toggle flag for multiple address

--- src/main/java/uk/gov/hmcts/reform/professionalapi/service/impl/FeatureToggleServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/service/impl/FeatureToggleServiceImpl.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.professionalapi.service.FeatureToggleService;
 
 import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiConstants.PRD_MFA_LD_FLAG;
-import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiConstants.RD_PROFESSIONAL_ADD_CONTACTS_INFO_TO_ORG;
 import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiConstants.RD_PROFESSIONAL_MULTI_PBA_LD_FLAG;
 import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiConstants.RD_PROFESSIONAL_MULTIPLE_ADDRESS;
 
@@ -56,7 +55,7 @@ public class FeatureToggleServiceImpl implements FeatureToggleService {
         launchDarklyMap.put("OrganisationInternalController.updateAnOrganisationsRegisteredPbas",
                 RD_PROFESSIONAL_MULTI_PBA_LD_FLAG);
         launchDarklyMap.put("OrganisationExternalController.addContactInformationsToOrganisation",
-                RD_PROFESSIONAL_ADD_CONTACTS_INFO_TO_ORG);
+                RD_PROFESSIONAL_MULTIPLE_ADDRESS);
         launchDarklyMap.put("OrganisationExternalController.deleteMultipleAddressesOfOrganisation",
                 RD_PROFESSIONAL_MULTIPLE_ADDRESS);
 
