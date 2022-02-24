34f990c048d3ef0d6332ca0fbe7249d61c347253
amitkumarshrivastava <86308925+amitkumarshrivastava@users.noreply.github.com>
2022-02-24 16:13:19 +0000

    RDCC-3457 Fixed review comments

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/external/ProfessionalExternalUserController.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/external/ProfessionalExternalUserController.java
@@ -114,7 +114,7 @@ public class ProfessionalExternalUserController extends SuperController {
 
         if (!organisationIdentifierValidatorImpl.ifUserRoleExists(jwtGrantedAuthoritiesConverter.getUserInfo()
                 .getRoles(), PUI_USER_MANAGER)) {
-            status = StringUtils.isEmpty(status) ? ACTIVE : status;
+            status = StringUtils.isBlank(status) ? ACTIVE : status;
             profExtUsrReqValidator.validateStatusIsActive(status);
         }
 
