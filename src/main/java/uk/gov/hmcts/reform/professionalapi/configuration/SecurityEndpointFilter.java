59aea7b17764881c45f569f99f43e3c5c1f2cf90
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-05-26 17:08:10 +0100

    RDCC-4680:Changes made to fix build failure

--- src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/SecurityEndpointFilter.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/SecurityEndpointFilter.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.professionalapi.configuration;
 
-import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -8,12 +7,11 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
 import uk.gov.hmcts.reform.professionalapi.controller.advice.UnauthorizedException;
 
-import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import java.io.IOException;
 
 
 @Configuration
@@ -33,10 +31,7 @@ public class SecurityEndpointFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
         } catch (Exception e) {
             Throwable throwable = e.getCause();
-            if (throwable instanceof FeignException.FeignClientException feignClientException) {
-                response.setStatus(feignClientException.status());
-                return;
-            } else if (e instanceof UnauthorizedException) {
+            if (e instanceof UnauthorizedException) {
                 logger.error("Authorisation exception", e);
                 response.sendError(HttpStatus.FORBIDDEN.value(), "Access Denied");
                 return;
