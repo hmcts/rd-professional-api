bd03a16b113f7e7d242a7d025309e36d5ea79cc4
shivajihmcts <shivaji.gaikwad@hmcts.net>
2022-05-27 11:23:38 +0100

    RDCC-4680:Changes made to incorporate review comment

--- src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/SecurityEndpointFilter.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/configuration/SecurityEndpointFilter.java
@@ -1,10 +1,9 @@
 package uk.gov.hmcts.reform.professionalapi.configuration;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.OncePerRequestFilter;
-import uk.gov.hmcts.reform.idam.client.IdamApi;
 import uk.gov.hmcts.reform.professionalapi.controller.advice.UnauthorizedException;
 
 import javax.servlet.FilterChain;
@@ -13,15 +12,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
 @Configuration
+@Slf4j
 public class SecurityEndpointFilter extends OncePerRequestFilter {
-
-    public static final String AUTHORIZATION = "Authorization";
-    public static final String SERVICE_AUTHORIZATION2 = "ServiceAuthorization";
-
-    @Autowired
-    IdamApi idamApi;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -32,7 +25,7 @@ public class SecurityEndpointFilter extends OncePerRequestFilter {
         } catch (Exception e) {
             Throwable throwable = e.getCause();
             if (e instanceof UnauthorizedException) {
-                logger.error("Authorisation exception", e);
+                log.error("Authorisation exception", e);
                 response.sendError(HttpStatus.FORBIDDEN.value(), "Access Denied");
                 return;
             }
