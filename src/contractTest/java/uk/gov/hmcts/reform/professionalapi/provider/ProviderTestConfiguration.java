790a71ddf5c09ffab7dd2a2fa850aae3e2149b79
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2022-07-09 16:28:34 +0100

    pact test config

--- src/contractTest/java/uk/gov/hmcts/reform/professionalapi/provider/ProviderTestConfiguration.java
+++ src/contractTest/java/uk/gov/hmcts/reform/professionalapi/provider/ProviderTestConfiguration.java
@@ -22,6 +22,7 @@ import uk.gov.hmcts.reform.professionalapi.controller.request.validator.PaymentA
 import uk.gov.hmcts.reform.professionalapi.controller.request.validator.ProfessionalUserReqValidator;
 import uk.gov.hmcts.reform.professionalapi.controller.request.validator.UpdateOrganisationRequestValidator;
 import uk.gov.hmcts.reform.professionalapi.controller.request.validator.impl.OrganisationIdentifierValidatorImpl;
+import uk.gov.hmcts.reform.professionalapi.repository.IdamRepository;
 import uk.gov.hmcts.reform.professionalapi.repository.ProfessionalUserRepository;
 import uk.gov.hmcts.reform.professionalapi.service.PrdEnumService;
 import uk.gov.hmcts.reform.professionalapi.service.UserAccountMapService;
@@ -58,7 +59,8 @@ public class ProviderTestConfiguration {
 
     @MockBean
     public OrganisationCreationRequestValidator organisationCreationRequestValidatorMock;
-
+    @MockBean
+    public IdamRepository idamRepository;
 
     @Value("${prd.security.roles.hmcts-admin:}")
     protected String prdAdmin;
