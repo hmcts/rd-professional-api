8c6adbd30ada3a5c9640b54a891f19e9059b48ed
manukundloo-hmcts <114229572+manukundloo-hmcts@users.noreply.github.com>
2022-10-20 15:29:09 +0100

    RDCC-5592:rd-professional-api code smell issues fixes (#1328)
    * RDCC-5592:rd-professional-api code smell issues fixes
    
    * RDCC-5592:rd-professional-api code smell issues fixes

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/DxAddressCreationRequest.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/DxAddressCreationRequest.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.Pattern;
 public class DxAddressCreationRequest {
 
     @NotNull
-    @Pattern(regexp = "^(?:DX|NI) [0-9]{10}+$")
+    @Pattern(regexp = "^(?:DX|NI) \\d{10}+$")
     private final String dxNumber;
     @NotNull
     private final String dxExchange;
@@ -21,7 +21,6 @@ public class DxAddressCreationRequest {
     @JsonCreator
     public DxAddressCreationRequest(@JsonProperty("dxNumber") String dxNumber,
                                     @JsonProperty("dxExchange") String dxExchange) {
-
         this.dxNumber = dxNumber;
         this.dxExchange = dxExchange;
     }
