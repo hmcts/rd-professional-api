4c0f35e2c5c007f7cf5df156dd3d2f6795a2c653
shivajihmcts <shivaji.gaikwad@hmcts.net>
2023-02-24 11:07:59 +0000

    RDCC-6413:Changes made to add ManagerOrg

---
+++ src/main/resources/db/migration/V11__add_manage_org_roles_enums.sql
@@ -0,0 +1,3 @@
+--RDCC-6413 ManageOrg roles
+INSERT INTO PRD_ENUM (ENUM_CODE, ENUM_NAME, ENUM_TYPE, ENUM_DESC, ENABLED) VALUES (47, 'caseworker-employment', 'CCD_ROLE', 'Case Worker Employment Tribunals', 'YES');
+INSERT INTO PRD_ENUM (ENUM_CODE, ENUM_NAME, ENUM_TYPE, ENUM_DESC, ENABLED) VALUES (48, 'caseworker-employment-legalrep-solicitor', 'CCD_ROLE', 'Case Worker Employment Tribunals Legal Rep Solicitor', 'YES');
