b4fc301d4881f88b985b8238433096d45ee3d5f6
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2023-02-27 17:39:36 +0000

    Add New Roles

---
+++ src/main/resources/db/migration/V11__adding_new_roles.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbrefdata.prd_enum (enum_code, enum_name, enum_type, enum_desc, enabled)
+VALUES(47, 'caseworker-employment', 'CCD_ROLE', 'Case Worker Employment Tribunals', 'YES');
+INSERT INTO dbrefdata.prd_enum (enum_code, enum_name, enum_type, enum_desc, enabled)
+VALUES(48, 'caseworker-employment-legalrep-solicitor', 'CCD_ROLE', 'Case Worker Employment Tribunals Legal Rep
+Solicitor', 'YES');
