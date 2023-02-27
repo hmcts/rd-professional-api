612dc2a881d64fb4c2b2c14e5e3d64b27fd291c9
Sahitya Desireddy <sahitya.desireddy@hmcts.net>
2023-02-27 18:31:54 +0000

    Add New Roles

--- src/integrationTest/resources/db/migration/V14__add_new_roles_abc.sql
+++ src/integrationTest/resources/db/migration/V14__add_new_roles_abc.sql
@@ -1,5 +1,5 @@
 INSERT INTO prd_enum (enum_code, enum_name, enum_type, enum_desc, enabled)
-VALUES(47, 'caseworker-employment', 'CCD_ROLE', 'Case Worker Employment Tribunals', 'YES');
+VALUES(49, 'caseworker-employment', 'CCD_ROLE', 'Case Worker Employment Tribunals', 'YES');
 INSERT INTO prd_enum (enum_code, enum_name, enum_type, enum_desc, enabled)
-VALUES(48, 'caseworker-employment-legalrep-solicitor', 'CCD_ROLE', 'Case Worker Employment Tribunals Legal Rep
+VALUES(50, 'caseworker-employment-legalrep-solicitor', 'CCD_ROLE', 'Case Worker Employment Tribunals Legal Rep
 Solicitor', 'YES');
