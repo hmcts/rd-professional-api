84ee0e851d6e4d0fea642fb8b3ccbaa132d06dcb
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-09-01 13:34:16 +0100

    DTSRD-1173-latest

--- src/contractTest/java/uk/gov/hmcts/reform/professionalapi/WebMvcProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/professionalapi/WebMvcProviderTest.java
@@ -3,8 +3,6 @@ package uk.gov.hmcts.reform.professionalapi;
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import org.junit.jupiter.api.BeforeEach;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
 import uk.gov.hmcts.reform.professionalapi.controller.external.OrganisationExternalController;
 
 public class WebMvcProviderTest extends BaseProviderTest {
