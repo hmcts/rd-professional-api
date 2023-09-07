e0f73d5c80e344b8bd5dd95a4e139e84e1873b1f
hmcts <prudhvi.maddineni@hmcts.net>
2023-09-07 11:03:46 +0100

    Sonar Qube issue

--- src/main/java/uk/gov/hmcts/reform/professionalapi/service/impl/OrganisationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/service/impl/OrganisationServiceImpl.java
@@ -85,6 +85,7 @@ import static java.lang.Boolean.TRUE;
 import static org.apache.commons.lang3.ObjectUtils.isNotEmpty;
 import static org.springframework.util.CollectionUtils.isEmpty;
 import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiConstants.ERROR_MSG_PARTIAL_SUCCESS;
+import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiConstants.FALSE;
 import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiConstants.LENGTH_OF_ORGANISATION_IDENTIFIER;
 import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiConstants.LOG_ERROR_BODY_START;
 import static uk.gov.hmcts.reform.professionalapi.controller.constants.ProfessionalApiConstants.NO_ORG_FOUND_FOR_GIVEN_ID;
@@ -341,7 +342,7 @@ public class OrganisationServiceImpl implements OrganisationService {
                     .toList());
             updatedOrganisationDetails = RefDataUtil.getMultipleUserProfilesFromUp(userProfileFeignClient,
                     retrieveUserProfilesRequest,
-                    "false", activeOrganisationDtls);
+                    FALSE, activeOrganisationDtls);
 
         }
         return updatedOrganisationDetails;
@@ -395,7 +396,7 @@ public class OrganisationServiceImpl implements OrganisationService {
                     .toList());
             updatedActiveOrganisations = RefDataUtil.getMultipleUserProfilesFromUp(userProfileFeignClient,
                     retrieveUserProfilesRequest,
-                    "false", activeOrganisationDetails);
+                FALSE, activeOrganisationDetails);
         }
 
         resultingOrganisations.addAll(pendingOrganisations);
@@ -459,7 +460,7 @@ public class OrganisationServiceImpl implements OrganisationService {
                     .toList());
             updatedActiveOrganisations = RefDataUtil.getMultipleUserProfilesFromUp(userProfileFeignClient,
                     retrieveUserProfilesRequest,
-                    "false", activeOrganisationDetails);
+                FALSE, activeOrganisationDetails);
         }
 
         resultingOrganisations.addAll(pendingOrganisations);
