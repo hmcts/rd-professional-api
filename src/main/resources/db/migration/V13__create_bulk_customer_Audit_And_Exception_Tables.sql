5a6f3926e7902197bd09fbe4fba3426b212ac4fa
hmcts <prudhvi.maddineni@hmcts.net>
2023-09-20 16:39:45 +0100

    flyway script

--- src/main/resources/db/migration/V13__create_bulk_customer_Audit_And_Exception_Tables.sql
+++ src/main/resources/db/migration/V13__create_bulk_customer_Audit_And_Exception_Tables.sql
@@ -1,25 +1,22 @@
-create table dataload_schedular_audit(
-    id serial4 NOT NULL,
-    scheduler_name varchar(64) not null,
-    file_name varchar(128),
-    scheduler_start_time TIMESTAMP NOT NULL,
-    scheduler_end_time TIMESTAMP ,
-    status VARCHAR(32),
-    constraint dataload_schedular_audit_pk primary key (id)
+CREATE TABLE dataload_schedular_audit(
+  id serial NOT NULL,
+  scheduler_name varchar(64) NOT NULL,
+  file_name varchar(128),
+  scheduler_start_time timestamp NOT NULL,
+  scheduler_end_time timestamp,
+  status varchar(32),
+  CONSTRAINT dataload_schedular_audit_pk PRIMARY KEY (id)
 );
 
-
-create table dataload_exception_records(
-    id serial4 NOT NULL,
-    table_name varchar(64) NULL,
-    scheduler_name varchar(64) NOT NULL,
-    scheduler_start_time TIMESTAMP NOT NULL,
-    "key" varchar(256) NULL,
-    field_in_error varchar(256) NULL,
-    error_description varchar(512) NULL,
-    updated_timestamp TIMESTAMP NOT NULL,
-    row_id int8 NULL,
-    constraint dataload_exception_records_pk primary key (id)
+CREATE TABLE dataload_exception_records(
+ id SERIAL NOT NULL,
+ table_Name varchar(64),
+ scheduler_name varchar(64) NOT NULL,
+ scheduler_start_time timestamp NOT NULL,
+ "key" varchar(256),
+ field_in_error varchar(256),
+ error_description varchar(512),
+ updated_timestamp timestamp NOT NULL,
+ row_id bigint,
+ CONSTRAINT dataload_exception_records_pk PRIMARY KEY (ID)
 );
-
-COMMIT;
