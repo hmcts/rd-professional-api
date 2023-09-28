9303c21ef7f4b2d0d1bfd098dca7f5ee0acb4001
arshinsalim <arshin.salim@hmcts.net>
2023-09-28 13:46:00 +0100

    prd executer

--- src/test/java/uk/gov/hmcts/reform/professionalapi/util/PrdExecutorTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/util/PrdExecutorTest.java
@@ -6,9 +6,9 @@ import org.apache.camel.impl.DefaultCamelContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.professionalapi.dataload.service.AuditServiceImpl;
+import uk.gov.hmcts.reform.professionalapi.dataload.util.DataLoadUtil;
 import uk.gov.hmcts.reform.professionalapi.dataload.util.PrdDataExecutor;
 
 import static org.mockito.Mockito.doNothing;
@@ -31,6 +31,8 @@ class PrdExecutorTest {
 
     ProducerTemplate producerTemplate = mock(ProducerTemplate.class);
 
+    DataLoadUtil dataLoadUtil = mock(DataLoadUtil.class);
+
     @BeforeEach
     void init() {
         setField(prdDataExecutorSpy, "auditService", auditService);
@@ -38,7 +40,8 @@ class PrdExecutorTest {
 
     @Test
     void testExecute() {
-        doNothing().when(producerTemplate).sendBody(Mockito.any());
+        setField(prdDataExecutorSpy, "dataLoadUtil", dataLoadUtil);
+        setField(prdDataExecutorSpy, "producerTemplate", producerTemplate);
         doNothing().when(auditService).auditSchedulerStatus(camelContext);
         prdDataExecutorSpy.execute(camelContext, "test", "test");
         verify(prdDataExecutorSpy, times(1)).execute(camelContext, "test", "test");
@@ -46,7 +49,8 @@ class PrdExecutorTest {
 
     @Test
     void testExecute_AuditDisabled() {
-        doNothing().when(producerTemplate).sendBody(Mockito.any());
+        setField(prdDataExecutorSpy, "dataLoadUtil", dataLoadUtil);
+        setField(prdDataExecutorSpy, "producerTemplate", producerTemplate);
         doNothing().when(auditService).auditSchedulerStatus(camelContext);
         camelContext.getGlobalOptions().put(IS_READY_TO_AUDIT, Boolean.FALSE.toString());
         prdDataExecutorSpy.execute(camelContext, "test", "test");
@@ -56,7 +60,8 @@ class PrdExecutorTest {
 
     @Test
     void testExecute_AuditEnabled() {
-        doNothing().when(producerTemplate).sendBody(Mockito.any());
+        setField(prdDataExecutorSpy, "dataLoadUtil", dataLoadUtil);
+        setField(prdDataExecutorSpy, "producerTemplate", producerTemplate);
         doNothing().when(auditService).auditSchedulerStatus(camelContext);
         camelContext.getGlobalOptions().put(IS_READY_TO_AUDIT, Boolean.TRUE.toString());
         prdDataExecutorSpy.execute(camelContext, "test", "test");
@@ -66,7 +71,8 @@ class PrdExecutorTest {
 
     @Test
     void testExecute_NoAuditPreference() {
-        doNothing().when(producerTemplate).sendBody(Mockito.any());
+        setField(prdDataExecutorSpy, "dataLoadUtil", dataLoadUtil);
+        setField(prdDataExecutorSpy, "producerTemplate", producerTemplate);
         doNothing().when(auditService).auditSchedulerStatus(camelContext);
         camelContext.getGlobalOptions().put(IS_READY_TO_AUDIT, null);
         prdDataExecutorSpy.execute(camelContext, "test", "test");
@@ -76,6 +82,8 @@ class PrdExecutorTest {
 
     @Test
     void testExecuteException() {
+        setField(prdDataExecutorSpy, "dataLoadUtil", dataLoadUtil);
+        setField(prdDataExecutorSpy, "producerTemplate", producerTemplate);
         doNothing().when(auditService).auditSchedulerStatus(camelContext);
         prdDataExecutorSpy.execute(camelContext, "test", "test");
         verify(prdDataExecutorSpy, times(1)).execute(camelContext, "test", "test");
