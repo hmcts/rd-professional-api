37c97a2486f1b22f039deeb7488995ddeae1f9dc
arshinsalim <arshin.salim@hmcts.net>
2023-10-02 08:40:22 +0100

    prd executer

--- src/integrationTest/resources/db/migration/V15__create_table_user_detail.sql
+++
@@ -1,6 +0,0 @@
--- dataload_schedular_job definition
-CREATE TABLE user_details(
-    id bigint ,
-    first_name VARCHAR(16),
-    last_name VARCHAR(16)
-);
--- src/main/resources/db/migration/V13__schedlock_scheduler_detail.sql
+++
@@ -1,3 +0,0 @@
---Created table for lock details
-CREATE TABLE lock_details_provider(name VARCHAR(64) NOT NULL, lock_until TIMESTAMP NOT NULL,
-    locked_at TIMESTAMP NOT NULL, locked_by VARCHAR(255) NOT NULL, PRIMARY KEY (name));
