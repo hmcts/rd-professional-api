9013c2f8c750b59005d28ebe87740f5ccf04fa09
arshinsalim <arshin.salim@hmcts.net>
2023-10-02 08:40:43 +0100

    prd executer

--- src/integrationTest/resources/db/migration/V12__scheduler_job_config.sql
+++ src/integrationTest/resources/db/migration/V12__scheduler_job_config.sql
@@ -5,3 +5,6 @@ CREATE TABLE dataload_schedular_job(
     job_start_time TIMESTAMP NOT NULL,
     job_end_time TIMESTAMP
 );
+
+CREATE TABLE lock_details_provider(name VARCHAR(64) NOT NULL, lock_until TIMESTAMP NOT NULL,
+    locked_at TIMESTAMP NOT NULL, locked_by VARCHAR(255) NOT NULL, PRIMARY KEY (name));
--- src/integrationTest/resources/db/migration/V14__create_table_bulk_customer_detail.sql
+++ src/integrationTest/resources/db/migration/V14__create_table_bulk_customer_detail.sql
@@ -17,25 +17,3 @@ create table bulk_customer_details(
     references organisation (id)
 );
 
-CREATE TABLE dataload_exception_records(
- id SERIAL NOT NULL,
- table_Name varchar(64),
- scheduler_name varchar(64) NOT NULL,
- scheduler_start_time timestamp NOT NULL,
- key varchar(256),
- field_in_error varchar(256),
- error_description varchar(512),
- updated_timestamp timestamp NOT NULL,
- row_id bigint,
- CONSTRAINT dataload_exception_records_pk PRIMARY KEY (ID)
-);
-
-CREATE TABLE dataload_schedular_audit(
-  id serial NOT NULL,
-  scheduler_name varchar(64) NOT NULL,
-  file_name varchar(128),
-  scheduler_start_time timestamp NOT NULL,
-  scheduler_end_time timestamp,
-  status varchar(32),
-  CONSTRAINT dataload_schedular_audit_pk PRIMARY KEY (id)
-);
