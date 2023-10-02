f2edd77dcbba5afb064f26c3f2e844b93e23ecdb
manukundloo-hmcts <Manukundloo.sinha@hmcts.net>
2023-10-02 23:03:24 +0100

    [RDCC-7059]fix build failure

--- src/main/resources/db/migration/V15__create_singleton_org_type.sql
+++ src/main/resources/db/migration/V15__create_singleton_org_type.sql
@@ -1,5 +1,5 @@
 CREATE TABLE singleton_org_type (
-            id serial4 NOT NULL,
+            id serial NOT NULL,
             org_type varchar(256) NOT NULL,
             CONSTRAINT singleton_org_type_pk PRIMARY KEY (id),
             CONSTRAINT org_type_uk UNIQUE (org_type));
