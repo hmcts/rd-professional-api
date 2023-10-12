40bd38f9f9cce28bfdf2a33ba9c4e16039dc3d12
hmcts <prudhvi.maddineni@hmcts.net>
2023-10-12 16:57:26 +0100

    regex modification

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/constants/ProfessionalApiConstants.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/constants/ProfessionalApiConstants.java
@@ -17,8 +17,7 @@ public class ProfessionalApiConstants {
     public static final String COMMON_NAME_AND_SEARCH_REGEX = "^[(a-zA-Z0-9 )\\p{L}\\p{N}'’-]";
 
     public static final String EMAIL_SEARCH_REGEX = "[@_'`.\\+-]";
-    public static final String SEARCHSTRING_FORMAT_REGEX = COMMON_NAME_AND_SEARCH_REGEX + "{3,}$"
-                                        + EMAIL_SEARCH_REGEX;
+    public static final String SEARCHSTRING_FORMAT_REGEX = EMAIL_SEARCH_REGEX + COMMON_NAME_AND_SEARCH_REGEX + "{3,}$";
     public static final String NO_ORG_FOUND_FOR_GIVEN_ID
             = "No Organisation was found with the given organisationIdentifier ";
     public static final String ORG_NOT_ACTIVE_NO_USERS_RETURNED
