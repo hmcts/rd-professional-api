0df72fd7fd130aa232f58aaccda484487d6810aa
SabinaHMCTS <139119493+SabinaHMCTS@users.noreply.github.com>
2023-10-17 13:07:03 +0100

    DTSRD-1242.Upgrade to postgres V15 . - merge demo to perftest (#1471)
    * DTSRD-1239.Postgres upgrade v15 (#1464)
    
    * DTSRD-1017.commit suppression
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade v15 (#1465)
    
    * DTSRD-1017.commit suppression
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1239.Postgres upgrade to V15
    
    * DTSRD-1466.Remove branches to sync -location demo and master  (#1468) (#1469)
    
    * DTSRD-1017.commit suppression
    
    * DTSRD-1239.Remove branches to sync -professional demo and master

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -2,13 +2,20 @@ provider "azurerm" {
   features {}
 }
 
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  alias                      = "postgres_network"
+  subscription_id            = var.aks_subscription_id
+}
+
 terraform {
   backend "azurerm" {}
 
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "~> 3.40.0"
+      version = "~> 3.72.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
