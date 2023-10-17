f8a4d89afb05f3e8864dfa87deb3b3e5c9e3d2b6
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2023-10-17 14:54:56 +0100

    DTSRD-1242-1.Remove branches to sync -professional demo and master

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,10 +2,6 @@ variable "product" {
   type = string
 }
 
-variable "product-V15" {
-  type = string
-  default="rd-professional-api"
-}
 
 variable "raw_product" {
   type    = string
@@ -14,11 +10,6 @@ variable "raw_product" {
 
 variable "component" {
   type = string
-}
-
-variable "component-V15" {
-  type = string
-  default="postgres-db-v15"
 }
 
 variable "location" {
@@ -41,51 +32,4 @@ variable "common_tags" {
 variable "team_name" {
   type    = string
   default = "RD"
-}
-
-variable "aks_subscription_id" {
-}
-variable "team_contact" {
-  type        = string
-  description = "The name of your Slack channel people can use to contact your team about your infrastructure"
-  default     = "#refdata-pet"
-}
-variable "destroy_me" {
-  type        = string
-  description = "In the future if this is set to Yes then automation will delete this resource on a schedule. Please set to No unless you know what you are doing"
-  default     = "No"
-}
-variable "sku" {
-  type        = string
-  default     = "Premium"
-  description = "SKU type(Basic, Standard and Premium)"
-}
-
-variable "tenant_id" {
-  type        = string
-  description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environment variables and not normally required to be specified."
-}
-
-variable "jenkins_AAD_objectId" {
-  type        = string
-  description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
-}
-
-variable "pgsql_server_configuration" {
-  description = "Postgres server configuration"
-  type        = list(object({ name : string, value : string }))
-  default = [
-    {
-      name  = "azure.extensions"
-      value = "plpgsql"
-    },
-    {
-      name  = "azure.extensions"
-      value = "pg_stat_statements"
-    },
-    {
-      name  = "azure.extensions"
-      value = "pg_buffercache"
-    }
-  ]
 }
