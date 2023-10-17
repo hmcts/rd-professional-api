943495ce5c8a16aee480f964b270bc0f9387700d
SabinaHMCTS <139119493+SabinaHMCTS@users.noreply.github.com>
2023-10-17 14:58:12 +0100

    Update terraform.tf
    Reverting V15 changes done to perftest

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -2,13 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-provider "azurerm" {
-  features {}
-  skip_provider_registration = true
-  alias                      = "postgres_network"
-  subscription_id            = var.aks_subscription_id
-}
-
 terraform {
   backend "azurerm" {}
 
