c85b0aefefdb5d0da7fc69cb66d49ce5fc246699
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-11-22 10:48:40 +0000

    Fix Prd Enum table issue

--- src/main/java/uk/gov/hmcts/reform/professionalapi/domain/PrdEnum.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/domain/PrdEnum.java
@@ -8,10 +8,12 @@ import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
 @NoArgsConstructor
 @Entity(name = "prd_enum")
+@Table(name = "prd_enum")
 public class PrdEnum implements Serializable {
 
     @EmbeddedId
