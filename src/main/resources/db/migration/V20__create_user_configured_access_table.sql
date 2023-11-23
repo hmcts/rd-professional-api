745afca9b896b6a6451c8edf6d7d7176f8560407
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2023-11-23 16:09:09 +0000

    GA-22: db and entities

---
+++ src/main/resources/db/migration/V20__create_user_configured_access_table.sql
@@ -0,0 +1,12 @@
+-- dataload_schedular_job definition
+create table user_configured_access(
+	professional_user_id uuid NOT NULL,
+	jurisdiction_id varchar(255) NOT NULL,
+	organisation_profile_id  varchar(255) NOT NULL,
+	access_type_id  varchar(255) NOT NULL,
+	enabled boolean,
+	constraint user_configured_access_pk primary key (professional_user_id, jurisdiction_id, organisation_profile_id, access_type_id)
+);
+
+alter table user_configured_access add constraint user_configured_access_fk1 foreign key (professional_user_id)
+references professional_user (id) on delete cascade;
