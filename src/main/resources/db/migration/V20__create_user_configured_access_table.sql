b746fa7843dbc9f7ca84f1a74cf7fab73b41773b
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2023-11-24 15:04:02 +0000

    GA-24: updated req models

--- src/main/resources/db/migration/V20__create_user_configured_access_table.sql
+++ src/main/resources/db/migration/V20__create_user_configured_access_table.sql
@@ -1,9 +1,9 @@
 -- dataload_schedular_job definition
 create table user_configured_access(
 	professional_user_id uuid NOT NULL,
-	jurisdiction_id varchar(255) NOT NULL,
-	organisation_profile_id  varchar(255) NOT NULL,
-	access_type_id  varchar(255) NOT NULL,
+	jurisdiction_id varchar(70) NOT NULL,
+	organisation_profile_id  varchar(200) NOT NULL,
+	access_type_id  varchar(200) NOT NULL,
 	enabled boolean,
 	constraint user_configured_access_pk primary key (professional_user_id, jurisdiction_id, organisation_profile_id, access_type_id)
 );
