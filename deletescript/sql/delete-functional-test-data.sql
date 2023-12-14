25ff45c59afb85f4a23d248aafdd201e91ee3d4d
raj katla <raj.katla1@hmcts.net>
2023-12-14 14:50:05 +0000

    DTSRD-1873: Clean test data - Refactor

--- deletescript/sql/delete-functional-test-data.sql
+++ deletescript/sql/delete-functional-test-data.sql
@@ -1,78 +1,148 @@
-delete from user_attribute
-where professional_user_id in
-(
-select id from professional_user where organisation_id in (
-select id from organisation where company_number like '%com'
-and company_url like '%company-url'
-and sra_id like '%sra-id-number1'
-)
-);
+delete
+from
+    user_attribute u
+where
+        u.professional_user_id in
+        (
+            select
+                pu.id
+            from
+                professional_user pu
+                    join organisation o on
+                        pu.organisation_id = o.id
+            where
+                    o.company_url like '%prd-func-test-company-url'
+              and o."name" like '%prd-func-test-name'
+              and o.sra_id like '%prd-func-test-sra-id'
+        );
 
-delete from user_account_map
-where professional_user_id in
-(
- select up.id from organisation org , professional_user up
- where up.organisation_id in (select id from organisation where company_number like '%com'
-and company_url like '%company-url'
-and sra_id like '%sra-id-number1')
-);
+delete
+from
+    user_account_map uam
+where
+        uam.professional_user_id in
+        (
+            select
+                pu.id
+            from
+                professional_user pu
+                    join organisation o on
+                        pu.organisation_id = o.id
+            where
+                    o.company_url like '%prd-func-test-company-url'
+              and o."name" like '%prd-func-test-name'
+              and o.sra_id like '%prd-func-test-sra-id'
+        );
 
-delete from payment_account
-where organisation_id in
-(
- select id from organisation where company_number like '%com'
-and company_url like '%company-url'
-and sra_id like '%sra-id-number1'
-);
+delete
+from
+    payment_account pa
+where
+        pa.organisation_id in
+        (
+            select
+                o.id
+            from
+                organisation o
+            where
+                    o.company_url like '%prd-func-test-company-url'
+              and o."name" like '%prd-func-test-name'
+              and o.sra_id like '%prd-func-test-sra-id'
+        );
 
-delete from dx_address
-where contact_information_id in
-(
- select contactinfo.id from organisation org , contact_information contactinfo, professional_user up
- where up.organisation_id in (select id from organisation where company_number like '%com'
-and company_url like '%company-url'
-and sra_id like '%sra-id-number1')
-);
+delete
+from
+    dx_address dx
+where
+        dx.contact_information_id in
+        (
+            select
+                c.id
+            from
+                organisation o
+                    join contact_information c on
+                        o.id = c.organisation_id
+            where
+                    o.company_url like '%prd-func-test-company-url'
+              and o."name" like '%prd-func-test-name'
+              and o.sra_id like '%prd-func-test-sra-id'
+        );
 
-delete from contact_information
-where organisation_id in
-(
- select id from organisation where company_number like '%com'
-and company_url like '%company-url'
-and sra_id like '%sra-id-number1'
-);
+delete
+from
+    contact_information c
+where
+        c.organisation_id in
+        (
+            select
+                o.id
+            from
+                organisation o
+            where
+                    o.company_url like '%prd-func-test-company-url'
+              and o."name" like '%prd-func-test-name'
+              and o.sra_id like '%prd-func-test-sra-id'
+        );
 
-delete from organisation_mfa_status
-where organisation_id in
-(
-select id from organisation where company_number like '%com'
-and company_url like '%company-url'
-and sra_id like '%sra-id-number1'
+delete
+from
+    organisation_mfa_status mfa
+where
+        mfa.organisation_id in
+        (
+            select
+                o.id
+            from
+                organisation o
+            where
+                    o.company_url like '%prd-func-test-company-url'
+              and o."name" like '%prd-func-test-name'
+              and o.sra_id like '%prd-func-test-sra-id'
 
-);
+        );
 
-delete from professional_user
-where organisation_id in
-(
-select id from organisation where company_number like '%com'
-and company_url like '%company-url'
-and sra_id like '%sra-id-number1'
+delete
+from
+    professional_user pu
+where
+        pu.organisation_id in
+        (
+            select
+                o.id
+            from
+                organisation o
+            where
+                    o.company_url like '%prd-func-test-company-url'
+              and o."name" like '%prd-func-test-name'
+              and o.sra_id like '%prd-func-test-sra-id'
 
-);
+        );
 
 
-delete from org_attributes
-where organisation_id in
-(
- select id from organisation where company_number like '%com'
-and company_url like '%company-url'
-and sra_id like '%sra-id-number1'
-);
+delete
+from
+    org_attributes oa
+where
+        oa.organisation_id in
+        (
+            select
+                o.id
+            from
+                organisation o
+            where
+                    o.company_url like '%prd-func-test-company-url'
+              and o."name" like '%prd-func-test-name'
+              and o.sra_id like '%prd-func-test-sra-id'
+
+        );
 
 
-delete from organisation
-where company_number like '%com'
-and company_url like '%company-url'
-and sra_id like '%sra-id-number1';
+delete
+from
+    organisation o
+where
+        o.company_url like '%prd-func-test-company-url'
+  and o."name" like '%prd-func-test-name'
+  and o.sra_id like '%prd-func-test-sra-id';
 
 commit;
