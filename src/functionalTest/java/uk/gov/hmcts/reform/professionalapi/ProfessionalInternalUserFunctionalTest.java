40ce406d07e2501aa0770ba2c5544b2121419101
raj katla <raj.katla1@hmcts.net>
2023-12-19 16:59:36 +0000

    DTSRD-1886: fixed failed functional test

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/ProfessionalInternalUserFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/ProfessionalInternalUserFunctionalTest.java
@@ -844,8 +844,8 @@ class ProfessionalInternalUserFunctionalTest extends AuthorizationFunctionalTest
                 .sorted(Comparator.comparing(map -> (String) map.get("addressLine1")))
                 .collect(Collectors.toList());
 
-        Map<String,Object> contactInformation1 = contactInformation.get(0);
-        Map<String,Object> contactInformation2 = contactInformation.get(1);
+        final Map<String,Object> contactInformation1 = contactInformation.get(0);
+        final Map<String,Object> contactInformation2 = contactInformation.get(1);
 
         String firstAddressUprn = (String) contactInformation1.get("uprn");
         assertThat(firstAddressUprn)
@@ -903,7 +903,8 @@ class ProfessionalInternalUserFunctionalTest extends AuthorizationFunctionalTest
                 (List<Map<String, String>>) contactInformation1.get("dxAddress");
         assertThat(firstAddressDxAddress)
                 .isNotNull()
-                .hasSize(2);
+                .hasSize(3);
+        verifyDxAddressDetails(firstAddressDxAddress);
 
         String secondAddressUprn = (String) contactInformation2.get("uprn");
         assertThat(secondAddressUprn)
@@ -959,5 +960,43 @@ class ProfessionalInternalUserFunctionalTest extends AuthorizationFunctionalTest
                 .isNotNull()
                 .hasSize(3);
 
+        verifyDxAddressDetails(secondAddressDxAddress);
+
+    }
+
+    private static void verifyDxAddressDetails(List<Map<String, String>> secondAddressDxAddress) {
+        Map<String, String> secondAddressDxAddress1 = secondAddressDxAddress.get(0);
+        Map<String, String> secondAddressDxAddress2 = secondAddressDxAddress.get(1);
+        Map<String, String> secondAddressDxAddress3 = secondAddressDxAddress.get(2);
+
+        String secondAddressDxNumber1  = secondAddressDxAddress1.get("dxNumber");
+        assertThat(secondAddressDxNumber1)
+                .isNotNull()
+                .isEqualTo("DX 1234567890");
+
+        String secondAddressDxExchange1 = secondAddressDxAddress1.get("dxExchange");
+        assertThat(secondAddressDxExchange1)
+                .isNotNull()
+                .isEqualTo("dxExchange");
+
+        String secondAddressDxNumber2  = secondAddressDxAddress2.get("dxNumber");
+        assertThat(secondAddressDxNumber2)
+                .isNotNull()
+                .isEqualTo("DX 123456777");
+
+        String secondAddressDxExchange2 = secondAddressDxAddress2.get("dxExchange");
+        assertThat(secondAddressDxExchange2)
+                .isNotNull()
+                .isEqualTo("dxExchange");
+
+        String secondAddressDxNumber3  = secondAddressDxAddress3.get("dxNumber");
+        assertThat(secondAddressDxNumber3)
+                .isNotNull()
+                .isEqualTo("DX 123456788");
+
+        String secondAddressDxExchange3 = secondAddressDxAddress3.get("dxExchange");
+        assertThat(secondAddressDxExchange3)
+                .isNotNull()
+                .isEqualTo("dxExchange");
     }
 }
