cf4ee7f6e0fce268d0746c8b0b0aba96ac93116a
sanjayparekh-hmcts <sanjay.parekh@hmcts.net>
2024-01-12 11:47:41 +0000

    Revert "GA-97: improved tests"
    This reverts commit f0a88a0478936ee01a301f905f21e86998d3f923.

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/AuthorizationFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/AuthorizationFunctionalTest.java
@@ -19,7 +19,6 @@ import uk.gov.hmcts.reform.professionalapi.controller.request.NewUserCreationReq
 import uk.gov.hmcts.reform.professionalapi.controller.request.OrganisationCreationRequest;
 import uk.gov.hmcts.reform.professionalapi.controller.request.OrganisationOtherOrgsCreationRequest;
 import uk.gov.hmcts.reform.professionalapi.domain.RoleName;
-import uk.gov.hmcts.reform.professionalapi.domain.UserAccessType;
 import uk.gov.hmcts.reform.professionalapi.domain.UserProfileUpdatedData;
 import uk.gov.hmcts.reform.professionalapi.idam.IdamOpenIdClient;
 
@@ -190,14 +189,11 @@ public class AuthorizationFunctionalTest {
         String userEmail = generateRandomEmail();
         String lastName = "someLastName";
         String firstName = "someFirstName";
-        Set<UserAccessType> userAccessTypes = new HashSet<>();
-        userAccessTypes.add(new UserAccessType("jurisdictionId", "organisationProfileId", "accessTypeId", false));
         NewUserCreationRequest userCreationRequest = aNewUserCreationRequest()
                 .firstName(firstName)
                 .lastName(lastName)
                 .email(userEmail)
                 .roles(userRoles)
-                .userAccessTypes(userAccessTypes)
                 .build();
         return userCreationRequest;
     }
