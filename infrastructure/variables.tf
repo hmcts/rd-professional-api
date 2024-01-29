9c7df0d16702bc8c6864ed02e471d8839dedb11b
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-01-29 09:51:11 +0000

    Adding force_user_permissions_trigger to get access to DTS JIT Access rd DB Reader SC

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -73,6 +73,12 @@ variable "tenant_id" {
   description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environment variables and not normally required to be specified."
 }
 
+variable "force_user_permissions_trigger" {
+  default     = ""
+  type        = string
+  description = "Update this to a new value to force the user permissions script to run again"
+}
+
 variable "jenkins_AAD_objectId" {
   type        = string
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
