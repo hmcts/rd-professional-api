f4c408276309e5c318db8d47e5052352b57c78e2
Abul Syed <abul.syed@HMCTS.net>
2024-02-01 12:50:11 +0000

    test new pact

--- src/contractTest/java/uk/gov/hmcts/reform/professionalapi/provider/ProfessionalUserInternalControllerProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/professionalapi/provider/ProfessionalUserInternalControllerProviderTest.java
@@ -5,14 +5,14 @@ import au.com.dius.pact.provider.junitsupport.State;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ContextConfiguration;
 import uk.gov.hmcts.reform.professionalapi.WebMvcProviderTest;
 import uk.gov.hmcts.reform.professionalapi.configuration.WebConfig;
 import uk.gov.hmcts.reform.professionalapi.controller.internal.ProfessionalUserInternalController;
 import uk.gov.hmcts.reform.professionalapi.domain.Organisation;
 import uk.gov.hmcts.reform.professionalapi.domain.ProfessionalUser;
-import uk.gov.hmcts.reform.professionalapi.domain.UserConfiguredAccess;
-import uk.gov.hmcts.reform.professionalapi.domain.UserConfiguredAccessId;
 import uk.gov.hmcts.reform.professionalapi.repository.ProfessionalUserRepository;
 
 import java.time.LocalDateTime;
@@ -20,8 +20,10 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static uk.gov.hmcts.reform.professionalapi.pact.util.PactUtils.getMinimalOrganisation;
+import static uk.gov.hmcts.reform.professionalapi.pact.util.PactUtils.getUserConfiguredAccesses;
 
 @Provider("referenceData_professionalInternalUsers")
 @WebMvcTest({ProfessionalUserInternalController.class})
@@ -34,6 +36,26 @@ public class ProfessionalUserInternalControllerProviderTest extends WebMvcProvid
 
     @State({"A user identifier for a PRD internal user request"})
     public void toRetrieveRefreshUserUsingUserIdentifier() {
+        ProfessionalUser professionalUser = setupProfessionalUserWithOrganisation();
+
+        when(professionalUserRepositoryMock.findByUserIdentifier(any(String.class)))
+                .thenReturn(professionalUser);
+    }
+
+    @SuppressWarnings("unchecked")
+    @State({"A since, searchAfter & page size for a PRD internal user request"})
+    public void toRetrieveRefreshUserUsingSinceSearchAfterAndPagination() {
+        ProfessionalUser professionalUser = setupProfessionalUserWithOrganisation();
+
+        Page<ProfessionalUser> professionalUserPage = (Page<ProfessionalUser>) mock(Page.class);
+
+        when(professionalUserRepositoryMock.findByLastUpdatedGreaterThanEqualAndIdGreaterThan(
+                any(LocalDateTime.class), any(UUID.class), any(Pageable.class))
+        ).thenReturn(professionalUserPage);
+        when(professionalUserPage.getContent()).thenReturn(List.of(professionalUser));
+    }
+
+    private ProfessionalUser setupProfessionalUserWithOrganisation() {
         Organisation organisation = getMinimalOrganisation();
         organisation.setLastUpdated(LocalDateTime.now());
 
@@ -43,22 +65,8 @@ public class ProfessionalUserInternalControllerProviderTest extends WebMvcProvid
         professionalUser.setUserIdentifier("123");
         professionalUser.setId(UUID.randomUUID());
         professionalUser.setLastUpdated(LocalDateTime.now());
+        professionalUser.setUserConfiguredAccesses(getUserConfiguredAccesses(professionalUser));
 
-        UserConfiguredAccessId userConfiguredAccessId = new UserConfiguredAccessId(
-                professionalUser,
-                "CIVIL",
-                "SOLICITOR_PROFILE",
-                "123"
-        );
-
-        UserConfiguredAccess userConfiguredAccess = new UserConfiguredAccess(
-                userConfiguredAccessId, true
-        );
-
-        professionalUser.setUserConfiguredAccesses(List.of(userConfiguredAccess));
-
-        when(professionalUserRepositoryMock.findByUserIdentifier(any(String.class)))
-                .thenReturn(professionalUser);
+        return professionalUser;
     }
-
 }
