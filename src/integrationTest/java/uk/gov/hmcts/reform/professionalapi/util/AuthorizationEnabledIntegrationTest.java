dfd4ff6f30aa232b613e525b63cdcad2f8c8cbe3
Kiran Kumar Yenigala <51748133+kiran-yenigala-hmcts@users.noreply.github.com>
2024-02-13 14:46:04 +0000

    DTSRD-2103: Sorting of Active Orgs Users (#1553)
    * DTSRD-2103: Sorting of Active Orgs Users
    
    * DTSRD-2103: Sorting of Active Orgs Users
    
    * DTSRD-2103: Sorting of Active Orgs Users
    
    * DTSRD-2103: Sorting of Active Orgs Users
    
    * DTSRD-2103: Sorting of Active Orgs Users
    
    * DTSRD-2103: Sorting of Active Orgs Users
    
    * DTSRD-2103: fix sonar issues
    
    * DTSRD-2103: Sorting of Active Orgs Users
    
    * DTSRD-2103: Sorting of Active Orgs Users
    
    * DTSRD-2103: Sorting of Active Orgs Users
    
    ---------
    
    Co-authored-by: SabinaHMCTS <139119493+SabinaHMCTS@users.noreply.github.com>

--- src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/util/AuthorizationEnabledIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/util/AuthorizationEnabledIntegrationTest.java
@@ -371,10 +371,10 @@ public abstract class AuthorizationEnabledIntegrationTest extends SpringBootInte
         return orgIdentifier;
     }
 
-    public NewUserCreationRequest inviteUserCreationRequest(String userEmail, List<String> userRoles) {
-
-        String lastName = "someLastName";
-        String firstName = "1Aaron";
+    public NewUserCreationRequest inviteUserCreationRequest(String userEmail,
+                                                            String lastName,
+                                                            String firstName,
+                                                            List<String> userRoles) {
         NewUserCreationRequest userCreationRequest = aNewUserCreationRequest()
                 .firstName(firstName)
                 .lastName(lastName)
@@ -383,7 +383,10 @@ public abstract class AuthorizationEnabledIntegrationTest extends SpringBootInte
                 .build();
 
         return userCreationRequest;
+    }
 
+    public NewUserCreationRequest inviteUserCreationRequest(String userEmail, List<String> userRoles) {
+        return inviteUserCreationRequest(userEmail, "someLastName", "1Aaron", userRoles);
     }
 
     public NewUserCreationRequest reInviteUserCreationRequest(String userEmail, List<String> userRoles) {
@@ -411,6 +414,16 @@ public abstract class AuthorizationEnabledIntegrationTest extends SpringBootInte
     }
 
     public void userProfileCreateUserWireMock(HttpStatus status)  {
+        userProfileCreateUserWireMock("testFn", "R", "dummy@email.com", "testFn", "L", "dummy@email.com", status);
+    }
+
+    public void userProfileCreateUserWireMock(String firstName,
+                                              String lastName,
+                                              String email,
+                                              String firstName2,
+                                              String lastName2,
+                                              String email2,
+                                              HttpStatus status)  {
         String body = null;
         int returnHttpStaus = status.value();
         if (status.is2xxSuccessful()) {
@@ -432,9 +445,9 @@ public abstract class AuthorizationEnabledIntegrationTest extends SpringBootInte
                 + "  \"userProfiles\": ["
                 + "  {"
                 + "  \"userIdentifier\":\"%s" + "\","
-                + "  \"firstName\": \"testFn\","
-                + "  \"lastName\": \"R\","
-                + "  \"email\": \"dummy@email.com\","
+                + "  \"firstName\": \"" + firstName + "\","
+                + "  \"lastName\": \"" + lastName + "\","
+                + "  \"email\": \"" + email + "\","
                 + "  \"idamStatus\": \"" + IdamStatus.ACTIVE + "\","
                 + "  \"roles\": ["
                 + "  \"pui-organisation-manager\""
@@ -444,9 +457,9 @@ public abstract class AuthorizationEnabledIntegrationTest extends SpringBootInte
                 + "  },"
                 + "  {"
                 + "  \"userIdentifier\":\" %s" + "\","
-                + "  \"firstName\": \"testFn\","
-                + "  \"lastName\": \"L\","
-                + "  \"email\": \"dummy@email.com\","
+                + "  \"firstName\": \"" + firstName2 + "\","
+                + "  \"lastName\": \"" + lastName2 + "\","
+                + "  \"email\": \"" + email2 + "\","
                 + "  \"idamStatus\": \"" + IdamStatus.ACTIVE + "\","
                 + "  \"roles\": ["
                 + "  \"pui-case-manager\""
@@ -482,9 +495,9 @@ public abstract class AuthorizationEnabledIntegrationTest extends SpringBootInte
                 + "  \"userProfiles\": ["
                 + "  {"
                 + "  \"userIdentifier\":\"%s" + "\","
-                + "  \"firstName\": \"testFN\","
-                + "  \"lastName\": \"R\","
-                + "  \"email\": \"dummy@email.com\","
+                + "  \"firstName\": \"" + firstName + "\","
+                + "  \"lastName\": \"" + lastName + "\","
+                + "  \"email\": \"" + email + "\","
                 + "  \"idamStatus\": \"" + IdamStatus.ACTIVE + "\","
                 + "  \"roles\": [],"
                 + "  \"idamStatusCode\": \"0\","
@@ -492,9 +505,9 @@ public abstract class AuthorizationEnabledIntegrationTest extends SpringBootInte
                 + "  },"
                 + "  {"
                 + "  \"userIdentifier\":\"%s" + "\","
-                + "  \"firstName\": \"testFn\","
-                + "  \"lastName\": \"L\","
-                + "  \"email\": \"dummy@email.com\","
+                + "  \"firstName\": \"" + firstName2 + "\","
+                + "  \"lastName\": \"" + lastName2 + "\","
+                + "  \"email\": \"" + email2 + "\","
                 + "  \"idamStatus\": \"" + IdamStatus.ACTIVE + "\","
                 + "  \"roles\": [],"
                 + "  \"idamStatusCode\": \"0\","
