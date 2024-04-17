1c6db9f32003888ce6ed55e3505fcc3127c583b0
SabinaHMCTS <139119493+SabinaHMCTS@users.noreply.github.com>
2024-04-17 11:01:48 +0100

    Fix azure extensions (#1587)
    * Upgrading perftest branch to V16 and adding subnet_suffix
    
    * FIX AZURE EXTENSIONS
    
    * FIX AZURE EXTENSIONS
    
    * FIX AZURE EXTENSIONS

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -121,6 +121,9 @@ module "db-professional-ref-data-v16" {
   pgsql_version        = "16"
   product              = var.product-v16
   name               = join("-", [var.product-v16, var.component-v16])
+
+  pgsql_server_configuration = var.pgsql_server_configuration
+
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES-USER-v16" {
