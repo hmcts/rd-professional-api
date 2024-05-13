954cf2b769e678897f616a9a7782f2a4990ef51d
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-05-13 10:28:26 +0100

    fixing review comments

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/internal/OrganisationInternalController.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/internal/OrganisationInternalController.java
@@ -794,7 +794,6 @@ public class OrganisationInternalController extends SuperController {
         produces = APPLICATION_JSON_VALUE
     )
     @ResponseStatus(value = HttpStatus.CREATED)
-    @ResponseBody
     @Secured({"prd-admin"})
     public ResponseEntity<ContactInformationResponse> updateContactInformationForOrganisation(
         @io.swagger.v3.oas.annotations.parameters.RequestBody(description = "contactInformationCreationRequests")
