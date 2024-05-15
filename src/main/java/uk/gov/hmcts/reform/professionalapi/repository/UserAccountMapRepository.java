992f67529ead58678fc32ae025e9ddaf508db2bd
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-05-15 19:54:58 +0100

    Decomissioning single server

--- src/main/java/uk/gov/hmcts/reform/professionalapi/repository/UserAccountMapRepository.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/repository/UserAccountMapRepository.java
@@ -1,12 +1,10 @@
 package uk.gov.hmcts.reform.professionalapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.professionalapi.domain.UserAccountMap;
 import uk.gov.hmcts.reform.professionalapi.domain.UserAccountMapId;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,7 +13,4 @@ public interface UserAccountMapRepository extends JpaRepository<UserAccountMap,U
 
     Optional<UserAccountMap> findByUserAccountMapId(UserAccountMapId userAccountMapId);
 
-    @Query(value = "Select * from dbrefdata.user_account_map ua where ua.professional_user_id=:profUserId",
-        nativeQuery = true)
-    List<UserAccountMap>  fetchByProfessionalUserId(UUID profUserId);
 }
--- src/main/java/uk/gov/hmcts/reform/professionalapi/repository/UserAttributeRepository.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/repository/UserAttributeRepository.java
@@ -1,25 +1,12 @@
 package uk.gov.hmcts.reform.professionalapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import uk.gov.hmcts.reform.professionalapi.domain.UserAttribute;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UserAttributeRepository extends JpaRepository<UserAttribute, UUID> {
-
-    @Modifying
-    @Query(value = "delete from dbrefdata.user_attribute ua where ua.professional_user_id=:profUserId",
-        nativeQuery = true)
-    void deleteByProfessionalUserId(UUID profUserId);
-
-    @Query(value = "Select * from dbrefdata.user_attribute ua "
-        + "where ua.professional_user_id=:profUserId and prd_enum_type='ADMIN_ROLE'",
-        nativeQuery = true)
-    List<UserAttribute> fetchByProfessionalUserIdAndPrdEnumType(UUID profUserId);
 
 }
