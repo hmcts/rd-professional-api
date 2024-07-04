122e2374c77cb03fb1875ceac11cd696f1c6d205
Kiran Yenigala <kiran.yenigala@hmcts.net>
2024-07-04 18:20:16 +0100

    Fix duplicate column issue (DATE_APPROVED)

--- src/integrationTest/resources/db/testmigration/V18__alter_table_bulk_customer_detail.sql
+++ src/integrationTest/resources/db/testmigration/V18__alter_table_bulk_customer_detail.sql
@@ -9,9 +9,6 @@ ALTER TABLE bulk_customer_details ADD COLUMN organisation_id VARCHAR(7);
 ALTER TABLE bulk_customer_details ADD CONSTRAINT organisation_fk7 foreign key (organisation_id)
 references organisation (organisation_identifier);
 
-ALTER TABLE ORGANISATION ADD COLUMN date_approved TIMESTAMP;
-
-
 INSERT INTO organisation (id,name,status,sra_regulated,company_number,last_updated,created,organisation_identifier,company_url,sra_id,status_message,date_approved) VALUES
 	 ('046b6c7f-0b8a-43b9-b35d-6489e6daee91','Uday2 Test Org','ACTIVE',false,NULL,'2019-08-16 15:00:41.418','2019-08-16 14:56:47.227','W98ZZ5W',NULL,'SRA1234562134',NULL,NULL);
 
