3fef26e568331fffe767dfe2f151f9ceabda0608
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-09-04 10:55:57 +0100

    Changing endpoint to only update Organisation Name

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/OrganisationSraUpdateRequest.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/OrganisationSraUpdateRequest.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.professionalapi.controller.request;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
