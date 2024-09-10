41d4ffa8789de0b17b8fc4db1d6d6deb03d30668
raj katla <raj.katla1@hmcts.net>
2024-09-10 22:33:28 +0100

    DTSRD-2136: Org name update - refactored error messages

--- src/test/java/uk/gov/hmcts/reform/professionalapi/util/RefDataUtilTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/util/RefDataUtilTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
