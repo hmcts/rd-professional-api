967c9e1d7754775dce7cd328a410a1c5d0a5a131
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2024-09-19 22:53:38 +0100

    made c hanges to endpoint to accept multiple requests in line with DTSRD-2136

--- src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/OrganisationSraUpdateRequest.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/controller/request/OrganisationSraUpdateRequest.java
@@ -2,26 +2,44 @@ package uk.gov.hmcts.reform.professionalapi.controller.request;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
 
 @Getter
 @Setter
-@Builder(builderMethodName = "anOrganisationSraUpdateRequest")
 public class OrganisationSraUpdateRequest {
 
-    @Valid
-    @NotNull(message = "SRA Id is required.")
-    private  String sraId;
 
-    @JsonCreator
-    public OrganisationSraUpdateRequest(
+    @JsonProperty(value = "sarIds")
+    private List<OrganisationSraUpdateData> organisationSraUpdateDataList;
+
+    public OrganisationSraUpdateRequest() {
+
+    }
+
+    @Getter
+    @Setter
+    public static class OrganisationSraUpdateData {
+
+        @Valid
+        @NotNull(message = "SRA Id is required.")
+        private  String sraId;
+
+        @Valid
+        @NotNull(message = " organisation Id is required.")
+        private String organisationId;
+
+        @JsonCreator
+        public OrganisationSraUpdateData(
+            @JsonProperty("organisationId") String organisationId,
             @JsonProperty("sraId") String sraId) {
-        this.sraId = sraId;
+            this.organisationId = organisationId;
+            this.sraId = sraId;
+        }
     }
 }
