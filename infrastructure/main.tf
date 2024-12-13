427085e44374e71b7b4c87d9940c2d0a3c806ad0
Sammi Chong <shinyee.chong@hmcts.net>
2024-12-13 14:46:23 +0000

    Formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -81,9 +81,9 @@ module "db-professional-ref-data-v16" {
   action_group_name           = join("-", [var.action_group_name, local.db_name, var.env])
   email_address_key           = var.email_address_key
   email_address_key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-  cpu_threshold                  = var.cpu_threshold
-  memory_threshold               = var.memory_threshold
-  storage_threshold              = var.storage_threshold
+  cpu_threshold               = var.cpu_threshold
+  memory_threshold            = var.memory_threshold
+  storage_threshold           = var.storage_threshold
 
 }
 
