a21b946ea15168a0cb25963dbc9af80ab0f8a914
Antony Leons <antony@leons.dev>
2025-04-02 13:09:11 +0100

    Update V23__performance_indexes.sql

--- src/main/resources/db/postgres/V23__performance_indexes.sql
+++ src/main/resources/db/postgres/V23__performance_indexes.sql
@@ -1,10 +1,10 @@
-CREATE INDEX idx_bulk_customer_details_bulkCustomerId_sidamId ON dbrefdata.bulk_customer_details (bulk_customer_id, sidam_id);
+CREATE INDEX IF NOT EXISTS idx_bulk_customer_details_bulkCustomerId_sidamId ON dbrefdata.bulk_customer_details (bulk_customer_id, sidam_id);
 
-CREATE INDEX idx_organisation_status_last_updated ON dbrefdata.organisation (status, last_updated);
-CREATE INDEX idx_organisation_org_type_id ON dbrefdata.organisation (org_type, id);
+CREATE INDEX IF NOT EXISTS idx_organisation_status_last_updated ON dbrefdata.organisation (status, last_updated);
+CREATE INDEX IF NOT EXISTS idx_organisation_org_type_id ON dbrefdata.organisation (org_type, id);
 
-CREATE INDEX idx_payment_account_pba_number_organisation_id ON dbrefdata.payment_account (pba_number, organisation_id);
-CREATE INDEX idx_payment_account_pba_number_upper ON dbrefdata.payment_account (upper(pba_number));
+CREATE INDEX IF NOT EXISTS idx_payment_account_pba_number_organisation_id ON dbrefdata.payment_account (pba_number, organisation_id);
+CREATE INDEX IF NOT EXISTS idx_payment_account_pba_number_upper ON dbrefdata.payment_account (upper(pba_number));
 
-CREATE INDEX idx_professional_user_org_id_id ON dbrefdata.professional_user (organisation_id, id);
-CREATE INDEX idx_professional_user_last_updated_id ON dbrefdata.professional_user (last_updated, id);
+CREATE INDEX IF NOT EXISTS idx_professional_user_org_id_id ON dbrefdata.professional_user (organisation_id, id);
+CREATE INDEX IF NOT EXISTS idx_professional_user_last_updated_id ON dbrefdata.professional_user (last_updated, id);
