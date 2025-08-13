0ea0ccfb5599814a4a5bb58e716cc54de34bbee2
lukasz-wolski <1005015+lukasz-wolski@users.noreply.github.com>
2025-08-13 18:44:46 +0100

    Disables V23__performance_indexes.sql

--- src/main/resources/db/postgres/V23__performance_indexes.sql
+++ src/main/resources/db/postgres/V23__performance_indexes.sql
@@ -1,2 +1,2 @@
-CREATE INDEX IF NOT EXISTS idx_professional_user_org_id_id ON dbrefdata.professional_user (organisation_id, id);
-CREATE INDEX IF NOT EXISTS idx_user_attribute_admin_role ON dbrefdata.user_attribute (professional_user_id) WHERE prd_enum_code = 4 AND prd_enum_type = 'ADMIN_ROLE';
+-- CREATE INDEX IF NOT EXISTS idx_professional_user_org_id_id ON dbrefdata.professional_user (organisation_id, id);
+-- CREATE INDEX IF NOT EXISTS idx_user_attribute_admin_role ON dbrefdata.user_attribute (professional_user_id) WHERE prd_enum_code = 4 AND prd_enum_type = 'ADMIN_ROLE';
