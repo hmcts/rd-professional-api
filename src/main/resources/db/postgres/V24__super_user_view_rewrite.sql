4a2844cd8b91dc15d426dc0e27c2e90f27041c35
lukasz-wolski <1005015+lukasz-wolski@users.noreply.github.com>
2025-08-13 18:47:35 +0100

    Disables V24__super_user_view_rewrite.sql

--- src/main/resources/db/postgres/V24__super_user_view_rewrite.sql
+++ src/main/resources/db/postgres/V24__super_user_view_rewrite.sql
@@ -1,18 +1,18 @@
-CREATE OR REPLACE VIEW dbrefdata.super_user_view AS
-SELECT pu.id,
-       pu.first_name,
-       pu.last_name,
-       pu.email_address,
-       pu.organisation_id,
-       pu.last_updated,
-       pu.created,
-       pu.user_identifier,
-       pu.deleted
-FROM dbrefdata.professional_user pu
-WHERE EXISTS (
-    SELECT 1
-    FROM dbrefdata.user_attribute ua
-    WHERE ua.professional_user_id = pu.id
-      AND ua.prd_enum_code = 4
-      AND ua.prd_enum_type = 'ADMIN_ROLE'
-);
+-- CREATE OR REPLACE VIEW dbrefdata.super_user_view AS
+-- SELECT pu.id,
+--        pu.first_name,
+--        pu.last_name,
+--        pu.email_address,
+--        pu.organisation_id,
+--        pu.last_updated,
+--        pu.created,
+--        pu.user_identifier,
+--        pu.deleted
+-- FROM dbrefdata.professional_user pu
+-- WHERE EXISTS (
+--     SELECT 1
+--     FROM dbrefdata.user_attribute ua
+--     WHERE ua.professional_user_id = pu.id
+--       AND ua.prd_enum_code = 4
+--       AND ua.prd_enum_type = 'ADMIN_ROLE'
+-- );
