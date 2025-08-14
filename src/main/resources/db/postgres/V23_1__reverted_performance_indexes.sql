a39aa4f15857bdd109ff504b33d9da4f7476df23
lukasz-wolski <1005015+lukasz-wolski@users.noreply.github.com>
2025-08-14 09:08:44 +0100

    Update V23_1__reverted_performance_indexes.sql

--- src/main/resources/db/postgres/V23_1__reverted_performance_indexes.sql
+++ src/main/resources/db/postgres/V23_1__reverted_performance_indexes.sql
@@ -1,2 +1,2 @@
-drop if exists idx_professional_user_org_id_id;
-drop if exists idx_user_attribute_admin_role;
+drop index if exists idx_professional_user_org_id_id;
+drop index if exists idx_user_attribute_admin_role;
