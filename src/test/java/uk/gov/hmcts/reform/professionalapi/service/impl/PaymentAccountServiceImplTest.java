7011a26c52a902d7c843efa5354d22811d95d012
Antony Leons <antony@leons.dev>
2025-09-03 11:43:14 +0100

    build: update reformS2sClient dependency to 5.3.3
    test: remove unused mockito matcher anySet from PaymentAccountServiceImplTest

--- src/test/java/uk/gov/hmcts/reform/professionalapi/service/impl/PaymentAccountServiceImplTest.java
+++ src/test/java/uk/gov/hmcts/reform/professionalapi/service/impl/PaymentAccountServiceImplTest.java
@@ -38,7 +38,6 @@ import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
