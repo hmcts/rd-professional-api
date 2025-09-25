57f56b08e3cf699bc1bc05b7df10da218f14e8d3
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2025-09-25 15:54:46 +0100

    DTSRD-4960.Filtering records to remove nulls

--- src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/ProfessionalInternalUserFunctionalForV2ApiTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/professionalapi/ProfessionalInternalUserFunctionalForV2ApiTest.java
@@ -494,24 +494,16 @@ class ProfessionalInternalUserFunctionalForV2ApiTest extends AuthorizationFuncti
             organisationOtherOrgsCreationRequest);
         String organisationIdentifier = (String) newOrgResponse.get("organisationIdentifier");
 
-
-        OrganisationOtherOrgsCreationRequest organisationOtherOrgsCreationRequest1 = createOrganisationRequestForV2();
-        organisationOtherOrgsCreationRequest1.getSuperUser().setEmail(generateRandomEmail());
-
-        String intActiveOrgId = createAndUpdateOrganisationToActiveForV2(hmctsAdmin,
-            organisationOtherOrgsCreationRequest1);
-
-
         UsersInOrganisationsByOrganisationIdentifiersRequest request =
             new UsersInOrganisationsByOrganisationIdentifiersRequest();
-        request.setOrganisationIdentifiers(List.of(organisationIdentifier,intActiveOrgId));
+        request.setOrganisationIdentifiers(List.of(organisationIdentifier));
 
         UsersInOrganisationsByOrganisationIdentifiersResponse response =
             professionalApiClient.findUsersInOrganisationShouldBeSuccess(request,
                 null, null, null);
         String sinceDate = generateRandomDate(null, "05");
 
-        assertThat(response.getOrganisationInfo().get(0).getUsers().get(0).getUserIdentifier()).isNull();
+        assertThat(response.getOrganisationInfo()).isNull();
         Map<String, Object> userResponse = professionalApiClient.retrieveUsersBySinceDateOrAndUserId(
             sinceDate,null);
 
