c9336d1cc570e90e5297eea0d0bc71bdca584146
Antony Leons <antony@leons.dev>
2025-10-14 14:00:36 +0100

    Update ProfessionalUserRepository.java

--- src/main/java/uk/gov/hmcts/reform/professionalapi/repository/ProfessionalUserRepository.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/repository/ProfessionalUserRepository.java
@@ -35,7 +35,8 @@ public interface ProfessionalUserRepository extends JpaRepository<ProfessionalUs
     @Query(value = """
        SELECT pu.* FROM dbrefdata.professional_user pu
        INNER JOIN dbrefdata.organisation organisation ON pu.organisation_id = organisation.Id
-       WHERE (:organisationIdentifiers IS NULL OR organisation.organisation_identifier IN (:organisationIdentifiers))
+       WHERE (:organisationIdentifiers IS NULL 
+       OR organisation.organisation_identifier IN (:organisationIdentifiers))
        ORDER BY pu.organisation_id, pu.id
         """, nativeQuery = true)
     Page<ProfessionalUser> findUsersInOrganisations(
@@ -44,7 +45,8 @@ public interface ProfessionalUserRepository extends JpaRepository<ProfessionalUs
     @Query(value = """
        SELECT pu.* FROM dbrefdata.professional_user pu
        INNER JOIN dbrefdata.organisation organisation ON pu.organisation_id = organisation.Id
-       WHERE (:organisationIdentifiers IS NULL OR organisation.organisation_identifier IN (:organisationIdentifiers))
+       WHERE (:organisationIdentifiers IS NULL 
+       OR organisation.organisation_identifier IN (:organisationIdentifiers))
        AND (
            (organisation.Id = :searchAfterOrgId AND pu.Id > :searchAfterUserId)
            OR (organisation.Id > :searchAfterOrgId)
