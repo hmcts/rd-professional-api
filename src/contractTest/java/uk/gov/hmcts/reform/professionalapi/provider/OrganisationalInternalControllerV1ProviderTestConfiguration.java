f14ec666ab494c6c174e35435ace532f053d4532
padmapriyanalam <padmapriya.nalam@gmail.com>
2025-12-08 11:37:21 +0000

    POFCC-89 -Add required mocks

--- src/contractTest/java/uk/gov/hmcts/reform/professionalapi/provider/OrganisationalInternalControllerV1ProviderTestConfiguration.java
+++ src/contractTest/java/uk/gov/hmcts/reform/professionalapi/provider/OrganisationalInternalControllerV1ProviderTestConfiguration.java
@@ -1,11 +1,14 @@
 package uk.gov.hmcts.reform.professionalapi.provider;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import uk.gov.hmcts.reform.professionalapi.controller.internal.OrganisationInternalController;
-import uk.gov.hmcts.reform.professionalapi.controller.request.validator.impl.OrganisationByProfileIdsRequestValidator;
+import uk.gov.hmcts.reform.professionalapi.service.MfaStatusService;
+import uk.gov.hmcts.reform.professionalapi.service.PaymentAccountService;
+import uk.gov.hmcts.reform.professionalapi.service.ProfessionalUserService;
 import uk.gov.hmcts.reform.professionalapi.service.impl.OrganisationServiceImpl;
 
 @TestConfiguration
@@ -15,13 +18,18 @@ public class OrganisationalInternalControllerV1ProviderTestConfiguration extends
     protected OrganisationServiceImpl organisationService;
 
     @MockBean
-    OrganisationByProfileIdsRequestValidator organisationByProfileIdsRequestValidator;
+    protected ProfessionalUserService professionalUserService;
 
+    @MockBean
+    protected PaymentAccountService paymentAccountService;
+
+    @MockBean
+    MfaStatusService mfaStatusService;
 
     @Bean
     @Primary
     protected OrganisationInternalController organisationInternalController() {
-        return new OrganisationInternalController(organisationByProfileIdsRequestValidator);
+        return new OrganisationInternalController();
     }
 
 }
