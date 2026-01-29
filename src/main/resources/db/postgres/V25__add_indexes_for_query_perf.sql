6aecdb6b77f86320d6a75d5120877bc33a168e0c
Antony Leons <antony@leons.dev>
2026-01-29 09:07:58 +0000

    Delete V25__add_indexes_for_query_perf.sql

--- src/main/resources/db/postgres/V25__add_indexes_for_query_perf.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX IF NOT EXISTS idx_professional_user_org_user_identifier
-    ON dbrefdata.professional_user (organisation_id, user_identifier);
-
-CREATE INDEX IF NOT EXISTS idx_professional_user_last_updated_id
-    ON dbrefdata.professional_user (last_updated, id);
