f328352faf89cf1a6e9a1e0021e41c2e6406b1d8
prashanthnerligeb <prashanth.nerlige@hmcts.net>
2026-02-06 14:17:23 +0000

    DTSRB-5445 Initial commit with utility method

--- src/main/java/uk/gov/hmcts/reform/professionalapi/service/impl/OrganisationServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/professionalapi/service/impl/OrganisationServiceImpl.java
@@ -75,13 +75,13 @@ import uk.gov.hmcts.reform.professionalapi.util.ProfileOrgTypeUtility;
 import uk.gov.hmcts.reform.professionalapi.util.RefDataUtil;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
