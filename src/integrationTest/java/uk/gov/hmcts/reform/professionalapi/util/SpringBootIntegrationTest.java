f89cc9707c6675bdb7796de3ad0d913f7c7ab3b8
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2026-03-06 14:20:20 +0000

    DTSRD-4065. Springboot upgrade reverted mockBeans

--- src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/util/SpringBootIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/util/SpringBootIntegrationTest.java
@@ -1,14 +1,14 @@
 package uk.gov.hmcts.reform.professionalapi.util;
 
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.StorageCredentials;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import com.microsoft.azure.storage.CloudStorageAccount;
-import com.microsoft.azure.storage.StorageCredentials;
 import uk.gov.hmcts.reform.professionalapi.Application;
 import uk.gov.hmcts.reform.professionalapi.config.TestAzureBlobConfig;
 
