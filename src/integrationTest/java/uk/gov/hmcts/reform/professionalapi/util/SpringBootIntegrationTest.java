a81b7d710a27f716a2c601af56b8f4803a13dde9
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2026-03-06 17:34:44 +0000

    DTSRD-4065. Springboot upgrade reverted mockBeans

--- src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/util/SpringBootIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/util/SpringBootIntegrationTest.java
@@ -6,15 +6,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.professionalapi.Application;
-import uk.gov.hmcts.reform.professionalapi.config.TestAzureBlobConfig;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Import(TestAzureBlobConfig.class)
 public abstract class SpringBootIntegrationTest {
 
     @LocalServerPort
