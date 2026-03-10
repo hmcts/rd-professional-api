c7c3bf0834398c390bda6f583a17d1d870b05fe7
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2026-03-10 10:28:48 +0000

    DTSRD-4065. Springboot upgrade reverted mockBeans

--- src/contractTest/java/uk/gov/hmcts/reform/professionalapi/provider/ProviderTestConfiguration.java
+++ src/contractTest/java/uk/gov/hmcts/reform/professionalapi/provider/ProviderTestConfiguration.java
@@ -10,7 +10,6 @@ import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import jakarta.persistence.EntityManagerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
