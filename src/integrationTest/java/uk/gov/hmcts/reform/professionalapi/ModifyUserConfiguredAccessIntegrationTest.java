439d203a7593687f994d9b0c59f49ce2dfad75bc
SabinaHMCTS <sabina.sharangdhar@hmcts.net>
2026-03-23 14:45:18 +0000

    DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess (#1891)
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess
    
    * DTSRD-5903.PRD User lastUpdated value when modify UserConfiguredAccess

--- src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/ModifyUserConfiguredAccessIntegrationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/professionalapi/ModifyUserConfiguredAccessIntegrationTest.java
@@ -2,9 +2,11 @@ package uk.gov.hmcts.reform.professionalapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
+import uk.gov.hmcts.reform.professionalapi.domain.ProfessionalUser;
 import uk.gov.hmcts.reform.professionalapi.domain.UserProfileUpdatedData;
 import uk.gov.hmcts.reform.professionalapi.util.AuthorizationEnabledIntegrationTest;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
@@ -36,5 +38,28 @@ class ModifyUserConfiguredAccessIntegrationTest extends AuthorizationEnabledInte
                 .modifyUserRolesOfOrganisationExternal(userProfileUpdatedData, userIdentifier, puiUserManager);
         assertThat(response.get("http_status")).isNotNull();
         assertThat(response.get("http_status")).isEqualTo("200 OK");
+    }
+
+    @Test
+    void should_update_last_updated_when_modifying_user_configured_access_external() throws Exception {
+        updateUserProfileRolesMock(HttpStatus.OK);
+        String userIdentifier = settingUpOrganisation(puiUserManager);
+
+        ProfessionalUser before = professionalUserRepository.findByUserIdentifier(userIdentifier);
+        assertThat(before).isNotNull();
+        LocalDateTime beforeUpdated = before.getLastUpdated();
+        assertThat(beforeUpdated).isNotNull();
+
+        Thread.sleep(1100);
+
+        String email = randomAlphabetic(5) + "@email.com";
+        UserProfileUpdatedData userProfileUpdatedData = createModifyUserConfiguredAccessData(email, 1);
+        Map<String, Object> response = professionalReferenceDataClient
+                .modifyUserRolesOfOrganisationExternal(userProfileUpdatedData, userIdentifier, puiUserManager);
+        assertThat(response.get("http_status")).isNotNull();
+        assertThat(response.get("http_status")).isEqualTo("200 OK");
+
+        ProfessionalUser after = professionalUserRepository.findByUserIdentifier(userIdentifier);
+        assertThat(after.getLastUpdated()).isAfter(beforeUpdated);
     }
 }
